89dcd52bcfdf057e8b026321c4b2750eabdb17e8
Tzikas <niko.tzikas@gmail.com>
2019-11-23 09:54:32 -0500

    styling

--- README.md
+++ README.md
@@ -32,29 +32,35 @@ Create Pull Request so your TAs can check up your work.  Write your JavaScript i
 
 ** First construct three arrays that place the following steps in the correct order: **
 
-**Eggs**
-1. Get eggs from fridge
-2. Place eggs in pot with lid closed
-3. Boil water for 6 mins
-4. Stop and let cool
-5. Peel eggs 
+
+**Steak**
+1. Season steak generously with salt, pepper and garlic powder
+2. Place in ziplock bag
+3. Cook in sous vide at 120 F for 1-2 hours
+4. Remove from bag and pat dry
+5. Heat pan with grapeseed oil and a quarter stick of butter
+6. Cook steak for 30-60 seconds per side using a spoon to baste with butter
+7. Rest for 10 mintutes
+8. Enjoy
+
+**Brussel Sprouts**
+1. Wash burussel srouts
+2. Cut off base and chop in half
+3. Toss in bowl with olive oil, balsamic vinger and salt
+4. Preheat oven to 500
+5. Coat baking sheet with olive oil
+6. Place in flat side down
+7. Cook for 20 minutes
+8. Place back in bowl and add salt and pepper
+9. Enjoy
 
 
-**Bacon**
-1. Get bacon from fridge
-2. Space out the bacon strips on a cool frying pan or griddle
-3. Heat the bacon over medium heat until it starts to sizzle. 
-4. Cook the bacon strips for 10-12 minutes.
-5. Flip the bacon over and fry it for another 7-8 minutes until it's crisp. 
-6. Remove the bacon strips with tongs and drain them on a paper towel.
-
-
-**Orange Juice**
-1.  Get the oranges.
-2.   Peel the oranges.
-3.  Cut or tear them into 1-inch chunks and remove seeds. 
-4.  Pour them into a blender, and blend.   
-5.  Add water and/or surger until tastes good. 
+**Mash Potatoes**
+1.  Boil water
+2.  Tear open bag of of instant potato mix and pour into bowl
+3.  Pour in water
+4.  Mix  
+5.  Enjoy 
 
 
 ## Iteration #1: 
--- script.js
+++ script.js
@@ -1,39 +1,4 @@
-/*  
-Eggs
-1. Get eggs from fridge
-2. Place eggs in pot with lid closed
-3. Boil water for 6 mins
-4. Stop and let cool
-5. Peel eggs 
 
-
-Bacon
-1. Get bacon from fridge
-2. Space out the bacon strips on a cool frying pan or griddle
-3. Heat the bacon over medium heat until it starts to sizzle. 
-4. Cook the bacon strips for 10-12 minutes.
-5. Flip the bacon over and fry it for another 7-8 minutes until it's crisp. 
-6. Remove the bacon strips with tongs and drain them on a paper towel.
-
-
-OrangeJuice
-1.  Get the oranges.
-2.   Peel the oranges.
-3.  Cut or tear them into 1-inch chunks and remove seeds. 
-4.  Pour them into a blender, and blend.   
-5.  Add water and/or surger until tastes good. 
-
-*/
-
-
-
-const eggs = [
-    'Get eggs from fridge',
-    'Place eggs in pot with lid closed',
-    'Boil water for 6 mins',
-    'Stop and let cool',
-    'Peel eggs',
-]
 
 const steak = [
   'season steak generously with salt, pepper and garlic powder',
@@ -43,7 +8,7 @@ const steak = [
   'heat pan with grapeseed oil and a quarter stick of butter',
   'cook steak for 30-60 seconds per side using a spoon to baste with butter',
   'rest for 10 mintutes',
-  'enjoy the best steak of your life'
+  'enjoy'
 ]
 
 const brusselSprouts = [
@@ -63,26 +28,10 @@ const mashPotatoes = [
   'tear open bag of of instant potato mix and pour into bowl',
   'pour in water',
   'mix',
-  'enojoy'
-]
-
-const bacon = [
-    'Get bacon from fridge', 
-    'Space out the bacon strips on a cool frying pan or griddle',
-    'Heat the bacon over medium heat until it starts to sizzle.',
-    'Cook the bacon strips for 10-12 minutes.',
-    'Flip the bacon over and fry it for another 7-8 minutes until it\'s crisp.',    
-    'Remove the bacon strips with tongs and drain them on a paper towel.'
+  'enjoy'
 ]
 
 
-const orangeJuice = [
-    'Get the oranges.',
-    'Peel the oranges.',
-    'Cut or tear them into 1-inch chunks and remove seeds.',
-    'Pour them into a blender, and blend.',
-    'Add water and/or surger until tastes good.'
-]
 
 /** DO NOT ALTER **/
 
@@ -97,43 +46,22 @@ function addFood(step, id){
 }
   
   
-// function makeFood(steps, id){
-//   for(const step of steps){
-//     addFood(step,id)
-//   }
-//   document.querySelector('#table').innerHTML += (`<img src="images/${id.replace('#','')}.jpg" />`)
-//   //document.body.innerHTML += `<button onclick="new Audio('dinnerIsServed.mp3').play()">Dinner is Served!</button>`
-
-// }
-
-// makeFood(steak, '#steak')
-// makeFood(mashPotatoes, '#mashPotatoes')
-// makeFood(brusselSprouts, '#brusselSprouts')
-
-
-async function makeFood(steps, id){
-  let promises = []
+function makeFood(steps, id){
   for(const step of steps){
-    console.log(step)
-    promises.push( 
-      await addFood(step, id)
-    )
-    //console.log(p)
+    addFood(step,id)
   }
-  console.log('here?', id)
   document.querySelector('#table').innerHTML += (`<img src="images/${id.replace('#','')}.jpg" />`)
-  return promises
+
+  //USE AFTER EVERYTHING IS COMPLETE
+  //document.body.innerHTML += `<button onclick="new Audio('dinnerIsServed.mp3').play()">Dinner is Served!</button>`
+  
 }
 
 
+makeFood(steak, '#steak')
+makeFood(mashPotatoes, '#mashPotatoes')
+makeFood(brusselSprouts, '#brusselSprouts')
 
-
-
-Promise.all([makeFood(steak, '#steak'), makeFood(mashPotatoes, '#mashPotatoes'), makeFood(brusselSprouts, '#brusselSprouts')]).then(res=>{
-  console.log('breakfast is made!!',res)
-  //prompt('Congrats!' +String(res));
-  document.body.innerHTML += `<button onclick="new Audio('dinnerIsServed.mp3').play()">Dinner is served.</button>`
-})
 
 
 
