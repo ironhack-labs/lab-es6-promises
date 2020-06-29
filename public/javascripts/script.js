dccbcba4e184ac26ebc78ab154b94cc030a83c08
MarieAmp <marymary09@hotmail.com>
2020-06-29 18:53:20 +0200

    formated

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,87 +5,73 @@
 //   addFood(steak[i], '#steak');
 // }
 
-const table=document.getElementById("table")
-var stimg=new Image()
-stimg.src="../public/images/steak.jpg"
+const table = document.getElementById("table");
+var stimg = new Image();
+stimg.src = "../public/images/steak.jpg";
 
-var mpimg=new Image()
-mpimg.src="../public/images/mashPotatoes.jpg"
+var mpimg = new Image();
+mpimg.src = "../public/images/mashPotatoes.jpg";
 
-var bsimg=new Image()
-bsimg.src="../public/brusselSprouts.jpg"
+var bsimg = new Image();
+bsimg.src = "../public/brusselSprouts.jpg";
 // Iteration 1 using callbacks
-const getSteak=
-(addFood(steak[0], '#steak', () => {
-  addFood(steak[1], '#steak', () => {
-    addFood(steak[2], '#steak', () => {
-      addFood(steak[3], '#steak', () => {
-        addFood(steak[4], '#steak', () => {
-          addFood(steak[5], '#steak', () => {
-            addFood(steak[6], '#steak', () => {
-              addFood(steak[7], '#steak', () => {
-            table.innerHTML+='<img src="../public/images/steak.jpg"/>'
-              })
-            })
-          })
-        })
-      })
-    })
-  })
-})
-);
+const getSteak = addFood(steak[0], "#steak", () => {
+  addFood(steak[1], "#steak", () => {
+    addFood(steak[2], "#steak", () => {
+      addFood(steak[3], "#steak", () => {
+        addFood(steak[4], "#steak", () => {
+          addFood(steak[5], "#steak", () => {
+            addFood(steak[6], "#steak", () => {
+              addFood(steak[7], "#steak", () => {
+                table.innerHTML += '<img src="../public/images/steak.jpg"/>';
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
 
-console.log (getSteak)
+console.log(getSteak);
 
 // Iteration 2 using `.then()`
-const getMash=
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  addFood(mashPotatoes[1], '#mashPotatoes') .then(() => {
-    addFood(mashPotatoes[2], '#mashPotatoes') .then(() => {
-      addFood(mashPotatoes[3], '#mashPotatoes') .then(() => {
-        addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
-        table.innerHTML+='<img src="../public/images/mashPotatoes.jpg"/>'
-        })
-      })
-    })
-  }) 
+const getMash = addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
+  addFood(mashPotatoes[1], "#mashPotatoes").then(() => {
+    addFood(mashPotatoes[2], "#mashPotatoes").then(() => {
+      addFood(mashPotatoes[3], "#mashPotatoes").then(() => {
+        addFood(mashPotatoes[4], "#mashPotatoes").then(() => {
+          table.innerHTML += '<img src="../public/images/mashPotatoes.jpg"/>';
+        });
+      });
+    });
+  });
 });
 
 // Iteration 3 using async and await
 
-  
-    async function makeFood() {
-     for (let i=0;i<brusselSprouts.length;i++){
-    const step=brusselSprouts[i]
-   const eachStep=await addFood(step,'#brusselSprouts')  
-}
-
+async function makeFood() {
+  for (let i = 0; i < brusselSprouts.length; i++) {
+    const step = brusselSprouts[i];
+    const eachStep = await addFood(step, "#brusselSprouts");
   }
-  const getSprouts=
-  makeFood()
-  .then(()=>{
-    table.innerHTML+='<img src="../public/images/brusselSprouts.jpg"/>'
+}
+const getSprouts = makeFood()
+  .then(() => {
+    table.innerHTML += '<img src="../public/images/brusselSprouts.jpg"/>';
   })
-  .catch((err)=>{
+  .catch((err) => {
     console.error(err);
+  });
+
+const stp = document.getElementById("steps");
+
+Promise.all([getSteak, getMash, getSprouts])
+  .then(() => {
+    const btn = document.createElement("button");
+    btn.innerHTML = "Dinner is served";
+    stp.append(btn);
   })
-
-
-
-const stp=document.getElementById("steps");
-
-Promise.all([getSteak,getMash,getSprouts]) 
-.then(()=>{
-const btn=document.createElement('button');
-btn.innerHTML='Dinner is served';
-stp.append(btn)
-})
-.catch((err)=>{
-  console.error(err);
-
-})
-
-
-
-
-
+  .catch((err) => {
+    console.error(err);
+  });
