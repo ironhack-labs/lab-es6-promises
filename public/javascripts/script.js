dfe8f912b73704908092af5e05ecebfa484f3ae4
Cha Noguer <definitely.not.a.chatbot@gmail.com>
2020-06-29 16:18:45 +0200

    finished iterations

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,25 +1,54 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < steak.length; i++) {
-  addFood(steak[i], '#steak');
-}
+//for (let i = 0; i < steak.length; i++) {
+//  addFood(steak[i], '#steak');
+//}
 
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
+addFood(steak[0], "#steak", () => {
+  addFood(steak[1], "#steak", () => {
+    addFood(steak[2], "#steak", () => {
+      addFood(steak[3], "#steak", () => {
+        addFood(steak[4], "#steak", () => {
+          addFood(steak[5], "#steak", () => {
+            addFood(steak[6], "#steak", () => {
+              addFood(steak[7], "#steak", () => {});
+            });
+          });
+        });
+      });
+    });
+  });
 });
+
+
 
 // Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
+addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
+  addFood(mashPotatoes[1], "#mashPotatoes").then(() => {
+    addFood(mashPotatoes[2], "#mashPotatoes").then(() => {
+      addFood(mashPotatoes[3], "#mashPotatoes").then(() => {
+        addFood(mashPotatoes[4], "#mashPotatoes");
+      });
+    });
+  });
 });
+
+
 
 // Iteration 3 using async and await
+//works but not in order
 
-brusselSprouts.forEach(eachStep => {
-  async function makeFood(step) {
-    // ... your code here
-  }
-  makeFood(eachStep);
-});
+  async function makeFood() {
+    for (let i = 0; i < brusselSprouts.length; i++) {
+      try {
+         await addFood(brusselSprouts[i], "#brusselSprouts");
+        console.log("async works");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  makeFood();
