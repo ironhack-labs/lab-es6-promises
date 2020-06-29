564c4d019d6758a9b68bdc74aa735acc00969d89
Aude Richon <auderichon@gmail.com>
2020-06-29 17:40:36 +0200

    problem with promiseall

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,25 +1,63 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < steak.length; i++) {
-  addFood(steak[i], '#steak');
-}
+// for (let i = 0; i < steak.length; i++) {
+//   addFood(steak[i], '#steak');
+// }
+
+const table = document.getElementById("table");
 
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
-});
+function addSteak() {
+  addFood(steak[0], '#steak', () => {
+    addFood(steak[1], '#steak', () => {
+      addFood(steak[2], '#steak', () => {
+        addFood(steak[3], '#steak', () => {
+          addFood(steak[4], '#steak', () => {
+            addFood(steak[5], '#steak', () => {
+              addFood(steak[6], '#steak', () => {
+                addFood(steak[7], '#steak', () => {
+                  table.innerHTML += `<img src="../public/images/steak.jpg" alt="steak">`;
+  })})})})})})})})
+};
+//addSteak();
 
 // Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-});
+function addPotatoes() {
+  addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+          addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
+            table.innerHTML += `<img src="../public/images/mashPotatoes.jpg" alt="mashed potatoes">`;
+          })
+        })
+      })
+    })
+  })
+};
+//addPotatoes();
+
 
 // Iteration 3 using async and await
+// brusselSprouts.forEach(eachStep => {
+//   async function makeFood(step) {
+//     const result = await addFood(step, '#brusselSprouts');
+//   }
+//   makeFood(eachStep);
+// });
 
-brusselSprouts.forEach(eachStep => {
-  async function makeFood(step) {
-    // ... your code here
+async function bsLoop() {
+  for (let index = 0; index < brusselSprouts.length; index++) {
+    const step = brusselSprouts[index];
+    const print = await addFood(step, '#brusselSprouts');
   }
-  makeFood(eachStep);
+  table.innerHTML += `<img src="../public/images/brusselSprouts.png" alt="brussel sprout">`;
+};
+//bsLoop();
+//addSteak(), addPotatoes(), bsLoop()
+Promise.all([bsLoop()]).then(() => {
+  console.log("promiseall");
+  //console.log(Promise.all([addSteak, addPotatoes, bsLoop]));
+  alert("Dinner is served!");
 });
