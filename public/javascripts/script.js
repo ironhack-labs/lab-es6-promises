5c705eaa86c7d30de7c541f5990d087586da8e82
kevin4052 <kevin.hernandez001@gmail.com>
2020-07-21 16:23:57 -0400

    generalized async function for whole meal

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,35 +5,35 @@
 //   addFood(steak[i], '#steak');
 // }
 
-// Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  addFood(steak[1], '#steak', () => {
-    addFood(steak[2], '#steak', () => {
-      addFood(steak[3], '#steak', () => {
-        addFood(steak[4], '#steak', () => {
-          addFood(steak[5], '#steak', () => {
-            addFood(steak[6], '#steak', () => {
-              addFood(steak[7], '#steak', () => {
-              });
-            });
-          });
-        });
-      });
-    });
-  });
-});
+// // Iteration 1 using callbacks
+// addFood(steak[0], '#steak', () => {
+//   addFood(steak[1], '#steak', () => {
+//     addFood(steak[2], '#steak', () => {
+//       addFood(steak[3], '#steak', () => {
+//         addFood(steak[4], '#steak', () => {
+//           addFood(steak[5], '#steak', () => {
+//             addFood(steak[6], '#steak', () => {
+//               addFood(steak[7], '#steak', () => {
+//               });
+//             });
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
 
-// Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
-    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
-      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
-        addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
-        });
-      });
-    });
-  });
-});
+// // Iteration 2 using `.then()`
+// addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
+//   addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+//     addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+//       addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+//         addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
+//         });
+//       });
+//     });
+//   });
+// });
 
 // Iteration 3 using async and await
 
@@ -45,9 +45,13 @@ addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
 // });
 
 //changed to a "for of" loop for the async/await methods
-const makeFood = async () =>{
-  for (const eachStep of brusselSprouts) {
-    await addFood(eachStep, '#brusselSprouts');
+const makeFood = async (food, id) =>{
+  for (const eachStep of food) {
+    await addFood(eachStep, id);
   }
 }
-makeFood();
+makeFood(steak, '#steak');
+makeFood(mashPotatoes, '#mashPotatoes');
+makeFood(brusselSprouts, '#brusselSprouts');
+
+
