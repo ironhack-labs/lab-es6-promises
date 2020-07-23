1060d7b2b582d14938ded59a034db8a2bb6dcfe8
alexugo <alex.ugodnikov@gmail.com>
2020-07-22 21:29:01 -0400

    Iteration 3 done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,7 +3,7 @@
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
 for (let i = 0; i < mashPotatoes.length; i++) {
   //addFood(steak[i], '#steak');
-  console.log(steak[i]);
+  //console.log(steak[i]);
 }
 
 // Iteration 1 using callbacks
@@ -16,8 +16,9 @@ addFood(steak[0], '#steak', () => {
           addFood(steak[5], '#steak', () => {
             addFood(steak[6], '#steak', () => { 
               addFood(steak[7], '#steak', () => {
-                console.log('Steak cooked');
+                //console.log('Steak cooked');
                 document.getElementById('table').innerHTML+='<img src=\"public/images/steak.jpg\">';
+                const steakCooked = Promise.resolve(1);
                 });
               });
             });
@@ -37,6 +38,7 @@ addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
       addFood(mashPotatoes[3], '#mashPotatoes').then(()=>{
         addFood(mashPotatoes[4], '#mashPotatoes').then(()=>{
           document.getElementById('table').innerHTML+='<img src=\"public/images/mashPotatoes.jpg\">';
+          const potatoesCooked = Promise.resolve(2);
         })
       })
     });
@@ -47,8 +49,20 @@ addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
-  }
-  //makeFood(eachStep);
+  async function makeFood() {
+    await addFood(brusselSprouts[1], "#brusselSprouts");
+    await addFood(brusselSprouts[2], "#brusselSprouts");
+    await addFood(brusselSprouts[3], "#brusselSprouts");
+    await addFood(brusselSprouts[4], "#brusselSprouts");
+    await addFood(brusselSprouts[5], "#brusselSprouts");
+    await addFood(brusselSprouts[6], "#brusselSprouts");
+    await addFood(brusselSprouts[7], "#brusselSprouts");
+    await addFood(brusselSprouts[8], "#brusselSprouts")
+    .then((result) => {
+      document.getElementById('table').innerHTML+='<img src=\"public/images/brusselSprouts.jpg\">';
+      const brusselCooked = Promise.resolve(3);
+    });
+  }  
+  makeFood();
+
+
