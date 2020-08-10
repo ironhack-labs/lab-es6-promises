19088262b78962761317cfa9cc3ae12aa785a3da
Robson <robsonoliveira@gmail.com>
2020-08-09 22:08:29 -0300

    done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,38 +1,38 @@
-// // Iteration 1 using callbacks
-// addFood(steak[0], "#steak", () => {
-//   addFood(steak[1], "#steak", () => {
-//     addFood(steak[2], "#steak", () => {
-//       addFood(steak[3], "#steak", () => {
-//         addFood(steak[4], "#steak", () => {
-//           addFood(steak[5], "#steak", () => {
-//             addFood(steak[6], "#steak", () => {
-//               addFood(steak[7], "#steak", () => {
-//                 document.querySelector(
-//                   "#table"
-//                 ).innerHTML += `<img src="public/images/steak.jpg" />`;
-//               });
-//             });
-//           });
-//         });
-//       });
-//     });
-//   });
-// });
+// Iteration 1 using callbacks
+addFood(steak[0], "#steak", () => {
+  addFood(steak[1], "#steak", () => {
+    addFood(steak[2], "#steak", () => {
+      addFood(steak[3], "#steak", () => {
+        addFood(steak[4], "#steak", () => {
+          addFood(steak[5], "#steak", () => {
+            addFood(steak[6], "#steak", () => {
+              addFood(steak[7], "#steak", () => {
+                document.querySelector(
+                  "#table"
+                ).innerHTML += `<img src="public/images/steak.jpg" />`;
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
 
-// // Iteration 2 using `.then()`
-// addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
-//   addFood(mashPotatoes[1], "#mashPotatoes").then(() => {
-//     addFood(mashPotatoes[2], "#mashPotatoes").then(() => {
-//       addFood(mashPotatoes[3], "#mashPotatoes").then(() => {
-//         addFood(mashPotatoes[4], "#mashPotatoes").then(() => {
-//           document.querySelector(
-//             "#table"
-//           ).innerHTML += `<img src="public/images/mashPotatoes.jpg" />`;
-//         });
-//       });
-//     });
-//   });
-// });
+// Iteration 2 using `.then()`
+addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
+  addFood(mashPotatoes[1], "#mashPotatoes").then(() => {
+    addFood(mashPotatoes[2], "#mashPotatoes").then(() => {
+      addFood(mashPotatoes[3], "#mashPotatoes").then(() => {
+        addFood(mashPotatoes[4], "#mashPotatoes").then(() => {
+          document.querySelector(
+            "#table"
+          ).innerHTML += `<img src="public/images/mashPotatoes.jpg" />`;
+        });
+      });
+    });
+  });
+});
 
 // Iteration 3 using async and await
 
