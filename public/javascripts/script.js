71a9e099dca2c7ea0d8ec222716dfe4e7b57b19f
Kochasa <67097121+Kochasa@users.noreply.github.com>
2020-08-17 13:45:12 +0200

    finish bonus 2

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,3 @@
-// This will print in the wrong order
-// we added it for you to test to make sure data is loaded
-// 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
-}
-
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
   // ... your code here
@@ -16,7 +8,7 @@ addFood(steak[0], '#steak', () => {
           addFood(steak[5],`#steak`,() => {
             addFood(steak[6],`#steak`,() => {
               addFood(steak[7],`#steak`,() => {
-
+                document.querySelector("#table").innerHTML += `<img src="public/images/steak.jpg" />`;
               })
             })
           })
@@ -36,22 +28,29 @@ addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
     addFood(mashPotatoes[2],`#mashPotatoes`).then(() => {
       addFood(mashPotatoes[3],`#mashPotatoes`).then(() => {
         addFood(mashPotatoes[4],`#mashPotatoes`).then(() => {
-
+          document.querySelector("#table").innerHTML += `<img src="public/images/mashPotatoes.jpg" />`;
         })
       })
     })
   })
 });
-
+ 
+BrusellsImg = () => {
+  document.querySelector("#table").innerHTML += `<img src ="public/images/brusselSprouts.jpg" />`;
+}
 // Iteration 3 using async and await
 
 async function makeFood() {
 	for (let i = 0; i < brusselSprouts.length; i++) {
 		await addFood(brusselSprouts[i], "#brusselSprouts");
 	}
-	await addToTable();
+	await BrusellsImg();
 }
 
 makeFood();   
+
+Promise.all([makeFood()]).then(() => {
+    alert("Dinner is served!");
+});
   
 
