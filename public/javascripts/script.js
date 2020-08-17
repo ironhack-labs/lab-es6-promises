8d9b148e281b6df1a5321f3b1c6eac98579d4011
ViktorJT <viktor.jensentorp@hyperisland.se>
2020-08-17 15:21:01 +0200

    done, only bonus 2 remaining

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,63 @@
-// This will print in the wrong order
-// we added it for you to test to make sure data is loaded
-// 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
+const tableElement = document.getElementsByTagName('section')[0];3
+
+const addImage = (foodName) => {
+  tableElement.innerHTML += `<img src="./public/images/${foodName}.jpg">`;
 }
 
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
-  addFood(steak[1], '#steak', () => {
+// addFood(steak[0], '#steak', () => {
+//   addFood(steak[1], '#steak', () => {
+//     addFood(steak[2], '#steak', () => {
+//       addFood(steak[3], '#steak', () => {
+//         addFood(steak[4], '#steak', () => {
+//           addFood(steak[5], '#steak', () => {
+//             addFood(steak[6], '#steak', () => {
+//               addFood(steak[7], '#steak', () => {
+//                 addImage('steak');
+//               })
+//             })
+//           })
+//         })
+//       })
+//     })
+//   })
+// });
 
-  })
-});
-
-
-
-// Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
-});
+// // Iteration 2 using `.then()`
+// addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
+//   addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+//     addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+//       addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+//         addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
+//           addImage('mashPotatoes');
+//         });
+//       });
+//     });
+//   });
+// });
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
-  }
-  makeFood(eachStep);
+async function makeFood(steps, id) {
+  for (let step of steps) await addFood(step, id);
+}
+
+// Bonus 2:
+async function promiseAll() {
+  await Promise.all([
+    makeFood(steak, '#steak'),
+    makeFood(mashPotatoes, '#mashPotatoes'),
+    makeFood(brusselSprouts, '#brusselSprouts')
+  ]).then(() => {
+    addImage('steak');
+    addImage('mashPotatoes');
+    addImage('brusselSprouts');
+  }).then(() => {
+    const audio = new Audio('/public/media/dinnerIsServed.mp3');
+    audio.volume = 0.2;
+    audio.play();
+    alert('🍽 time!')
+  });
+}
+
+promiseAll();
