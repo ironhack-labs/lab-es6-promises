11088ca3ddf93e78cb865f7e874298196e0a89b3
anna dodo <anna.dorenskaya@gmail.com>
2020-08-20 23:03:50 +0200

    all iterations + bonuses

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,74 +1,71 @@
-// This will print in the wrong order
-// we added it for you to test to make sure data is loaded
-// 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-/* for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
-} */
 // Iteration 1 using callbacks
-let table = document.getElementById('table')
-function showSteak() {
-  
-  let steakImg = document.createElement('img')
-  steakImg.setAttribute('src', '../public/images/steak.jpg')
-  table.appendChild(steakImg)
+
+let steakImg = document.createElement("img");
+function prepareSteak() {
+  function showSteak() {
+    let table = document.getElementById("table");
+    steakImg.setAttribute("src", "../public/images/steak.jpg");
+    table.appendChild(steakImg);
+  }
+
+  addFood(steak[0], "#steak", () => {
+    addFood(steak[1], "#steak", () => {
+      addFood(steak[2], "#steak", () => {
+        addFood(steak[3], "#steak", () => {
+          addFood(steak[4], "#steak", () => {
+            addFood(steak[5], "#steak", () => {
+              addFood(steak[6], "#steak", () => {
+                addFood(steak[7], "#steak", () => {
+                  showSteak();
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
 }
-
-addFood(steak[0], '#steak', () => {
-  addFood(steak[1], '#steak', () => {
-    addFood(steak[2], '#steak', () => {
-      addFood(steak[3], '#steak', () => { 
-        addFood(steak[4], '#steak', () => {
-          addFood(steak[5], '#steak', () => { 
-            addFood(steak[6], '#steak', () => { 
-              addFood(steak[7], '#steak', () => { 
-                showSteak()
-
-              })
-            })
-          })
-         })
-      })
-     })
-  })
-});
 
 // Iteration 2 using `.then()`
-function showPotate() {
-  let potateImg = document.createElement('img')
-  potateImg.setAttribute('src', '../public/images/mashPotatoes.jpg')
-  table.appendChild(potateImg)
+function preparePotate() {
+  function showPotate() {
+    let potateImg = document.createElement("img");
+    potateImg.setAttribute("src", "../public/images/mashPotatoes.jpg");
+    table.appendChild(potateImg);
+  }
+
+  addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
+    addFood(mashPotatoes[1], "#mashPotatoes").then(() => {
+      addFood(mashPotatoes[2], "#mashPotatoes").then(() => {
+        addFood(mashPotatoes[3], "#mashPotatoes").then(() => {
+          addFood(mashPotatoes[4], "#mashPotatoes").then(() => {
+            showPotate();
+          });
+        });
+      });
+    });
+  });
 }
-
-
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  addFood(mashPotatoes[1], '#mashPotatoes').then(() => { 
-    addFood(mashPotatoes[2], '#mashPotatoes').then(() => { 
-      addFood(mashPotatoes[3], '#mashPotatoes').then(() => { 
-        addFood(mashPotatoes[4], '#mashPotatoes').then(() => { 
-          showPotate() 
-        })
-      })
-    })
-  })
-});
-
-
 
 // Iteration 3 using async and await
+
 function showBru() {
-  let bruImg = document.createElement('img')
-  bruImg.setAttribute('src', '../public/images/brusselSprouts.jpg')
-  table.appendChild(bruImg)
+  let bruImg = document.createElement("img");
+  bruImg.setAttribute("src", "../public/images/brusselSprouts.jpg");
+  table.appendChild(bruImg);
+}
+async function makeFood(step, id) {
+  for (let i = 0; i < brusselSprouts.length; i++) {
+    await addFood(brusselSprouts[i], "#brusselSprouts");
+  }
+  await showBru();
 }
 
-  async function makeFood(step, id) {
-    for(let i = 0; i < brusselSprouts.length; i++) {
-      await addFood(brusselSprouts[i], '#brusselSprouts')
-    }
-    await  showBru()
-  }
-makeFood();
-
-//bonus 1
-
+//bonus 2
+Promise.all([prepareSteak(), preparePotate(), makeFood()]).then(
+  () =>
+    (document.getElementById(
+      "steps"
+    ).innerHTML += `<button>Dinner is ready</button>`)
+);
