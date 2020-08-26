94027393d9c7db13c93d6b24388da2f4915138ca
unimatrix2 <dnny.nis@gmail.com>
2020-08-25 22:48:53 -0300

    finised iteration 1 & 2; started Bonus 1 & 2

--- public/javascripts/addFood.js
+++ public/javascripts/addFood.js
@@ -3,12 +3,12 @@
 /****** DO NOT TOUCH vvv *****/
 
 function addFood(step, id, callback) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      document.querySelector(id).innerHTML += `<li>${step}</li`;
-      resolve(step);
-      callback ? callback() : ''; // For iteration one
-    }, Math.floor(Math.random() * 1000));
-  });
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.querySelector(id).innerHTML += `<li>${step}</li`;
+            resolve(step);
+            callback ? callback() : ''; // For iteration one
+        }, Math.floor(Math.random() * 1000));
+    });
 }
 /***** ^^^ DO NOT TOUCH *****/
