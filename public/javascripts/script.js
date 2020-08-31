f3e2271ea153897a13d9309813e64beb96490092
Olesya Steinmetz <newsabome@icloud.com>
2020-08-31 15:42:53 +0200

    Done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,11 +6,26 @@ for (let i = 0; i < mashPotatoes.length; i++) {
   console.log(mashPotatoes[i])
 }
 
+const addImage = img => document.querySelector('#table').innerHTML += `<img src='public/images/${img}.jpg'/>`;
+
+
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
   // ... your code here
   addFood(steak[1], '#steak', () => {
-
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
+                //addImage('#steak')
+              })
+            })
+          })
+        })
+      })
+    })
   })
 });
 
@@ -18,14 +33,33 @@ addFood(steak[0], '#steak', () => {
 
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
+          //addImage('#mashPotatoes')
+        });
+      });
+    });
+  });
 });
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
+  async function makeFood(steps, id) {
     // ... your code here
-    
+    for (let step of steps) {
+      await addFood(step, id)
+    }
+    addImage(id.slice(1))
   }
-  makeFood(eachStep);
+  //makeFood(brusselSprouts, '#brusselSprouts');
+
+  Promise.all([
+    makeFood(steak, '#steak'),
+    makeFood(brusselSprouts, '#brusselSprouts'),
+    makeFood(mashPotatoes, '#mashPotatoes')
+  ]).then(response => {
+    document.body.innerHTML += `<button onclick ="new Audio('public/media/dinnerIsServed.mp3').play()"> Dinner is served </button>`;
+  })
+
