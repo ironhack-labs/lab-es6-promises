1ce0b5db915f674236caa9044476506908ac155d
carolinacervelaf <carolinacervelaf@gmail.com>
2020-09-07 20:22:20 -0300

    Miss the last bonus

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,17 +1,30 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
+/*  for (let i = 0; i < mashPotatoes.length; i++) {
   //addFood(steak[i], '#steak');
   console.log(mashPotatoes[i])
-}
+} */
+
+const img = document.getElementById('table');
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
   // ... your code here
   addFood(steak[1], '#steak', () => {
-
-  })
+   addFood(steak[2], '#steak', () => {
+     addFood(steak[3], '#steak', () => {
+       addFood(steak[4], '#steak', () => {
+         addFood(steak[5], '#steak', () => {
+           addFood(steak[6], '#steak', () => {
+             addFood(steak[7], '#steak');
+             img.innerHTML += '<img src= "./public/images/steak.jpg">'
+           });
+         });
+       });
+     });
+   });
+  });
 });
 
 
@@ -19,13 +32,27 @@ addFood(steak[0], '#steak', () => {
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
   // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[4], '#mashPotatoes');
+        img.innerHTML += '<img src= "./public/images/mashPotatoes.jpg">'
+      });
+    });
+  });
 });
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
+
+    brusselSprouts.forEach(eachStep => {
+    async function makeFood(step) {
+    await addFood(step, '#brusselSprouts')
   }
-  makeFood(eachStep);
+ 
+    makeFood(eachStep);
+    
+    });
+    img.innerHTML += '<img src= "./public/images/brusselSprouts.jpg">'
+    
+  
