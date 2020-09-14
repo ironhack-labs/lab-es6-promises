c5424bef34df504ee46c6f752f7db739e2abe636
Francois Vieux <francois.g.vieux@gmail.com>
2020-09-14 17:06:06 +0200

    done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,52 +5,57 @@
 //   addFood(steak[i], '#steak');
 //   console.log(mashPotatoes[i])
 // }
-const appendPic = (num) => {
-  if(num === 1) document.getElementById('table').innerHTML +='<img src="./public/images/brusselSprouts.jpg" alt=""></img>';
-  else if(num===2) document.getElementById('table').innerHTML += '<img src="./public/images/steak.jpg" alt="">';
+const appendPic = (id) => {
+  if(id === '#brusselSprouts') document.getElementById('table').innerHTML +='<img src="./public/images/brusselSprouts.jpg" alt=""></img>';
+  else if(id === '#steak') document.getElementById('table').innerHTML += '<img src="./public/images/steak.jpg" alt="">';
   else document.getElementById('table').innerHTML += '<img src="./public/images/mashPotatoes.jpg" alt="">';
 }
 
 
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  addFood(steak[1], '#steak', () => {
-    addFood(steak[2], '#steak', () => {
-      addFood(steak[3], '#steak', () => {
-        addFood(steak[4], '#steak', () => {
-          addFood(steak[5], '#steak', () => {
-            addFood(steak[6], '#steak', () => {
-              addFood(steak[7], '#steak', () => {
-                appendPic(2);
-              })
-            })
-          })
-        })
-      })
-    })
-  });
-});
+// addFood(steak[0], '#steak', () => {
+//   addFood(steak[1], '#steak', () => {
+//     addFood(steak[2], '#steak', () => {
+//       addFood(steak[3], '#steak', () => {
+//         addFood(steak[4], '#steak', () => {
+//           addFood(steak[5], '#steak', () => {
+//             addFood(steak[6], '#steak', () => {
+//               addFood(steak[7], '#steak', () => {
+//                 appendPic(2);
+//               })
+//             })
+//           })
+//         })
+//       })
+//     })
+//   });
+// });
 
 
 
-// Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
-    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
-      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
-        addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
-          appendPic();
-        })
-      });
-    });
-  });
-});
+// // Iteration 2 using `.then()`
+// addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
+//   addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+//     addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+//       addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+//         addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
+//           appendPic();
+//         })
+//       });
+//     });
+//   });
+// });
 
 // Iteration 3 using async and await
-const makeFood = async step => {
-  for(let i=0; i<step.length; i++) await addFood(step[i], '#brusselSprouts');
-  appendPic(1)
+const makeFood = async (step, id) => {
+  for(let i=0; i<step.length; i++) await addFood(step[i], id);
+  appendPic(id)
 };
 
-makeFood(brusselSprouts);
+let makeMash = makeFood(mashPotatoes, '#mashPotatoes');
+let makeBrussel = makeFood(brusselSprouts, '#brusselSprouts');
+let makeSteak = makeFood(steak, '#steak');
 
+
+Promise.all([makeMash, makeBrussel, makeSteak])
+.then(() => document.body.innerHTML += '<button>Dinner is served!</button>');
