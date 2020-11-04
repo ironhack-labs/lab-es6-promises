1cde633ed1a4475f63e8949ebd4933ec58620359
Dirk Kelderman <dirkkelderman@gmail.com>
2020-11-04 15:25:48 +0100

    first bonus

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -15,6 +15,9 @@ addFood(steak[0], '#steak', () => {
           addFood(steak[5], '#steak', () => {
             addFood(steak[6], '#steak', () => {
               addFood(steak[7], '#steak', () => {
+                let imgSteak = document.createElement('img');                 
+                imgSteak.src = './public/images/steak.jpg';       
+                document.getElementById('table').appendChild(imgSteak);
               });
             });
           });
@@ -35,6 +38,11 @@ addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
       return addFood(mashPotatoes[3], '#mashPotatoes')
       .then(() => {
         return addFood(mashPotatoes[4], '#mashPotatoes')
+        .then(() => {
+          let imgPotatoes = document.createElement('img');                 
+          imgPotatoes.src = './public/images/mashPotatoes.jpg';       
+          document.getElementById('table').appendChild(imgPotatoes);        
+        })
       })
     })
   })
@@ -52,6 +60,16 @@ addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
     await addFood(brusselSprouts[6], '#brusselSprouts')
     await addFood(brusselSprouts[7], '#brusselSprouts')
     await addFood(brusselSprouts[8], '#brusselSprouts')
+    let imgSprouts = document.createElement('img');                 
+    imgSprouts.src = './public/images/brusselSprouts.jpg';       
+    await document.getElementById('table').appendChild(imgSprouts);     
   }
   
   makeFood(brusselSprouts);
+
+  let resolvedPromisesArray = [steak[7],brusselSprouts[4], mashPotatoes[8] ]
+
+  Promise.all([steak[7], brusselSprouts[4], mashPotatoes[8]])
+  .then(() => {
+    alert('Dinner is served!')
+  });
