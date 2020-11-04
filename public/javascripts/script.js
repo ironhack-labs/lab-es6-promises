677a41d8bf0abd319b0f3a62685b88dbdf3f9c18
Zhenni Qian <zhenni.qian913@gmail.com>
2020-11-04 23:56:37 +0100

    3 iterations done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,48 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
+/*for (let i = 0; i < mashPotatoes.length; i++) {
+  addFood(steak[i], '#steak');
   console.log(mashPotatoes[i])
-}
+}*/
 
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
-  addFood(steak[1], '#steak', () => {
+function recursiveCallback(i) {
+  addFood(steak[i], '#steak', () => {
+    if (typeof steak[i+1] !== 'undefined') {
+      recursiveCallback(i+1);
+    }
+  });
+}
+recursiveCallback(0);
 
-  })
-});
-
+function showImage() {
+  const img = document.createElement('img');
+  img.setAttribute("src", "public/images/steak.jpg");
+  document.querySelector("#table").appendChild(img);
+}
 
 
 // Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
-});
+
+function writeRecipe(i) {
+  addFood(mashPotatoes[i], '#mashPotatoes')
+  .then(() => {
+    if (typeof mashPotatoes[i+1] !== 'undefined') {
+      writeRecipe(i+1);
+    }
+  }); 
+}
+writeRecipe(0);
+
+
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
+async function makeFood(steps) {
+  for (let i = 0; i < steps.length; i++) {
+    await addFood(steps[i], '#brusselSprouts');
   }
-  makeFood(eachStep);
+}
+makeFood(brusselSprouts);
+
