830e5e7d2fbaf7d45be36ef28a1acef055102c49
mahavinay <mahavinay28@gmail.com>
2020-11-07 17:56:46 +0100

    In iteration 3

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,60 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
+/* for (let i = 0; i < mashPotatoes.length; i++) {
   //addFood(steak[i], '#steak');
   console.log(mashPotatoes[i])
-}
+} */
 
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
-  addFood(steak[1], '#steak', () => {
 
-  })
-});
+/* function addFoodFunction(addfood) {
+  if(element < steak.length){
+    addFood(steak[element], '#steak',(addFoodFunction)=>{
+    element++;
+    addfood();
+    }) 
+} 
+} */
+
+
+//const directionArray = [1,3,4,5,0,2,6,7]
+addFood(steak[1], '#steak', () => {
+  addFood(steak[3], '#steak', () => {
+    addFood(steak[4], '#steak', () => {
+      addFood(steak[5], '#steak', () => {
+        addFood(steak[0], '#steak', () => {
+          addFood(steak[2], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
+              });
+            });
+         });
+        });
+      });
+    });
+  });
+}); 
 
 
 
-// Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
-});
+ //Iteration 2 using `.then()`
+ addFood(mashPotatoes[0], '#mashPotatoes') 
+  .then(() => { addFood(mashPotatoes[1], '#mashPotatoes')})
+  .then(() => { addFood(mashPotatoes[2], '#mashPotatoes')})
+  .then(() => { addFood(mashPotatoes[3], '#mashPotatoes')})
+  .then(() => { addFood(mashPotatoes[4], '#mashPotatoes')})
+  .then(() => { addFood(mashPotatoes[5], '#mashPotatoes')})
+  .then(() => { addFood(mashPotatoes[6], '#mashPotatoes')})
+  .then(() => { addFood(mashPotatoes[7], '#mashPotatoes')})
+  .then(() => { addFood(mashPotatoes[8], '#mashPotatoes')})
+  
+ 
 
 // Iteration 3 using async and await
+async function makeFood(step) {
+  // ... your code here
+  
+}
+makeFood(eachStep);
 
-  async function makeFood(step) {
-    // ... your code here
-    
-  }
-  makeFood(eachStep);
