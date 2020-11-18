9c12b884f277cefe61a0fa0c2a7ebf36a75fcd89
ayline <ayline1695@hotmail.com>
2020-11-19 00:32:07 +0100

    images

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -15,14 +15,15 @@ addFood(steak[0], '#steak', () => {
           addFood(steak[5], '#steak', () => {
             addFood(steak[6], '#steak', () => {
               addFood(steak[7], '#steak', () => {
-              })
+                document.querySelector("#table").innerHTML += '<img alt="steak" src="./public/images/steak.jpg" width="100%" />';
+               })
             })
           })
         })
       })
     })
   })
-
+              
 });
 
 
@@ -34,6 +35,7 @@ addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
     addFood(mashPotatoes[2], '#mashPotatoes').then(()=>{
       addFood(mashPotatoes[3], '#mashPotatoes').then(()=>{
         addFood(mashPotatoes[4], '#mashPotatoes').then(()=>{
+          document.querySelector("#table").innerHTML += '<img alt="mashPotatoe" src="./public/images/mashPotatoes.jpg" width="100%" />'
         })
       })
     })
@@ -42,22 +44,20 @@ addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
 
 // Iteration 3 using async and await
 
-//async function makeFood(step) {
-//  // ... your code here
-// 
-//  for(step = 0; step < brusselSprouts.lenght; step++){
-//
-//  await addFood(brusselSprouts[step], '#brusselSprouts')
-//  }
-//  
-//  
-// 
-//}
-//makeFood(brusselSprouts);
+
 async function makeFood(steps) {
   // your code here…
   for (let i = 0; i<steps.length; i++){
-    await addFood(steps[i], '#brusselSprouts')
+    await addFood(steps[i], '#brusselSprouts');
   }
+  document.querySelector("#table").innerHTML += '<img alt="brussel" src="./public/images/brusselSprouts.jpg" width="100%" >'
 }
 makeFood(brusselSprouts);
+
+//var p1 = addFood(mashPotatoes[4],steak[7],brusselSprouts[8])
+//var p2 = steak[7];
+//var p3 = brusselSprouts[8];
+//
+//Promise.all([mashPotatoes,steak,brusselSprouts]).then(()=>{
+//  alert("Dinner is served.");
+//})
