ae274cb6c8cfb5f40bd735cde853192671570114
Elisa Chen <skechen@gmail.com>
2020-11-30 17:52:00 +0100

    lab and bonus 1 completed

--- public/javascripts/addFood.js
+++ public/javascripts/addFood.js
@@ -1,13 +1,13 @@
 /* CALL THIS FUNCTION FROM OTHER FILES LIKE */
 
-/****** DO NOT TOUCH vvv *****/
+/****** DO NOT TOUCH vvv ****/
 
 function addFood(step, id, callback) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      document.querySelector(id).innerHTML += `<li>${step}</li`;
+      document.querySelector(id).innerHTML += `<li>${step}</li>`;
       resolve(step);
-      callback ? callback() : ''; // For iteration one
+      callback ? callback() : ""; // For iteration one
     }, Math.floor(Math.random() * 1000));
   });
 }
