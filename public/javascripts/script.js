4c1cff6ea5842c3b0cfadb57ff74ddac35dd5892
Ann Lampa <riann08@yahoo.com>
2020-11-30 17:41:08 +0100

    lab done sans bonus

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -10,22 +10,47 @@ for (let i = 0; i < mashPotatoes.length; i++) {
 addFood(steak[0], '#steak', () => {
   // ... your code here
   addFood(steak[1], '#steak', () => {
-
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
+              })
+            })
+          })
+        })
+      })
+    })
   })
 });
 
 
 
 // Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
+addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
   // ... your code here
+  addFood(mashPotatoes[0], '#mashPotatoes')
   addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[3], '#mashPotatoes')
+  addFood(mashPotatoes[4], '#mashPotatoes')
+  
 });
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
-  }
-  makeFood(eachStep);
+async function makeFood(step, id) {
+  for(let i=0; i<step.length; i++){
+    await addFood(step[i],id);
+ 
+    }
+
+  //     await addFood(brusselSprouts[0], '#brusselSprouts')
+  //     await addFood(brusselSprouts[1], '#brusselSprouts')
+  //     await addFood(brusselSprouts[2], '#brusselSprouts')
+  //     await addFood(brusselSprouts[3], '#brusselSprouts')
+  //     await addFood(brusselSprouts[4], '#brusselSprouts')
+  //     await addFood(brusselSprouts[5], '#brusselSprouts')
+};
+
+   makeFood(brusselSprouts, "#brusselSprouts");
