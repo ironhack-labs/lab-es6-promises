afb684190fc5e35f02d304cc4460b5ae19dd9adf
yuuu.miao@gmail.com <yuuumiao@miyaas.home>
2020-12-01 01:36:57 +0100

    123done

--- public/javascripts/addFood.js
+++ public/javascripts/addFood.js
@@ -8,7 +8,17 @@ function addFood(step, id, callback) {
       document.querySelector(id).innerHTML += `<li>${step}</li`;
       resolve(step);
       callback ? callback() : ''; // For iteration one
-    }, Math.floor(Math.random() * 1000));
+    }, 1000);
   });
 }
 /***** ^^^ DO NOT TOUCH *****/
+
+// function addFood(step, id, callback) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       document.querySelector(id).innerHTML += `<li>${step}</li`;
+//       resolve(step);
+//       callback ? callback() : ''; // For iteration one
+//     }, Math.floor(Math.random() * 1000));
+//   });
+// }
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,82 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
-}
+// for (let i = 0; i < mashPotatoes.length; i++) {
+//   //addFood(steak[i], '#steak');
+//   console.log(mashPotatoes[i])
+// }
+const tzero = Date.now();
+
 
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
-  addFood(steak[1], '#steak', () => {
+function loadSteak() {
+  addFood(steak[0], '#steak', () => {
+    // ... your code here
+    addFood(steak[1], '#steak', () => {
 
+      addFood(steak[2], '#steak', () => {
+
+        addFood(steak[3], '#steak', () => {
+
+          addFood(steak[4], '#steak', () => {
+
+            addFood(steak[5], '#steak', () => {
+
+              addFood(steak[6], '#steak', () => {
+
+                addFood(steak[7], '#steak', () => {
+
+                  document.getElementById("table").innerHTML += `<img src='./public/images/steak.jpg'>`
+                })
+              })
+            })
+          })
+        })
+      })
+    })
   })
-});
+}
 
+// async function loadPicSteak() {
+//   await loadListSteak();
+//   await document.getElementById("table").innerHTML += `<img src='./public/images/steak.jpg'>`;
+// }
 
+// loadPicSteak();
 
 // Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
-});
+function loadPotatoes() {
+  addFood(mashPotatoes[0], '#mashPotatoes')
+    .then(() => addFood(mashPotatoes[1], '#mashPotatoes'))
+    .then(() => addFood(mashPotatoes[2], '#mashPotatoes'))
+    .then(() => addFood(mashPotatoes[3], '#mashPotatoes'))
+    .then(() => addFood(mashPotatoes[4], '#mashPotatoes'))
+    .then(() => {
+      document.getElementById("table").innerHTML += `<img src='./public/images/mashPotatoes.jpg'>`;
+    });
+}
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
+async function makeFood(toto) {
+  // ... your code here
+  try {
+    for (let i = 0; i < toto.length; i++) {
+      await addFood(toto[i], '#brusselSprouts')
+      // console.log('iteration3)')
+      // console.log(Date.now() - tzero);
+    }
+  } catch (err) {
+    throw new Error("error")
   }
-  makeFood(eachStep);
+}
+
+
+function loadSprouts() {
+  makeFood(brusselSprouts).then(() => {
+    // console.log('then');
+    // console.log(Date.now() - tzero);
+    const place = document.getElementById("table")
+    place.innerHTML += `<img src='./public/images/brusselSprouts.jpg'>`;
+  })
+}
