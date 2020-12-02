fc5531c6823537fc331a1e3c14b0471a90488a6c
Jacqueline <j.jumaga@gmail.com>
2020-12-02 17:55:56 +0100

    done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,61 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
+/*£for (let i = 0; i < mashPotatoes.length; i++) {
   //addFood(steak[i], '#steak');
   console.log(mashPotatoes[i])
-}
+}*/
 
+let table = document.getElementById("table");
+let audio = document.getElementById("dinner");
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
-  addFood(steak[1], '#steak', () => {
-
-  })
+addFood(steak[0], "#steak", () => {
+  addFood(steak[1], "#steak", () => {
+    addFood(steak[2], "#steak", () => {
+      addFood(steak[3], "#steak", () => {
+        addFood(steak[4], "#steak", () => {
+          addFood(steak[5], "#steak", () => {
+            addFood(steak[6], "#steak", () => {
+              addFood(steak[7], "#steak", () => {});
+              table.innerHTML += `<img src="./public/images/steak.jpg" alt="Steak image"/>`;
+            });
+          });
+        });
+      });
+    });
+  });
 });
 
-
-
-// Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+// Iteration 2 using .then()
+addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
+  addFood(mashPotatoes[1], "#mashPotatoes").then(() => {
+    addFood(mashPotatoes[2], "#mashPotatoes").then(() => {
+      addFood(mashPotatoes[3], "#mashPotatoes").then(() => {
+        addFood(mashPotatoes[4], "#mashPotatoes").then(() => {});
+      });
+      table.innerHTML += `<img src="./public/images/mashPotatoes.jpg" alt="Potatoes image"/>`;
+    });
+  });
 });
-
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
+async function makeFood(step, id) {
+  try {
+    for (let i = 0; i < brusselSprouts.length; i++) {
+      await addFood(step[i], id);
+    }
+  } catch (err) {
+    console.error(err);
+  } finally {
+    table.innerHTML += `<img src="./public/images/brusselSprouts.jpg" alt="Brussel sprouts image"/>`;
+    document.body.innerHTML += `<button> Dinner is served ! </button>`;
+    audio.play();
   }
-  makeFood(eachStep);
+}
+makeFood(brusselSprouts, "#brusselSprouts");
+
+/*document.getElementById('myAudioTagID').play();
+
+//play audio with out html audio tag
+var myAudio = new Audio('my_great_song.mp3');
+myAudio.play();*/
