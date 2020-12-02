3d9309bcea80803303a36f44ad0f69e36e4aa02b
Intan Paoli <intankhairunnisa@gmail.com>
2020-12-02 17:50:49 +0100

    first commit

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,30 +2,70 @@
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
 for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
   console.log(mashPotatoes[i])
 }
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
-  // ... your code here
+  
   addFood(steak[1], '#steak', () => {
 
+    addFood(steak[2], '#steak', () => {
+
+      addFood(steak[3], '#steak', () => {
+
+        addFood(steak[4], '#steak', () => {
+        
+          addFood(steak[5], '#steak', () => {
+        
+            addFood(steak[6], '#steak', () => {
+
+              addFood(steak[7], '#steak', () => {
+      
+              }) 
+      
+            })   
+          })
+        })
+      })
+    })
   })
 });
 
 
-
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
+   
   addFood(mashPotatoes[1], '#mashPotatoes')
+
+  addFood(mashPotatoes[2], '#mashPotatoes')
+
+  addFood(mashPotatoes[4], '#mashPotatoes')
+
+  addFood(mashPotatoes[3], '#mashPotatoes')
 });
 
 // Iteration 3 using async and await
+const newImage1 = document.createElement("img");
+image.src = "../images/brusselSprouts.jpg";
+image2.src ="../images/mashPotatoes.jpg";
+image3.src ="../images/steak.jpg";
+document.querySelector('#table').appendChild(image);
+document.querySelector('#table').appendChild(image2);
+document.querySelector('#table').appendChild(image3);
 
-  async function makeFood(step) {
-    // ... your code here
-    
-  }
-  makeFood(eachStep);
+
+  async function makeFood(step, id, picname) {
+    for(let i=0; i<step.length; i++){
+      await addFood(step[i],id,picname[i]);
+    }
+  };
+
+makeFood(brusselSprouts,"#brusselSprouts","#table");
+
+
+//Bonus 1
+
+
+
+
