46a7e893204105118dcb60b69d7040c720ed0317
RFeldman84 <rachel.feldman84@gmail.com>
2020-12-12 15:26:01 -0500

    finish iterations & bonus, can't get sound file to play

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -15,35 +15,35 @@
 // });
 const table = document.querySelector('#table')
 
-function steaks() { addFood(steak[0], '#steak', () => {
-  console.log(steak[0])
-  addFood(steak[1], '#steak', () => {
-    console.log(steak[1])
-    addFood(steak[2], '#steak', () => {
-      console.log(steak[2])
-      addFood(steak[3], '#steak', () => {
-        console.log(steak[3])
-        addFood(steak[4], '#steak', () => {
-          console.log(steak[4])
-          addFood(steak[5], '#steak', () => {
-            console.log(steak[5])
-            addFood(steak[6], '#steak', () => {
-              console.log(steak[6])
-              addFood(steak[7], '#steak', () => {
-                console.log(steak[7])
-               
-                const steakImg = document.createElement('img')
-                steakImg.src = 'public/images/steak.jpg'
-                table.appendChild(steakImg)
-              });
-            });
-          });
-        });
-      });
-    });
-  });
-});
-}
+
+//  addFood(steak[0], '#steak', () => {
+//   console.log(steak[0])
+//   addFood(steak[1], '#steak', () => {
+//     console.log(steak[1])
+//     addFood(steak[2], '#steak', () => {
+//       console.log(steak[2])
+//       addFood(steak[3], '#steak', () => {
+//         console.log(steak[3])
+//         addFood(steak[4], '#steak', () => {
+//           console.log(steak[4])
+//           addFood(steak[5], '#steak', () => {
+//             console.log(steak[5])
+//             addFood(steak[6], '#steak', () => {
+//               console.log(steak[6])
+//               addFood(steak[7], '#steak', () => {
+//                 console.log(steak[7])
+//                 const steakImg = document.createElement('img')
+//                 steakImg.src = 'public/images/steak.jpg'
+//                 table.appendChild(steakImg)
+//               });
+//             });
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
+
 
 
 // Iteration 2 using `.then()`
@@ -53,78 +53,79 @@ function steaks() { addFood(steak[0], '#steak', () => {
 // });
 
  
-function mashP(){ addFood(mashPotatoes[0], '#mashPotatoes')
-.then(() => {
-  return addFood(mashPotatoes[1], '#mashPotatoes')
-})
-.then(()=>{
-  return addFood(mashPotatoes[2], '#mashPotatoes')
-})
-.then(()=>{
-  return addFood(mashPotatoes[3], '#mashPotatoes')
-})
-.then(()=>{
-  return addFood(mashPotatoes[4], '#mashPotatoes'); 
-})
-.then(()=>{
-  
-  const potatoImg = document.createElement('img')
-  potatoImg.src = 'public/images/mashPotatoes.jpg'
-   table.appendChild(potatoImg)
-})
-.catch((err) => console.log(err));
-}
+// addFood(mashPotatoes[0], '#mashPotatoes')
+// .then(() => {
+//   return addFood(mashPotatoes[1], '#mashPotatoes')
+// })
+// .then(()=>{
+//   return addFood(mashPotatoes[2], '#mashPotatoes')
+// })
+// .then(()=>{
+//   return addFood(mashPotatoes[3], '#mashPotatoes')
+// })
+// .then(()=>{
+//   return addFood(mashPotatoes[4], '#mashPotatoes'); 
+// })
+// .then(()=>{
+//   const potatoImg = document.createElement('img')
+//   potatoImg.src = 'public/images/mashPotatoes.jpg'
+//    table.appendChild(potatoImg)
+// })
+// .catch((err) => console.log(err));
 
 
 
 // // Iteration 3 using async and await
 
-  async function makeFood(step, id) {
-    for(i=0;i<step.length; i++)
-     await addFood(step[i], id)
-    
+  async function makeFood(step, id, name) {
+    for(i=0;i<step.length; i++){
+      await addFood(step[i], id)
+    }
+
+     let img = document.createElement('img')
+     img.src = `public/images/${name}.jpg` 
+     table.append(img)
   }
 
-  // makeFood(mashPotatoes, '#mashPotatoes');
-  // makeFood(steak, '#steak');
-  makeFood(brusselSprouts, '#brusselSprouts');
-
-
-
-
-
-// async function makeFood(){
-//   try {
-//    const makePotatoes = await mashP();
-//    const makeSteak = await steaks() 
+  // makeFood(brusselSprouts, '#brusselSprouts', "brusselSprouts")
+  // makeFood(mashPotatoes, '#mashPotatoes', "mashPotatoes");
+  // makeFood(steak, '#steak', "steak");
  
-//   } catch (err) {
-//     console.log(err);
-//   }
- 
+  
+// only 3 length??
 
-// }
 
-//makeFood();
+
+
+
+async function meal(){
+  
+ const makeMash =  await makeFood(mashPotatoes, '#mashPotatoes', "mashPotatoes");
+ const makeSteak =  await makeFood(steak, '#steak', "steak");
+ const makeSprout = await makeFood(brusselSprouts, '#brusselSprouts', "brusselSprouts");
+ }
+
+
+//makeFood(eachStep)
 
 function doneButton(){
+  const table = document.querySelector('#table')
   const alertButton = document.createElement("button")
   alertButton.innerHTML = 'Dinner is served.'
   table.appendChild(alertButton)
 }
 
-  // const brusselsImg = document.createElement('img')
-  // brusselsImg.src = 'public/images/brusselSprouts.jpg'
-  // table.appendChild(brusselsImg)
+/// sound not working anuy way i try
+// const sound = document.createElement("audio")
+// sound.src =  "public/media/dinnerIsServed.mp3"
+// sound.play()
+/// in html why not??  <audio id="serveSound" src="public/media/dinnerIsServed.mp3"></audio>
 
-  // Promise.all([makeFood()])
-  // .then(() => {
-  //   doneButton()
-  // })
-  // .catch((err) => console.log(err));
- 
-  Promise.all([])
+
+  Promise.all([meal()])
   .then(() => {
-    
+    doneButton()
+   
+    // sound!!!
   })
   .catch((err) => console.log(err));
