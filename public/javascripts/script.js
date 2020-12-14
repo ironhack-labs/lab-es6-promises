29834e315cefd77ff18feddc3d2d9ba24e363c23
BadGuerl <mi_ailis@hotmail.com>
2020-12-14 16:06:51 +0100

    done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -15,6 +15,7 @@ addFood(steak[0], '#steak', () => {
         addFood(steak[4], '#steak', () => {
           addFood(steak[5],'#steak', () => {
             addFood(steak[6], '#steak', () =>{});
+            table.innerHTML += `<img src="./public/images/steak.jpg">`;
           })
         })
       })
@@ -26,19 +27,36 @@ addFood(steak[0], '#steak', () => {
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
   // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
-    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
-      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
-        addFood(mashPotatoes[4], '#mashPotatoes').then(() =>{});
-      })
-    })
-  })
+  addFood(mashPotatoes[1], '#mashPotatoes')
+  .then(() => addFood(mashPotatoes[2], '#mashPotatoes'))
+  .then(() => addFood(mashPotatoes[3], '#mashPotatoes'))
+  .then(() => addFood(mashPotatoes[4], '#mashPotatoes'))
+  .then(() => document.getElementById('table').innerHTML);
+  table.innerHTML += `<img src="./public/images/mashPotatoes.jpg">`;
 });
+
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
+  async function makeFood(step, id) {
     // ... your code here
+   try {
+     for (let i = 0; i < brusselSprouts.length; i++) {
+       await addFood(step[i], id);
+     }
+   } catch (err) {
+     console.log(err);
+   } finally {
+     table.innerHTML += `<img src="./public/images/brusselSprouts.jpg">`;
+     document.body.innerHTML += `<button> Dinner is served! </button>`;
+     btn = document.querySelector("button");
+     btn.addEventListener("click", () => {
+       let audio = new Audio("./public/media/dinnerIsServed.mp3");
+       audio.play();
+     });
+    }
     
-  }
-  makeFood(eachStep);
+     makeFood(eachStep);
+   }
+  
+  makeFood(brusselSprouts, "#brusselSprouts");
