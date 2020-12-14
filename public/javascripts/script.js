c2ccc050218ae83f58d450c922314f06de0f3ef3
aguiluxo91 <apuertadelaguila@gmail.com>
2020-12-14 18:11:21 +0100

    done with bonus + audio

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,13 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
+/* for (let i = 0; i < mashPotatoes.length; i++) {
   //addFood(steak[i], '#steak');
   console.log(mashPotatoes[i])
-}
+} */
 
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
+const promise1 = addFood(steak[0], '#steak', () => {
   // ... your code here
   addFood(steak[1], '#steak', () => {
     addFood(steak[2], '#steak', () => {
@@ -28,7 +28,7 @@ addFood(steak[0], '#steak', () => {
 
 
 // Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
+const promise2 = addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
   // ... your code here
   addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
     addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
@@ -41,10 +41,36 @@ addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
   })
 });
 
+
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
+async function makeFood(step) {
     // ... your code here
-    
+    for (let i = 0; i < step.length; i++) {
+      await addFood(step[i], "#brusselSprouts");
+    }
   }
-  makeFood(eachStep);
+const promise3 = makeFood(brusselSprouts);
+let steakImg = new Image();
+steakImg.src = 'public/images/steak.jpg';
+document.getElementById('table').appendChild(steakImg);
+let potatoImg = new Image();
+potatoImg.src = 'public/images/mashPotatoes.jpg';
+document.getElementById('table').appendChild(potatoImg);
+let sproutsImg = new Image();
+sproutsImg.src = 'public/images/brusselSprouts.jpg';
+document.getElementById('table').appendChild(sproutsImg);
+
+Promise.all([promise1, promise2, promise3]).then(() => {
+  document.querySelector("body").innerHTML +=
+    `<button id="btn">Dinner is served!</button>`;
+    document.getElementById('btn').addEventListener('click', audio);
+})
+
+
+const dinnerSound = new Audio();
+dinnerSound.src = 'public/media/dinnerIsServed.mp3';
+
+function audio () {
+  dinnerSound.play();
+}
