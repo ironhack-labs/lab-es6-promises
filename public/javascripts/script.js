5b69d917fffdbdaf2e5f012dbc73d1319e464047
JuanBerrondo <bega2000@protonmail.com>
2020-12-14 16:19:12 +0100

    It 1,2,3 with bug on image head

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -10,22 +10,51 @@ for (let i = 0; i < mashPotatoes.length; i++) {
 addFood(steak[0], '#steak', () => {
   // ... your code here
   addFood(steak[1], '#steak', () => {
-
-  })
+    addFood(steak[2], '#steak', () => {
+        addFood(steak[3], '#steak', () => {
+          addFood(steak[4], '#steak', () => {
+            addFood(steak[5], '#steak', () => {
+              addFood(steak[6], '#steak', () => {
+                addFood(steak[7], '#steak', () => {
+                  let imageSteak = document.createElement('img');
+                  imageSteak.src = "./public/images/steak.jpg"
+                  document.getElementById('table').appendChild(imageSteak);
+                });
+              });
+            });
+          });
+        });
+      });
+  });
 });
 
 
-
+/*
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
   // ... your code here
   addFood(mashPotatoes[1], '#mashPotatoes')
+
+  addFood(mashPotatoes[2], '#mashPotatoes')
+  addFood(mashPotatoes[3], '#mashPotatoes')
+  addFood(mashPotatoes[4], '#mashPotatoes')
+  let imageSmash = document.createElement('img');
+  imageSmash.src = "./public/images/mashPotatoes.jpg";
+    document.getElementById('table').appendChild(imageSmash);
 });
 
 // Iteration 3 using async and await
 
   async function makeFood(step) {
     // ... your code here
-    
-  }
-  makeFood(eachStep);
+    for (let i = 0; i < step.length; i++) {
+        await addFood(step[i], "#brusselSprouts");
+      }
+    }
+    let imageBrussel = document.createElement('img');
+    imageBrussel.setAttribute("src", "public/images/brusselSprouts.jpg");
+   document.getElementById('table').appendChild(imageBrussel);
+  
+  makeFood(brusselSprouts);
+
+    */
