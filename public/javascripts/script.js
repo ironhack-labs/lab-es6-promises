8f6722477788d715919656ab786d86edd24c5ed4
MkAlba <albacastello74@gmail.com>
2020-12-15 15:48:16 +0100

    feat: iteration 2

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,3 @@
-// This will print in the wrong order
-// we added it for you to test to make sure data is loaded
-// 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-//for (let i = 0; i < mashPotatoes.length; i++) {
-//  addFood(steak[i], '#steak');
- /// console.log(mashPotatoes[i])
-//}
-
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
   addFood(steak[1], '#steak', () => {
@@ -19,21 +11,26 @@ addFood(steak[0], '#steak', () => {
             })
           })
         })
-      }
+      })
     })
   })
 });
 
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
+        })
+      })
+    })
+  })
 });
 
 // Iteration 3 using async and await
+  async function makeFood(step,id) {
+   
 
-  async function makeFood(step) {
-    // ... your code here
-    
   }
   makeFood(eachStep);
