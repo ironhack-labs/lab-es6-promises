74bcee0fb0de4ed78530550a18901c69b4d926e3
Kleber Bastos <bastoskp@hotmail.com>
2021-01-12 09:41:09 -0300

    done 2 iterations

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,29 +3,45 @@
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
 for (let i = 0; i < mashPotatoes.length; i++) {
   //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
+  console.log(mashPotatoes[i]);
 }
 
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
-  addFood(steak[1], '#steak', () => {
-
-  })
+addFood(steak[0], "#steak", () => {
+  addFood(steak[1], "#steak", () => {
+    addFood(steak[2], "#steak", () => {
+      addFood(steak[3], "#steak", () => {
+        addFood(steak[4], "#steak", () => {
+          addFood(steak[5], "#steak", () => {
+            addFood(steak[6], "#steak", () => {
+              addFood(steak[7], "#steak", () => {
+                document.getElementById("table").innerHTML +=
+                  '<img src="public/images/steak.jpg" />';
+              });
+            });
+          });
+        });
+      });
+    });
+  });
 });
 
-
-
 // Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
+  addFood(mashPotatoes[1], "#mashPotatoes");
+  addFood(mashPotatoes[2], "#mashPotatoes").then(() => {
+    addFood(mashPotatoes[3], "#mashPotatoes").then(() => {
+      addFood(mashPotatoes[4], "#mashPotatoes").then(() => {
+        document.getElementById("table").innerHTML +=
+          '<img src="public/images/mashPotatoes.jpg" />';
+      });
+    });
+  });
 });
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
-  }
-  makeFood(eachStep);
+async function makeFood(step) {
+  // ... your code here
+}
+makeFood(eachStep);
