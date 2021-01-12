531278c210499c20aeb3cd6658640388c7b0d559
digomattar21 <digomattar@gmail.com>
2021-01-12 20:58:07 -0300

    done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,15 +1,31 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
-}
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
   // ... your code here
   addFood(steak[1], '#steak', () => {
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
+                document.getElementById('table').innerHTML +=`<img src="./public/images/steak.jpg">`;
+
+
+              })
+
+            })
+
+          })
+
+        })
+
+      })
+
+    })
 
   })
 });
@@ -19,13 +35,35 @@ addFood(steak[0], '#steak', () => {
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
   // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then (()=>{
+
+    addFood(mashPotatoes[2], '#mashPotatoes').then (()=>{
+      addFood(mashPotatoes[3], '#mashPotatoes').then (()=>{
+        addFood(mashPotatoes[4], '#mashPotatoes').then (()=>{
+          document.getElementById('table').innerHTML += `<img src='./public/images/mashPotatoes.jpg'>`;
+        })
+      })
+    })
+
+  })
 });
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
-  }
-  makeFood(eachStep);
+    async function makeFood(step,id){
+      try{
+        for (let i=0;i<brusselSprouts.length;i++){
+          await addFood(step[i],id);
+        }
+
+      } catch (err){
+        console.log(err);
+      } finally {
+        document.getElementById('table').innerHTML += `<img src='./public/images/brusselSprouts.jpg'>`;
+        setTimeout(()=>{alert('Dinner is served')},100)
+      }
+ 
+    }
+
+  makeFood(brusselSprouts,'#brusselSprouts');
+  
