a48c846329469f9934668a07c266926a08fc751c
paulobart <paulobart@outlook.com>
2021-01-13 02:05:40 +0000

    falta a interacao 3, pois não funcionou

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -10,7 +10,19 @@ for (let i = 0; i < mashPotatoes.length; i++) {
 addFood(steak[0], '#steak', () => {
   // ... your code here
   addFood(steak[1], '#steak', () => {
-
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
+                document.getElementById('table').innerHTML +=`<img src="./public/images/steak.jpg">`;
+              })
+            })
+          })
+        })
+      })
+    })
   })
 });
 
@@ -18,14 +30,28 @@ addFood(steak[0], '#steak', () => {
 
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
+          document.getElementById('table').innerHTML += `<img src='./public/images/mashPotatoes.jpg'>`;
+        })
+      })       
+    }) 
+  })
 });
 
 // Iteration 3 using async and await
 
   async function makeFood(step) {
-    // ... your code here
-    
+    await addFood(brusselSprouts[0], '#brusselSprouts');
+    await addFood(brusselSprouts[1], '#brusselSprouts');
+    await addFood(brusselSprouts[3], '#brusselSprouts');
+    await addFood(brusselSprouts[4], '#brusselSprouts');
+    await addFood(brusselSprouts[5], '#brusselSprouts');
+    await addFood(brusselSprouts[6], '#brusselSprouts');
+    await addFood(brusselSprouts[7], '#brusselSprouts');
+    await addFood(brusselSprouts[8], '#brusselSprouts');
+    await addFood(brusselSprouts[9], '#brusselSprouts');
   }
   makeFood(eachStep);
