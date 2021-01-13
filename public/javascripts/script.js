b4fea8e45182f22366caeafa894cb9ba6a79a397
Volney Faustini <volney.faustini@gmail.com>
2021-01-12 22:36:02 -0300

    done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,67 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
-}
+
+// for (let i = 0; i < mashPotatoes.length; i++) {
+//   //addFood(steak[i], '#steak');
+//   console.log(mashPotatoes[i])
+// }
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
-  // ... your code here
+
   addFood(steak[1], '#steak', () => {
 
+    addFood(steak[2], '#steak', () => {
+
+      addFood(steak[3], '#steak', () => {
+
+        addFood(steak[4], '#steak', () => {
+
+          addFood(steak[5], '#steak', () => {
+
+            addFood(steak[6], '#steak', () => {
+
+              addFood(steak[7], '#steak', () => {
+
+              })
+            })
+          })
+        })
+      })
+    })
   })
 });
 
 
 
 // Iteration 2 using `.then()`
+console.log("ta funciona?")
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
+          addFood(mashPotatoes[5], '#mashPotatoes').then(() => {
+            addFood(mashPotatoes[6], '#mashPotatoes').then(() => {
+              addFood(mashPotatoes[7], '#mashPotatoes').then(() => {
+
+              })
+            })
+          })
+        })
+      })
+    })
+  })
 });
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
+async function makeFood(step) {
+
+  for (let i=0; i<step.length; i+=1) {
+  await addFood(step[i], `#brusselSprouts`)
   }
-  makeFood(eachStep);
+}
+makeFood(brusselSprouts);
