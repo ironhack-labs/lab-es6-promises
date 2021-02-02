0e48b14846e01ca625e6f2a78476286333448856
Luis <luisc8934@gmail.com>
2021-02-02 09:27:09 -0600

    done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,9 +8,26 @@ for (let i = 0; i < mashPotatoes.length; i++) {
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
-  // ... your code here
+  // ... your code here  
   addFood(steak[1], '#steak', () => {
 
+    addFood(steak[2], '#steak', () => {
+
+      addFood(steak[3], '#steak', () => {
+
+        addFood(steak[4], '#steak', () => {
+
+          addFood(steak[5], '#steak', () => {
+
+            addFood(steak[6], '#steak', () => {
+
+              addFood(steak[7], '#steak', () => {
+              })
+            })
+          })
+        })
+      })
+    })
   })
 });
 
@@ -18,14 +35,39 @@ addFood(steak[0], '#steak', () => {
 
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
+
   // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then(()=>{
+   
+    addFood(mashPotatoes[2], '#mashPotatoes').then (()=>{
+
+      addFood(mashPotatoes[3], '#mashPotatoes').then(()=>{
+
+        addFood(mashPotatoes[4], '#mashPotatoes')
+
+      })
+    })
+  })
+  
+ 
+ 
 });
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
-  }
-  makeFood(eachStep);
+async function makeFood(step) {
+  let eachStep = [...brusselSprouts]
+  console.log(eachStep)
+  
+    async function makeFood(steps) {
+      // ... your code here
+  
+        for (let step of steps) {
+          await addFood(step, '#brusselSprouts')
+        }
+    let sproutsImg = document.createElement('img');
+    sproutsImg.setAttribute('src', 'public/images/brusselSprouts.jpg')
+    sproutsImg.setAttribute('alt', 'brussels sprout')
+    document.getElementById('table').appendChild(sproutsImg)
+    }
+    makeFood(eachStep);
