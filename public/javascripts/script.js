010422b069da0c02f45b0c880430e5f747e1cca0
Daniela Alonzo <monsterjunior@outlook.es>
2021-02-01 21:20:12 -0600

    working in bonus

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,59 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
+//for (let i = 0; i < mashPotatoes.length; i++) {
   //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
-}
+//  console.log(mashPotatoes[i])
+//}
+const areaImg=document.querySelector("#table")
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
-  // ... your code here
   addFood(steak[1], '#steak', () => {
-
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak')
+              areaImg.innerHTML += `<img src="./public/images/steak.jpg"/>`
+            })
+          })
+        })
+      })
+    })
   })
 });
 
-
-
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
   addFood(mashPotatoes[1], '#mashPotatoes')
-});
+}).then(()=>{
+  addFood(mashPotatoes[2], '#mashPotatoes')
+}).then(()=>{
+  addFood(mashPotatoes[3], '#mashPotatoes')
+}).then(()=>{
+  addFood(mashPotatoes[4], '#mashPotatoes')
+}).then(()=>{
+  console.log(areaImg)
+  return areaImg.innerHTML += `<img src="./public/images/mashPotatoes.jpg"/>`
+})
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
+async function makeFood(step) {
+  for (index = 0; index < step.length; index++) {
+      await addFood(step[index], '#brusselSprouts')
   }
-  makeFood(eachStep);
+  return areaImg.innerHTML += `<img src="./public/images/brusselSprouts.jpg"/>`
+}
+makeFood(brusselSprouts);
+///////////////////////////////
+table.appendChild()
+Promise.all([
+      makeFood(mashPotatoes),
+      makeFood(steak),
+      makeFood(brusselSprouts)
+  ]).then(()=>{
+    alert("Dinner is served!")
+  })
