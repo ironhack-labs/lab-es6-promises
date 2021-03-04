29889b1522af0b164320c2cb1b319ec93a29f309
cintiathieme <cintiathieme@gmail.com>
2021-03-04 20:55:02 -0300

    done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,25 +7,71 @@ for (let i = 0; i < mashPotatoes.length; i++) {
 }
 
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
-  addFood(steak[1], '#steak', () => {
+const table = document.getElementById("table")
 
+const addSteak = () => {addFood(steak[0], '#steak', () => {
+  addFood(steak[1], '#steak', () => {
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
+                const imagem = document.createElement("img")
+                imagem.src ="./public/images/steak.jpg"
+                table.appendChild(imagem)
+              })  
+            })
+          })
+        })
+      })
+    })
   })
-});
+})
+};
 
 
 
 // Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
-});
+const addMashPotatoes = () => {addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
+          const imagem = document.createElement("img")
+          imagem.src ="./public/images/mashPotatoes.jpg"
+          table.appendChild(imagem)
+        })
+      })
+    })
+  })
+})
+};
+
 
 // Iteration 3 using async and await
 
   async function makeFood(step) {
-    // ... your code here
-    
+    for (i = 0; i < step.length; i++) {
+      await addFood(step[i], `#brusselSprouts`)
+    }
+    const imagem = document.createElement("img")
+    imagem.src ="./public/images/brusselSprouts.jpg"
+    table.appendChild(imagem)
   }
-  makeFood(eachStep);
+  
+
+// bônus 2
+
+Promise.all([addSteak(), addMashPotatoes(), makeFood(brusselSprouts)]).then(() => {
+  const steps = document.getElementById("steps")
+  const button = document.createElement("button")
+  button.innerHTML = "Dinner is served"
+  steps.appendChild(button)
+})
+
+
+
+
+
+
