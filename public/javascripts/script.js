64879fd39e38175abe81735ae25b682432c8bc6d
deboranis <dc.nisen@hotmail.com>
2021-03-04 21:08:59 -0300

    done with first bonus

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,66 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
-}
+// for (let i = 0; i < mashPotatoes.length; i++) {
+//   //addFood(steak[i], '#steak');
+//   console.log(mashPotatoes[i])
+// }
+
+const tabela = document.getElementById("table");
 
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
-  addFood(steak[1], '#steak', () => {
-
-  })
+addFood(steak[0], "#steak", () => {
+  addFood(steak[1], "#steak", () => {
+    addFood(steak[2], "#steak", () => {
+      addFood(steak[3], "#steak", () => {
+        addFood(steak[4], "#steak", () => {
+          addFood(steak[5], "#steak", () => {
+            addFood(steak[6], "#steak", () => {
+              addFood(steak[7], "#steak", () => {
+                const imagem = document.createElement("img");
+                tabela.appendChild(imagem);
+                console.log(tabela);
+                tabela
+                  .querySelector("img:last-child")
+                  .setAttribute("src", "./public/images/steak.jpg");
+              });
+            });
+          });
+        });
+      });
+    });
+  });
 });
 
-
-
 // Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
+  addFood(mashPotatoes[1], "#mashPotatoes").then(() => {
+    addFood(mashPotatoes[2], "#mashPotatoes").then(() => {
+      addFood(mashPotatoes[3], "#mashPotatoes").then(() => {
+        addFood(mashPotatoes[4], "#mashPotatoes").then(() => {
+          const imagem = document.createElement("img");
+          tabela.appendChild(imagem);
+          tabela
+            .querySelector("img")
+            .setAttribute("src", "./public/images/mashPotatoes.jpg");
+        });
+      });
+    });
+  });
 });
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
+async function makeFood(brusselSprouts) {
+  for (let i = 0; i < brusselSprouts.length; i++) {
+    await addFood(brusselSprouts[i], "#brusselSprouts");
   }
-  makeFood(eachStep);
+  if (brusselSprouts.length) {
+    const imagem = document.createElement("img");
+    tabela.appendChild(imagem);
+    tabela
+      .querySelector("img:last-child")
+      .setAttribute("src", "./public/images/brusselSprouts.jpg");
+  }
+}
+makeFood(brusselSprouts);
