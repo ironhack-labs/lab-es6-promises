69c08c156e667b1c5a0cacd0c8fd6bb5df743b9d
Paula Nicioli <paulanicioli@gmail.com>
2021-03-04 23:06:44 -0300

    Changes files to single quote vs. double quote

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,23 +2,23 @@
 
 function steakCallback(index) {
   if (index < 8) {
-    addFood(steak[index], "#steak", () => {
+    addFood(steak[index], '#steak', () => {
       steakCallback(index + 1);
     });
   }
 }
 
-addFood(steak[0], "#steak", () => {
+addFood(steak[0], '#steak', () => {
   steakCallback(1);
 });
 
 // Iteration 2 using `.then()`
 
-addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
-  addFood(mashPotatoes[1], "#mashPotatoes").then(() => {
-    addFood(mashPotatoes[2], "#mashPotatoes").then(() => {
-      addFood(mashPotatoes[3], "#mashPotatoes").then(() => {
-        addFood(mashPotatoes[4], "#mashPotatoes");
+addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[4], '#mashPotatoes');
       });
     });
   });
@@ -27,7 +27,7 @@ addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
 // Iteration 3 using async and await
 
 async function makeFood(step) {
-  await addFood(brusselSprouts[step], "#brusselSprouts");
+  await addFood(brusselSprouts[step], '#brusselSprouts');
   if (step < brusselSprouts.length - 1) {
     makeFood(step + 1);
   }
