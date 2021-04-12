40ea462a81c9033eca82de3d16785a78420f57a9
Marcia Faria <mfndnc@gmail.com>
2021-04-12 15:54:29 +0200

    done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,62 +8,73 @@
 
 // ***** callback hell javascript *****
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
-  addFood(steak[1], '#steak', () => {
-    // ... your code here
-    addFood(steak[2], '#steak', () => {
-      // ... your code here
-      addFood(steak[3], '#steak', () => {
-        // ... your code here
-        addFood(steak[4], '#steak', () => {
-          // ... your code here
-          addFood(steak[5], '#steak', () => {
-            // ... your code here
-            addFood(steak[6], '#steak', () => {
-              // ... your code here
-              addFood(steak[7], '#steak', () => {
-                console.log('steak iteractive finished');
-                document.querySelector('#table').innerHTML +=
-                  '<img src="public/images/steak.jpg" />';
-              });
-            });
-          });
-        });
-      });
-    });
-  });
-});
+// addFood(steak[0], '#steak', () => {
+//   // ... your code here
+//   addFood(steak[1], '#steak', () => {
+//     // ... your code here
+//     addFood(steak[2], '#steak', () => {
+//       // ... your code here
+//       addFood(steak[3], '#steak', () => {
+//         // ... your code here
+//         addFood(steak[4], '#steak', () => {
+//           // ... your code here
+//           addFood(steak[5], '#steak', () => {
+//             // ... your code here
+//             addFood(steak[6], '#steak', () => {
+//               // ... your code here
+//               addFood(steak[7], '#steak', () => {
+//                 console.log('steak iteractive finished');
+//                 document.querySelector('#table').innerHTML +=
+//                   '<img src="public/images/steak.jpg" />';
+//               });
+//             });
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
 
 // Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
-    // ... your code here
-    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
-      // ... your code here
-      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
-        // ... your code here
-        addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
-          // ... your code here
-          console.log('mashPotatoes iteractive finished');
-          document.querySelector('#table').innerHTML +=
-            '<img src="public/images/mashPotatoes.jpg" />';
-        });
-      });
-    });
-  });
-});
+// addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
+//   // ... your code here
+//   addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+//     // ... your code here
+//     addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+//       // ... your code here
+//       addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+//         // ... your code here
+//         addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
+//           // ... your code here
+//           console.log('mashPotatoes iteractive finished');
+//           document.querySelector('#table').innerHTML +=
+//             '<img src="public/images/mashPotatoes.jpg" />';
+//         });
+//       });
+//     });
+//   });
+// });
 
 // Iteration 3 using async and await
 
-async function makeFood(steps) {
+async function makeFood(steps, id) {
   for (let step of steps) {
-    await addFood(step, '#brusselSprouts');
+    await addFood(step, '#' + id);
     console.log({ step });
   }
   console.log('brusselSprouts iteractive finished');
-  document.querySelector('#table').innerHTML +=
-    '<img src="public/images/brusselSprouts.jpg" />';
+  document.querySelector(
+    '#table'
+  ).innerHTML += `<img src="public/images/${id}.jpg" />`;
 }
-makeFood(brusselSprouts);
+
+// #### BONUS 2 promise.all
+
+Promise.all([
+  makeFood(steak, 'steak'),
+  makeFood(mashPotatoes, 'mashPotatoes'),
+  makeFood(brusselSprouts, 'brusselSprouts'),
+]).then(() => {
+  // show the button
+  document.body.innerHTML += '<button>dinner is ready</button>';
+});
