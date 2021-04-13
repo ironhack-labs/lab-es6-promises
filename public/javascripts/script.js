c79dc9028f828f98b115eb60ee16f6a74f9aadc2
Jeroen van Bart <jeroenvanbart83@gmail.com>
2021-04-13 13:55:07 +0200

    iterations done bonus part needs more time

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,16 +1,27 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
-}
+// for (let i = 0; i < mashPotatoes.length; i++) {
+//   //addFood(steak[i], '#steak');
+//   console.log(mashPotatoes[i])
+// }
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
-  // ... your code here
   addFood(steak[1], '#steak', () => {
-
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              const image = document.createElement('img');
+                image.src  = '/public/images/steak.jpg';
+                document.querySelector('#table').appendChild(image)             
+            })
+          })
+        })
+      })
+    })
   })
 });
 
@@ -18,14 +29,29 @@ addFood(steak[0], '#steak', () => {
 
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
   addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[2], '#mashPotatoes')
+  addFood(mashPotatoes[3], '#mashPotatoes')
+  addFood(mashPotatoes[4], '#mashPotatoes')
+  const image = document.createElement('img');
+  image.src  = '/public/images/mashPotatoes.jpg';
+  document.querySelector('#table').appendChild(image)
 });
 
 // Iteration 3 using async and await
+function printPicture() {
+  const image = document.createElement('img');
+  image.src  = '/public/images/2brusselSprouts.jpg';
+  document.querySelector('#table').appendChild(image)
+  printPicture = function(){};
 
+};
+
+brusselSprouts.forEach(eachStep => {
   async function makeFood(step) {
-    // ... your code here
-    
-  }
-  makeFood(eachStep);
+  await addFood(step, '#brusselSprouts')  
+  printPicture()  
+}
+
+makeFood(eachStep);
+})
