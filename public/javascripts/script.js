b3a50d893102a260847dfc0f8093044596b3a2be
Mohamud Issa <mohamud.issa@ymail.com>
2021-04-29 16:47:30 +0200

    completed

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,16 +1,26 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
-// 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
-}
+// // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
+// for (let i = 0; i < mashPotatoes.length; i++) {
+  // addFood(steak[i], '#steak');
+//   console.log(mashPotatoes[i])
+// }
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
-  // ... your code here
   addFood(steak[1], '#steak', () => {
-
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {                
+              })
+            }) 
+          })
+        })
+      })
+    })
   })
 });
 
@@ -18,14 +28,40 @@ addFood(steak[0], '#steak', () => {
 
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[4], '#mashPotatoes')
+      });
+    });
+  });
 });
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
+async function makeFood(steps, id) {
+  
+  
+  const forLoop = function(steps, id){
+    steps.forEach((element, index) => {
+      addFood(steps[index], id)
+    });
   }
-  makeFood(eachStep);
+  
+
+  const sectionImg = document.querySelector('#table')
+  const img = document.createElement('img')
+  img.src = './public/images/brusselSprouts.jpg'
+
+  
+  const addImg = function addImg(){
+    sectionImg.appendChild(img)
+  };
+
+
+  forLoop(steps, id);
+  Const DrawImg = await addImg();
+
+}
+
+makeFood(brusselSprouts, '#brusselSprouts');
