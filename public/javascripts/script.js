5f0e6d074e00991a34e3c6a787ae58d7ccfeddc6
root <asifmahmoodjabbar@gmail.com>
2021-05-04 00:07:45 +0200

    Iteration 1 completed

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,7 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
+
 for (let i = 0; i < mashPotatoes.length; i++) {
   //addFood(steak[i], '#steak');
   console.log(mashPotatoes[i])
@@ -8,9 +9,20 @@ for (let i = 0; i < mashPotatoes.length; i++) {
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
-  // ... your code here
   addFood(steak[1], '#steak', () => {
-
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
+                document.getElementById('table').innerHTML += `<img src="public/images/steak.jpg"/>`
+              })
+            })
+          })
+        })
+      })
+    })
   })
 });
 
