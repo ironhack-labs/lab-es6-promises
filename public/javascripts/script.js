2297a761acd1adc89fdecb40ff2e0e0ad8b40d83
Lucia Sanchez <luci.sanchez.ortiz@gmail.com>
2021-06-04 10:31:39 +0200

    Iteration1

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,16 +1,34 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
+// for (let i = 0; i < mashPotatoes.length; i++) {
   //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
-}
+//   console.log(mashPotatoes[i])
+// }
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
-  // ... your code here
+  console.log(steak[0])
   addFood(steak[1], '#steak', () => {
-
+    console.log(steak[1])
+    addFood(steak[2], '#steak', () => {
+      console.log(steak[2])
+      addFood(steak[3], '#steak', () => {
+        console.log(steak[3])
+        addFood(steak[4], '#steak', () => {
+          console.log(steak[4])
+          addFood(steak[5], '#steak', () => {
+            console.log(steak[5])
+            addFood(steak[6], '#steak', () => {
+              console.log(steak[6])
+              addFood(steak[7], '#steak', () => {
+                console.log(steak[7])
+              })
+            })
+          })
+        })
+      })
+    })
   })
 });
 
