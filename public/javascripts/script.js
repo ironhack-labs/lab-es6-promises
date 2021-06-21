bb6fb99188f0796d2617d0914cdb93d90d7fb531
dechilds1@gmail.com <dechilds1@gmail.com>
2021-06-21 18:12:58 +0200

    Completed Lab Promises

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,77 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
+
+/*for (let i = 0; i < steak.length; i++) {
+  addFood(steak[i], '#steak');
   console.log(mashPotatoes[i])
 }
+*/
 
-// Iteration 1 using callbacks
+/*THIS IS METHOD 1 (THE SLOPPY WAY). BY CHAINING THE FUNCTIONS WITHIN EACH OTHER, 
+YOU ARE CALLING THE FOLLOWING FUNCTION AS THE CALLBACK AFTER EACH TIMEOUT HAS RUN. 
+THIS WORKS IN THIS CASE, BUT DOES NOT TAKE ADVANTAGE OF THE ".THEN" METHOD
+
 addFood(steak[0], '#steak', () => {
-  // ... your code here
   addFood(steak[1], '#steak', () => {
-
-  })
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
+                tableTop.appendChild(steakPic);
+              });
+            });
+          });
+        });
+      });
+    });
+  });
 });
+*/
+
+let tableTop = document.querySelector("#table");
+let potatoesPic = new Image(100,100);
+potatoesPic.src = "./public/images/mashPotatoes.jpg";
+potatoesPic.classList.add("potatoeImage");
+let steakPic = new Image(100,100);
+steakPic.src = "./public/images/steak.jpg";
+steakPic.classList.add("steakImage");
+let sproutsPic = new Image(100,100);
+sproutsPic.src = "./public/images/brusselSprouts.jpg";
+sproutsPic.classList.add("sproutsImage");
+
+/*THIS IS METHOD 2 (THE BETTER WAY). IT TAKES ADVANTAGE OF THE ".THEN" METHOD TO WAIT UNTIL
+THE COMPLETION OF EACH PREVIOUS PROMISE BEFORE BEGINNING THE NEXT ONE*/
+addFood(steak[0], '#steak')
+	.then(() => addFood(steak[1], '#steak'))
+	.then(() => addFood(steak[2], '#steak'))
+  .then(() => addFood(steak[3], '#steak'))
+  .then(() => addFood(steak[4], '#steak'))
+  .then(() => addFood(steak[5], '#steak'))
+  .then(() => addFood(steak[6], '#steak'))
+  .then(() => addFood(steak[7], '#steak'))
+  .then(() => tableTop.appendChild(steakPic))
 
 
+addFood(mashPotatoes[0], '#mashPotatoes')
+	.then(() => addFood(mashPotatoes[1], '#mashPotatoes'))
+	.then(() => addFood(mashPotatoes[2], '#mashPotatoes'))
+  .then(() => addFood(mashPotatoes[3], '#mashPotatoes'))
+  .then(() => addFood(mashPotatoes[4], '#mashPotatoes'))
+  .then(() => tableTop.appendChild(potatoesPic))
 
-// Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
-});
 
-// Iteration 3 using async/await
+addFood(brusselSprouts[0], '#brusselSprouts')
+  .then(() => addFood(brusselSprouts[1], '#brusselSprouts'))
+  .then(() => addFood(brusselSprouts[2], '#brusselSprouts'))
+  .then(() => addFood(brusselSprouts[3], '#brusselSprouts'))
+  .then(() => addFood(brusselSprouts[4], '#brusselSprouts'))
+  .then(() => addFood(brusselSprouts[5], '#brusselSprouts'))
+  .then(() => addFood(brusselSprouts[6], '#brusselSprouts'))
+  .then(() => addFood(brusselSprouts[7], '#brusselSprouts'))
+  .then(() => addFood(brusselSprouts[8], '#brusselSprouts'))
+  .then(() => tableTop.appendChild(sproutsPic))
 
-  async function makeFood(step) {
-    // ... your code here
-    
-  }
-  makeFood(eachStep);
+  
