da853faf2de34381331c0fdb690600906ab86862
sebastiangomezzz <sebastiangomezzz@gmail.com>
2021-06-21 18:00:49 +0200

    text and images are displayed

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,70 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
-}
+//for (let i = 0; i < mashPotatoes.length; i++) {
+//  //addFood(steak[i], '#steak');
+//  console.log(mashPotatoes[i])
+//}
 
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
-  addFood(steak[1], '#steak', () => {
+// addFood(steak[0], '#steak', () => {
+//   // ... your code here
+  
+//   addFood(steak[1], '#steak', () => {
 
-  })
-});
+//   })
+// });
 
 
 
-// Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
-});
+// Iteration 2 using `.then()` // for the 3 recipes, ignore the other iterations
+addFood(mashPotatoes[0], '#mashPotatoes')
+  .then(() => addFood(mashPotatoes[1], '#mashPotatoes'))
+  .then(() => addFood(mashPotatoes[2], "#mashPotatoes"))
+  .then(() => addFood(mashPotatoes[3], "#mashPotatoes"))
+  .then(() => addFood(mashPotatoes[4], "#mashPotatoes"))
+  .then(() => {const tableNode = document.getElementById("table")
+              let mashPotImg = document.createElement('img')
+              mashPotImg.src = "/public/images/mashPotatoes.jpg"
+              tableNode.appendChild(mashPotImg);
+})
 
-// Iteration 3 using async/await
+.then(()=>addFood(steak[0], '#steak')
+  .then(() => addFood(steak[1], '#steak'))
+  .then(() => addFood(steak[2], "#steak"))
+  .then(() => addFood(steak[3], "#steak"))
+  .then(() => addFood(steak[4], "#steak"))
+  .then(() => addFood(steak[5], "#steak"))
+  .then(() => addFood(steak[6], "#steak"))
+  .then(() => addFood(steak[7], "#steak"))
+  .then(()=> {const tableNode = document.getElementById("table")
+              let steakImg = document.createElement("img")
+              steakImg.src = "/public/images/steak.jpg"
+              tableNode.appendChild(steakImg);
 
-  async function makeFood(step) {
-    // ... your code here
+  }))
+
+.then(()=>addFood(brusselSprouts[0], '#brusselSprouts')
+  .then(() => addFood(brusselSprouts[1], '#brusselSprouts'))
+  .then(() => addFood(brusselSprouts[2], "#brusselSprouts"))
+  .then(() => addFood(brusselSprouts[3], "#brusselSprouts"))
+  .then(() => addFood(brusselSprouts[4], "#brusselSprouts"))
+  .then(() => addFood(brusselSprouts[5], "#brusselSprouts"))
+  .then(() => addFood(brusselSprouts[6], "#brusselSprouts"))
+  .then(() => addFood(brusselSprouts[7], "#brusselSprouts"))
+  .then(() => addFood(brusselSprouts[8], "#brusselSprouts"))
+  .then(()=> {const tableNode = document.getElementById("table")
+              let bruSproImg = document.createElement("img")
+              bruSproImg.src = "/public/images/brusselSprouts.jpg"
+              tableNode.appendChild(bruSproImg);
+
+  }));
+
+
+// // Iteration 3 using async/await
+
+//   async function makeFood(step) {
+//     // ... your code here
     
-  }
-  makeFood(eachStep);
+//   }
+//   makeFood(eachStep);
