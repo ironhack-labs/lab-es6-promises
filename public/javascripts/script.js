c5afe4ecb866e5866c09ce1a31dd42981efdadb1
Hanlin Li <lihanlinapp@gmail.com>
2021-06-21 16:55:20 +0200

    image not shown in right order

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,17 +1,30 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
-}
+//for (let i = 0; i < mashPotatoes.length; i++) {
+  ////addFood(steak[i], '#steak');
+  //console.log(mashPotatoes[i])
+//}
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
   // ... your code here
   addFood(steak[1], '#steak', () => {
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
 
+              })
+            })
+          })
+        })
+      })
+    })
   })
+  document.querySelector('#steak').innerHTML = "<img src ='/lab-es6-promises/public/images/steak.jpg'>"
 });
 
 
@@ -19,13 +32,26 @@ addFood(steak[0], '#steak', () => {
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
   // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[4], '#mashPotatoes')
+      })
+    })
+  })
+document.querySelector('#mashPotatoes').innerHTML = "<img src ='/lab-es6-promises/public/images/mashPotatoes.jpg'>"
+
 });
 
 // Iteration 3 using async/await
 
   async function makeFood(step) {
     // ... your code here
-    
-  }
-  makeFood(eachStep);
+    for (let i = 0; i < step.length; i++) {
+      await addFood(step[i], '#brusselSprouts');
+    } 
+    makeFood(eachStep);
+  }   
+  document.querySelector('#brusselSprouts').innerHTML = "<img src ='/lab-es6-promises/public/images/brusselSprouts.jpg'>"
+
+  console.log(makeFood(brusselSprouts))
