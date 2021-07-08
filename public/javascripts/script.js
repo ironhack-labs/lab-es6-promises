51cecd200d14c57ccd780a0e5d8e5279c03e4ed8
Ana Carolina R Lima <anacarolina.rdel@gmail.com>
2021-07-08 19:37:32 -0300

    lab done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,58 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
-}
+// for (let i = 0; i < mashPotatoes.length; i++) {
+//   //addFood(steak[i], '#steak');
+//   console.log(mashPotatoes[i])
+// }
 
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
-  addFood(steak[1], '#steak', () => {
-
+addFood(steak[0], '#steak', result => {  
+  addFood(steak[1], '#steak', result => {
+    addFood(steak[2], '#steak', result => {
+      addFood(steak[3], '#steak', result => {
+        addFood(steak[4], '#steak', result => {
+          addFood(steak[5], '#steak', result => {
+            addFood(steak[6], '#steak', result => {
+              addFood(steak[7], '#steak', result => {
+                let steakImg = document.createElement('img');
+                steakImg.src = "./public/images/steak.jpg"
+                let src = document.getElementById("table");
+                src.appendChild(steakImg);
+              })
+            })
+          })
+        })
+      })
+    })
   })
 });
 
-
-
 // Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+addFood(mashPotatoes[0], '#mashPotatoes').then((result) => {
+  addFood(mashPotatoes[1], '#mashPotatoes').then((result) => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then((result) => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then((result) => {
+        addFood(mashPotatoes[4], '#mashPotatoes').then((result) => {
+          let mashPotatoesImg = document.createElement('img');
+          mashPotatoesImg.src = "./public/images/mashPotatoes.jpg"
+          let src = document.getElementById("table");
+          src.appendChild(mashPotatoesImg);
+        });
+      });
+    });
+  });
 });
 
 // Iteration 3 using async/await
-
-  async function makeFood(step) {
-    // ... your code here
-    
+async function makeFood(step) {
+    let result 
+    for (let i = 0; i < step.length; i++) {
+      result = await addFood(step[i], '#brusselSprouts')  
+    }
+    let brusselSproutsImg = document.createElement('img');
+    brusselSproutsImg.src = "./public/images/brusselSprouts.jpg"
+    let src = document.getElementById("table");
+    src.appendChild(brusselSproutsImg);
   }
-  makeFood(eachStep);
+makeFood(brusselSprouts);
