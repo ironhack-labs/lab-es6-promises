ac016dff0a49e89eaa5668511c12767b017b5d40
christopherfulldev <highlander.all@gmail.com>
2021-07-08 22:25:39 -0300

    Callbacks, Promisses and Async

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,30 +2,54 @@
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
 for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
+  addFood(steak[i], '#steak');
   console.log(mashPotatoes[i])
 }
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
-  // ... your code here
-  addFood(steak[1], '#steak', () => {
-
+  addFood (steak[1], '#steak', () => {
+    addFood (steak[2], '#steak', () => {
+      addFood (steak[3], '#steak', () => {
+        addFood (steak[4], '#steak', () => { //hadouken
+          addFood (steak[5], '#steak', () => {
+            addFood (steak[6], '#steak', () => {
+              addFood (steak[7], '#steak', () => {
+              })
+            })
+          })
+        })
+      })
+    })
   })
+  document.getElementsByClassName ('#table').innerHTML += '<img src ="public/images/steak.jpg"';
 });
+    
+    
 
 
 
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then (() => { //1/2 hadouken
+      addFood(mashPotatoes[3], '#mashPotatoes').then (() => {
+        addFood(mashPotatoes[4], '#mashPotatoes').then (() => {
+        })
+      })
+    })
+  })
+  document.getElementsByClassName('#table').innerHTML += `<img src="public/images/mashPotatoes.jpg" />`;
 });
 
 // Iteration 3 using async/await
 
   async function makeFood(step) {
-    // ... your code here
-    
+    for (let i = 0; i < steps.length; i ++) { //top
+      await addFood (steps[i], id);
+    }
+    makeFood(eachStep);
   }
-  makeFood(eachStep);
+  makeFood(brusselSprouts, '#brusselSprouts');
+
+  
