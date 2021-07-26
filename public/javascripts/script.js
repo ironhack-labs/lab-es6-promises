8c79326414d5476409b431fe1795c8dc02a30c8e
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-07-26 20:06:25 +0300

    iteration 3 completed

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,39 +8,40 @@
 */
 
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  addFood(steak[1], '#steak', () => {
-    addFood(steak[2], '#steak', () => {
-      addFood(steak[3], '#steak', () => {
-        addFood(steak[4], '#steak', () => {
-          addFood(steak[5], '#steak', () => {
-            addFood(steak[6], '#steak', () => {
-              addFood(steak[7], '#steak', () => {
-                
-              })
-            })
-          })
-        })
-      })
-    })
-
-  })
+addFood(steak[0], "#steak", () => {
+  addFood(steak[1], "#steak", () => {
+    addFood(steak[2], "#steak", () => {
+      addFood(steak[3], "#steak", () => {
+        addFood(steak[4], "#steak", () => {
+          addFood(steak[5], "#steak", () => {
+            addFood(steak[6], "#steak", () => {
+              addFood(steak[7], "#steak", () => {});
+            });
+          });
+        });
+      });
+    });
+  });
 });
 
-
-
 // Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes')
-  .then(() => addFood(mashPotatoes[1], '#mashPotatoes'))
-  .then(() => addFood(mashPotatoes[2], '#mashPotatoes'))
-  .then(() => addFood(mashPotatoes[3], '#mashPotatoes'))
-  .then(() => addFood(mashPotatoes[4], '#mashPotatoes'))
-
+addFood(mashPotatoes[0], "#mashPotatoes")
+  .then(() => addFood(mashPotatoes[1], "#mashPotatoes"))
+  .then(() => addFood(mashPotatoes[2], "#mashPotatoes"))
+  .then(() => addFood(mashPotatoes[3], "#mashPotatoes"))
+  .then(() => addFood(mashPotatoes[4], "#mashPotatoes"));
 
 // Iteration 3 using async/await
 
-async function makeFood(step) {
-  // ... your code here
-
+async function makeFood() {
+  const result0 = await addFood(brusselSprouts[0], "#brusselSprouts");
+  const result1 = await addFood(brusselSprouts[1], "#brusselSprouts");
+  const result2 = await addFood(brusselSprouts[2], "#brusselSprouts");
+  const result3 = await addFood(brusselSprouts[3], "#brusselSprouts");
+  const result4 = await addFood(brusselSprouts[4], "#brusselSprouts");
+  const result5 = await addFood(brusselSprouts[5], "#brusselSprouts");
+  const result6 = await addFood(brusselSprouts[6], "#brusselSprouts");
+  const result7 = await addFood(brusselSprouts[7], "#brusselSprouts");
+  const result8 = await addFood(brusselSprouts[8], "#brusselSprouts");
 }
-makeFood(eachStep);
+makeFood();
