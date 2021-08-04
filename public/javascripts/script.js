44c05a12d670de367c53350623a827de014f6b02
vloparevich <vadim.loparevich@gmail.com>
2021-08-03 20:07:22 -0700

    Resolve the lab

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,55 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
-}
+// for (let i = 0; i < mashPotatoes.length; i++) {
+//   //addFood(steak[i], '#steak');
+//   console.log(mashPotatoes[i])
+// }
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
-  // ... your code here
   addFood(steak[1], '#steak', () => {
-
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
+              })
+            })
+          })
+        })
+      })
+    })
   })
 });
 
-
-
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
-});
+  return addFood(mashPotatoes[1], '#mashPotatoes');
+}).then(() => {
+  return addFood(mashPotatoes[2], '#mashPotatoes');
+}).then(resolve => {
+  return addFood(mashPotatoes[3], '#mashPotatoes');
+}).then(() => {
+  return addFood(mashPotatoes[4], '#mashPotatoes');
+}).catch(err => {
+  console.log(err.message);
+})
 
 // Iteration 3 using async/await
+async function makeFood(step) {
+  return await addFood(step, '#brusselSprouts');
 
-  async function makeFood(step) {
-    // ... your code here
-    
-  }
-  makeFood(eachStep);
+
+}
+makeFood(brusselSprouts[0])
+  .then(() => makeFood(brusselSprouts[1]))
+  .then(() => makeFood(brusselSprouts[2]))
+  .then(() => makeFood(brusselSprouts[2]))
+  .then(() => makeFood(brusselSprouts[3]))
+  .then(() => makeFood(brusselSprouts[4]))
+  .then(() => makeFood(brusselSprouts[5]))
+  .then(() => makeFood(brusselSprouts[6]))
+  .then(() => makeFood(brusselSprouts[7]))
+  .then(() => makeFood(brusselSprouts[8]))
