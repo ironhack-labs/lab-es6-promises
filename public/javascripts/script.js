edb8bf3116fd06be0d548cad7b850379d2c70da2
“Michael <michaelrodiles@gmail.com>
2021-08-07 11:03:57 -0400

    Solved Lab

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,9 +8,18 @@ for (let i = 0; i < mashPotatoes.length; i++) {
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
-  // ... your code here
   addFood(steak[1], '#steak', () => {
-
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {})
+            })
+          })
+        })
+      })
+    })
   })
 });
 
@@ -19,13 +28,29 @@ addFood(steak[0], '#steak', () => {
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
   // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[4], '#mashPotatoes').then(() => {})
+      })
+    })
+
+  })
+
+
 });
 
 // Iteration 3 using async/await
 
-  async function makeFood(step) {
-    // ... your code here
-    
-  }
-  makeFood(eachStep);
+async function makeFood(step) {
+  return await addFood(step, '#brusselSprouts')
+}
+makeFood(brusselSprouts[0])
+  .then(() => makeFood(brusselSprouts[1]))
+  .then(() => makeFood(brusselSprouts[2]))
+  .then(() => makeFood(brusselSprouts[3]))
+  .then(() => makeFood(brusselSprouts[4]))
+  .then(() => makeFood(brusselSprouts[5]))
+  .then(() => makeFood(brusselSprouts[6]))
+  .then(() => makeFood(brusselSprouts[7]))
+  .then(() => makeFood(brusselSprouts[8]))
