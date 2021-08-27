47ec4df86cfde8e92ba3745ef9a7ad7e69d062d8
Carlos Daniel <caduccino@live.com>
2021-08-26 21:45:58 -0300

    done untill bonus 1

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,24 +8,56 @@ for (let i = 0; i < mashPotatoes.length; i++) {
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
-  // ... your code here
   addFood(steak[1], '#steak', () => {
-
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
+                const steakImage = new Image();
+                steakImage.src = './public/images/steak.jpg';
+                document.getElementById('table').appendChild(steakImage);
+              }
+              )
+            })
+          })
+        })
+      })
+    })
   })
 });
 
 
-
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() =>{
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() =>{
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() =>{
+        addFood(mashPotatoes[4], '#mashPotatoes').then(() =>{
+          const mashPotatoesImage = new Image();
+          mashPotatoesImage.src = './public/images/mashPotatoes.jpg';
+          document.getElementById('table').appendChild(mashPotatoesImage);
+        })
+      })
+    }) 
+  })
 });
 
 // Iteration 3 using async/await
 
-  async function makeFood(step) {
-    // ... your code here
-    
-  }
-  makeFood(eachStep);
+  async function makeFood(step, id) {
+    await addFood(brusselSprouts[0], '#brusselSprouts');
+    await addFood(brusselSprouts[1], '#brusselSprouts');
+    await addFood(brusselSprouts[2], '#brusselSprouts');
+    await addFood(brusselSprouts[3], '#brusselSprouts');
+    await addFood(brusselSprouts[4], '#brusselSprouts');
+    await addFood(brusselSprouts[5], '#brusselSprouts');
+    await addFood(brusselSprouts[6], '#brusselSprouts');
+    await addFood(brusselSprouts[7], '#brusselSprouts');
+    await addFood(brusselSprouts[8], '#brusselSprouts');
+    const brusselSproutsImage = new Image();
+    brusselSproutsImage.src = './public/images/brusselSprouts.jpg';
+    document.getElementById('table').appendChild(brusselSproutsImage);
+  };
+  makeFood();
