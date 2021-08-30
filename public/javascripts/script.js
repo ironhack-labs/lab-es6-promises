6a6f8d56618d04d649e7dadf1d8c6cfc7a3dacc5
Marishka Zachariah <marishka.zachariah@gmail.com>
2021-08-30 15:18:03 +0200

    iteration 1 and 2 complete

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -10,8 +10,20 @@ for (let i = 0; i < mashPotatoes.length; i++) {
 addFood(steak[0], '#steak', () => {
   // ... your code here
   addFood(steak[1], '#steak', () => {
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
 
-  })
+              });
+            });
+          });
+        });
+      });
+    });
+  });
 });
 
 
@@ -19,7 +31,14 @@ addFood(steak[0], '#steak', () => {
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
   // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
+        })
+      })
+    })
+  })
 });
 
 // Iteration 3 using async/await
