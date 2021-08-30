f7f9d6029509f307dfbad9142d66527e1c5d4049
Ana Martins <ana.af.martins@gmail.com>
2021-08-30 22:02:54 +0200

    lab done!
    Co-authored-by: AlexVascon <AlexVascon@users.noreply.github.com>

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,86 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
+/*for (let i = 0; i < mashPotatoes.length; i++) {
   //addFood(steak[i], '#steak');
   console.log(mashPotatoes[i])
+}*/
+
+console.time("timer");
+
+//Bonus 1
+
+function addImage(img) {
+  const newImg = document.createElement("img");
+  newImg.src = img;
+  const table = document.getElementById("table");
+  table.appendChild(newImg);
 }
 
-// Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
-  addFood(steak[1], '#steak', () => {
+//Bonus 2 - part 1
 
-  })
-});
+// -- BUTTON --
+function alertButton() {
+  const alertBtn = document.createElement("button");
+  alertBtn.innerText = "Dinner is served.";
+  document.body.appendChild(alertBtn);
+}
 
-
+// Iteration 1 using callBacks
+function makeSteak() {
+  addFood(steak[0], "#steak", () => {
+    addFood(steak[1], "#steak", () => {
+      addFood(steak[2], "#steak", () => {
+        addFood(steak[3], "#steak", () => {
+          addFood(steak[4], "#steak", () => {
+            addFood(steak[5], "#steak", () => {
+              addFood(steak[6], "#steak", () => {
+                addFood(steak[7], "#steak", () => {
+                  addImage("/public/images/steak.jpg");
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+}
 
 // Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
-});
+function makePotatoes() {
+  addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
+    addFood(mashPotatoes[1], "#mashPotatoes").then(() =>
+      addFood(mashPotatoes[2], "#mashPotatoes").then(() =>
+        addFood(mashPotatoes[3], "#mashPotatoes").then(() =>
+          addFood(mashPotatoes[4], "#mashPotatoes")
+            .then(() => addImage("/public/images/mashPotatoes.jpg"))
+            .then(() => (potatoesReady = true))
+            .catch((error) => console.log(`Error in secondPromise ${error}`))
+        )
+      )
+    );
+  });
+}
 
 // Iteration 3 using async/await
 
-  async function makeFood(step) {
-    // ... your code here
-    
+async function makeFood() {
+  for (let i = 0; i < brusselSprouts.length; i++) {
+    let response = await addFood(brusselSprouts[i], "#brusselSprouts");
+    if (response.ok) {
+      continue;
+    }
   }
-  makeFood(eachStep);
+  addImage("/public/images/brusselSprouts.jpg");
+}
+
+const firstPromise = makeSteak();
+const secondPromise = makePotatoes();
+const thirdPromise = makeFood();
+
+//Bonus 2 - part 2
+
+Promise.all([firstPromise, secondPromise, thirdPromise])
+  .then(alertButton)
+  .catch((error) => console.log(`Error in promises ${error}`));
