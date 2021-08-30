29749286b0447b72c0ddad32a7424feac2faa0db
thomaskno <thomas.kno@yahoo.com>
2021-08-30 17:42:27 -0400

    Complete lab

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,9 +8,23 @@ for (let i = 0; i < mashPotatoes.length; i++) {
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
-  // ... your code here
+  // let step = document.createElement("li")
+  // step.innerHTML = steak[0];
+  // document.getElementById('steak').append(step);
   addFood(steak[1], '#steak', () => {
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
 
+              })
+            })
+          })
+        })
+      })
+    })
   })
 });
 
@@ -18,14 +32,27 @@ addFood(steak[0], '#steak', () => {
 
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
+        })
+      })
+    })
+  })
 });
 
 // Iteration 3 using async/await
 
   async function makeFood(step) {
-    // ... your code here
-    
+
+
+  if(step <  brusselSprouts.length){
+    await addFood(brusselSprouts[step], '#brusselSprouts');
+    makeFood(step + 1);
+  } 
+   
+
   }
-  makeFood(eachStep);
+
+  makeFood(0);
