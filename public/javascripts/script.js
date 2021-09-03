0e2ccdbb151a9a616f06c43aafadce54b207119c
eduardo brandiao <eduardo.brandiao.adm@gmail.com>
2021-09-03 18:39:05 -0300

    done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,57 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
+for (let i = 0; i < steak.length; i++) {
   //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
+  console.log(steak[i])
 }
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
   // ... your code here
   addFood(steak[1], '#steak', () => {
-
-  })
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
+              });
+            });
+          });
+        });
+      });
+    });
+  });
 });
 
 
 
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
+
+        });
+      });
+    });
+  });
 });
 
 // Iteration 3 using async/await
 
   async function makeFood(step) {
-    // ... your code here
+    await addFood(steps[0], '#brusselSprouts')
+    await addFood(steps[1], '#brusselSprouts')
+    await addFood(steps[2], '#brusselSprouts')
+    await addFood(steps[3], '#brusselSprouts')
+    await addFood(steps[4], '#brusselSprouts')
+    await addFood(steps[5], '#brusselSprouts')
+    await addFood(steps[5], '#brusselSprouts')
+    await addFood(steps[7], '#brusselSprouts')
+    await addFood(steps[8], '#brusselSprouts')
     
   }
-  makeFood(eachStep);
+  makeFood(brusselSprouts);
