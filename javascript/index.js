610c9bb32311e7c0700d139adbe41c4dd7b87081
Michael Seitz <michael.s.seitz@gmail.com>
2021-11-02 22:44:24 +0100

    done

--- javascript/index.js
+++ javascript/index.js
@@ -50,16 +50,86 @@ getInstruction("mashedPotatoes", 0, (step0) => {
   }, (error) => console.log(error));
 
 // Iteration 2 - using promises
-// obtainInstruction('steak', 0)
-//   .then( (step0) => {
-//     document.querySelector("#steak").innerHTML += `<li>${step0}</li>`
-//   }).catch( (error) => {
-//     console.log(error)
-//     ;}
+obtainInstruction('steak', 0)
+  .then( (step) => {
+    buildDom("#steak", step)
+    return obtainInstruction('steak', 1)
+  })
+  .then( (step) => {
+    buildDom("#steak", step)
+    return obtainInstruction('steak', 2)
+  })
+  .then( (step) => {
+    buildDom("#steak", step)
+    return obtainInstruction('steak', 3)
+  })
+  .then( (step) => {
+    buildDom("#steak", step)
+    return obtainInstruction('steak', 4)
+  })
+  .then( (step) => {
+    buildDom("#steak", step)
+    return obtainInstruction('steak', 5)
+  })
+  .then( (step) => {
+    buildDom("#steak", step)
+    return obtainInstruction('steak', 6)
+  })
+  .then( (step) => {
+    buildDom("#steak", step)
+    return obtainInstruction('steak', 7)
+  })
+  .then( (step) => {
+    buildDom("#steak", step)
+    document.querySelector("#steakImg").removeAttribute("hidden");
+  })
+  .catch((error) => {console.log(error);})
 
 
 // Iteration 3 using async/await
-// ...
+async function makeBroccoli() {
+  try {
+    let step0 = await obtainInstruction("broccoli", 0)
+    buildDom("#broccoli", step0)
+    let step1 = await obtainInstruction("broccoli", 1)
+    buildDom("#broccoli", step1)
+    let step2 = await obtainInstruction("broccoli", 2)
+    buildDom("#broccoli", step2)
+    let step3 = await obtainInstruction("broccoli", 3)
+    buildDom("#broccoli", step3)
+    let step4 = await obtainInstruction("broccoli", 4)
+    buildDom("#broccoli", step4)
+    let step5 = await obtainInstruction("broccoli", 5)
+    buildDom("#broccoli", step5)
+    let step6 = await obtainInstruction("broccoli", 6)
+    buildDom("#broccoli", step6)
+    document.querySelector("#broccoliImg").removeAttribute("hidden");
+  } catch (error) {
+    
+  }
+}
+makeBroccoli()
 
 // Bonus 2 - Promise all
-// ...
+async function makeBrussels() {
+  try {
+    let step0 = await obtainInstruction("brusselsSprouts", 0)
+    let step1 = await obtainInstruction("brusselsSprouts", 1)
+    let step2 = await obtainInstruction("brusselsSprouts", 2)
+    let step3 = await obtainInstruction("brusselsSprouts", 3)
+    let step4 = await obtainInstruction("brusselsSprouts", 4)
+    let step5 = await obtainInstruction("brusselsSprouts", 5)
+    let step6 = await obtainInstruction("brusselsSprouts", 6)
+    Promise.all([step0, step1, step2, step3, step4, step5, step6])
+    .then(value => {
+      value.forEach(element => {
+        buildDom("#brusselsSprouts", element)
+      });
+      buildDom("#brusselsSprouts", "Brussels sprouts are ready!")
+    })
+    document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
+  } catch (error) {
+    
+  }
+}
+makeBrussels()
