96e46cf5ab47abda56ae1dcad2d8cf2dbce25e13
TatianaPrada <tatiana.1227@hotmail.com>
2021-11-09 13:13:38 +0000

    second interaction done

--- javascript/index.js
+++ javascript/index.js
@@ -28,7 +28,7 @@
 
 
 // Iteration 1 - using callbacks
-const first = getInstruction('mashedPotatoes', 0, (step0) => {
+const potatoes = getInstruction('mashedPotatoes', 0, (step0) => {
   document.querySelector("#mashedPotatoes").innerHTML += `<li>${step0}</li>`
   getInstruction('mashedPotatoes', 1, (step1)=>{
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`
@@ -39,22 +39,51 @@ const first = getInstruction('mashedPotatoes', 0, (step0) => {
         getInstruction('mashedPotatoes', 4, (step4)=>{
           document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`
           document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`
-          
         })
       })
     })
   })
-  
 });
 
 // Iteration 2 - using promises
-// ...
+const stake = ()=>{
+  obtainInstruction('steak', 0)
+  .then( (step0) => {
+    document.querySelector("#steak").innerHTML += `<li>${step0}</li>`
+    obtainInstruction('steak', 1)
+    .then( (step1) => {
+      document.querySelector("#steak").innerHTML += `<li>${step1}</li>`
+      obtainInstruction('steak', 2)
+      .then( (step2) => {
+        document.querySelector("#steak").innerHTML += `<li>${step2}</li>`
+        obtainInstruction('steak', 3)
+        .then( (step3) => {
+          document.querySelector("#steak").innerHTML += `<li>${step3}</li>`
+          obtainInstruction('steak', 4)
+          .then( (step4) => {
+           document.querySelector("#steak").innerHTML += `<li>${step4}</li>`
+           obtainInstruction('steak', 5)
+            .then( (step5) => {
+              document.querySelector("#steak").innerHTML += `<li>${step5}</li>`
+              obtainInstruction('steak', 6)
+              .then( (step6) => {
+                document.querySelector("#steak").innerHTML += `<li>${step6}</li>`
+                document.querySelector("#steak").innerHTML += `<li>Stake is ready!</li>`
+              })
+            }) 
+          })
+        })
+      })
+    })
+  })
+}
+
+stake()
+
 
 // Iteration 3 using async/await
 // ...
 
 // Bonus 2 - Promise all
 // ...
-
-
 
