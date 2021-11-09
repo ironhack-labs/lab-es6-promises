f1f4fac77f5ec19d74cfc14f486591a170446dca
Santi Jimenez <jimenez.santi@gmail.com>
2021-11-09 13:36:57 +0100

    Iteration 2

--- javascript/index.js
+++ javascript/index.js
@@ -46,7 +46,76 @@ getInstruction('mashedPotatoes', 0, (step0) => {
 }, (error) => console.log(error));
 
 // Iteration 2 - using promises
-// ...
+obtainInstruction("steak", 0)
+  .then((step0) => {
+    document.querySelector("#steak").innerHTML += `<li>${step0}</li>`;
+    obtainInstruction("steak", 1)
+      .then((step1) => {
+        document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
+        obtainInstruction("steak", 2)
+          .then((step2) => {
+            document.querySelector("#steak").innerHTML += `<li>${step2}</li>`;
+            obtainInstruction("steak", 3)
+              .then((step3) => {
+                document.querySelector(
+                  "#steak"
+                ).innerHTML += `<li>${step3}</li>`;
+                obtainInstruction("steak", 4)
+                  .then((step4) => {
+                    document.querySelector(
+                      "#steak"
+                    ).innerHTML += `<li>${step4}</li>`;
+                    obtainInstruction("steak", 5)
+                      .then((step5) => {
+                        document.querySelector(
+                          "#steak"
+                        ).innerHTML += `<li>${step5}</li>`;
+                        obtainInstruction("steak", 6)
+                          .then((step6) => {
+                            document.querySelector(
+                              "#steak"
+                            ).innerHTML += `<li>${step6}</li>`;
+                            obtainInstruction("steak", 7)
+                              .then((step7) => {
+                                document.querySelector(
+                                  "#steak"
+                                ).innerHTML += `<li>${step7}</li>`;
+                                document.querySelector(
+                                  "#steak"
+                                ).innerHTML += `<li>Steak is ready!</li>`;
+                              })
+                              .catch((err) => {
+                                console.log(err);
+                              });
+                          })
+                          .catch((err) => {
+                            console.log(err);
+                          });
+                      })
+                      .catch((err) => {
+                        console.log(err);
+                      });
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                  });
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  // ... Your code here
 
 // Iteration 3 using async/await
 // ...
