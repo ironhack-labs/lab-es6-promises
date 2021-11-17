ab69cae1dcd74b7b77b4fe2eadf0a820c47615a3
SantiagoCortina <cortina.zapfe@gmail.com>
2021-11-17 07:54:07 -0600

    iteracion 2, done

--- javascript/index.js
+++ javascript/index.js
@@ -28,7 +28,6 @@
 
 
 // Iteration 1 - using callbacks
-// Iteration 1 - using callbacks
 getInstruction('mashedPotatoes', 0, (step0) => {
   document.querySelector("#mashedPotatoes").innerHTML += `<li>${step0}</li>`
   getInstruction('mashedPotatoes', 1, (step1) => {
@@ -50,7 +49,41 @@ getInstruction('mashedPotatoes', 0, (step0) => {
 
 
 // Iteration 2 - using promises
-// ...
+obtainInstruction('steak', 0)
+  .then( (step0) => {
+    document.querySelector("#steak").innerHTML += `<li>${step0}</li>`;
+    return obtainInstruction('steak', 1);
+  })
+  .then ( (step1) => {
+    document.querySelector("#steak").innerHTML += `<li>${step1}</li>`
+    return obtainInstruction('steak', 2)
+  })
+  .then ( (step2) => {
+    document.querySelector("#steak").innerHTML += `<li>${step2}</li>`
+    return obtainInstruction('steak', 3)
+  })
+  .then ( (step3) => {
+    document.querySelector("#steak").innerHTML += `<li>${step3}</li>`
+    return obtainInstruction('steak', 4)
+  })
+  .then ( (step4) => {
+    document.querySelector("#steak").innerHTML += `<li>${step4}</li>`
+    return obtainInstruction('steak', 5)
+  })
+  .then ( (step5) => {
+    document.querySelector("#steak").innerHTML += `<li>${step5}</li>`
+    return obtainInstruction('steak', 6)
+  })
+  .then ( (step6) => {
+    document.querySelector("#steak").innerHTML += `<li>${step6}</li>`
+    return obtainInstruction('steak', 7)
+  })
+  .finally ( (step7) => {
+    document.querySelector("#steak").innerHTML += `<li>${step7}</li>`
+  })
+  .catch((err) => {
+    console.error("Instruction not found", err);
+  });
 
 // Iteration 3 using async/await
 // ...
