4312aede05c7b8d5efcbae9fc6878d8d00e72e3f
SantiagoCortina <cortina.zapfe@gmail.com>
2021-11-17 08:33:41 -0600

    done

--- javascript/index.js
+++ javascript/index.js
@@ -123,4 +123,21 @@ makeBroccoli()
 
 
 // Bonus 2 - Promise all
-// ...
+const p1 = obtainInstruction('brusselsSprouts', 0);
+const p2 = obtainInstruction('brusselsSprouts', 1);
+const p3 = obtainInstruction('brusselsSprouts', 2);
+const p4 = obtainInstruction('brusselsSprouts', 3);
+const p5 = obtainInstruction('brusselsSprouts', 4);
+const p6 = obtainInstruction('brusselsSprouts', 5);
+const p7 = obtainInstruction('brusselsSprouts', 6);
+const p8 = obtainInstruction('brusselsSprouts', 7);
+
+Promise.all([p1,p2,p3,p4,p5,p6,p7,p8])
+.then((values) => {
+  values.forEach(element => document.querySelector("#brusselsSprouts").innerHTML += `<li>${element}</li>`)
+})
+.finally( () => {
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>Brussels Sprouts are ready</li>`
+  let bs = document.getElementById('brusselsSproutsImg')
+  bs.removeAttribute("hidden")
+})
