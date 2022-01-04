8f1e62f01e3cd304003b24f96fc0a4dce2884915
Vitor Afonso <vittokm@hotmail.com>
2022-01-04 15:28:30 +0000

    finished iteration 1, 2 & 3. only missing  the bonus

--- javascript/index.js
+++ javascript/index.js
@@ -44,7 +44,13 @@ getInstruction('mashedPotatoes', 0, (step0) => {
 
         getInstruction('mashedPotatoes', 4, (step5) => {
           document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
-          document.querySelector("#mashedPotatoes").innerHTML += `<li>${'Mashed potatoes are ready!'}</li>`;
+
+          setTimeout( () => {
+        
+            document.querySelector("#mashedPotatoes").innerHTML += `<li>${'Mashed potatoes are ready!'}</li>`;
+    
+          }, Math.floor(Math.random() * 1000));
+          
         });
 
       });
@@ -59,36 +65,107 @@ getInstruction('mashedPotatoes', 0, (step0) => {
 obtainInstruction('steak', 0)
 
   .then( (step0) => {
+
     document.querySelector("#steak").innerHTML += `<li>${step0}</li>`;  
-    return steak[1];
+
+    return new Promise(function(resolve, reject) {
+    
+      setTimeout( () => {
+        
+        resolve(steak[1]);
+
+      }, Math.floor(Math.random() * 1000));
+    });
   })
   .then( (step1) => {
-    document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
-    return steak[2];
+
+    document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;  
+
+    return new Promise(function(resolve, reject) {
+
+      setTimeout( () => {
+        
+        resolve(steak[2]);
+
+      }, Math.floor(Math.random() * 1000));
+    });
+
   })
   .then( (step2) => {
-    document.querySelector("#steak").innerHTML += `<li>${step2}</li>`;
-    return steak[3];
+
+    document.querySelector("#steak").innerHTML += `<li>${step2}</li>`;  
+
+    return new Promise(function(resolve, reject) {
+
+      setTimeout( () => {
+
+        resolve(steak[3]);
+
+      }, Math.floor(Math.random() * 1000));
+    });
   })
   .then( (step3) => {
-    document.querySelector("#steak").innerHTML += `<li>${step3}</li>`;
-    return steak[4];
+
+    document.querySelector("#steak").innerHTML += `<li>${step3}</li>`; 
+
+    return new Promise(function(resolve, reject) {
+
+      setTimeout( () => {
+         
+        resolve(steak[4]);
+
+      }, Math.floor(Math.random() * 1000));
+    });
   })
   .then( (step4) => {
-    document.querySelector("#steak").innerHTML += `<li>${step4}</li>`;
-    return steak[5];
+
+    document.querySelector("#steak").innerHTML += `<li>${step4}</li>`; 
+    
+    return new Promise(function(resolve, reject) {
+
+      setTimeout( () => { 
+
+        resolve(steak[5]);
+
+      }, Math.floor(Math.random() * 1000));
+    });
   })
   .then( (step5) => {
-    document.querySelector("#steak").innerHTML += `<li>${step5}</li>`;
-    return steak[6];
+    
+    document.querySelector("#steak").innerHTML += `<li>${step5}</li>`;  
+
+    return new Promise(function(resolve, reject) {
+
+      setTimeout( () => {
+
+        resolve(steak[6]);
+
+      }, Math.floor(Math.random() * 1000));
+    });
   })
   .then( (step6) => {
-    document.querySelector("#steak").innerHTML += `<li>${step6}</li>`;
-    return steak[7];
+    
+    document.querySelector("#steak").innerHTML += `<li>${step6}</li>`;  
+
+    return new Promise(function(resolve, reject) {
+
+      setTimeout( () => {
+        
+        resolve(steak[7]);
+
+      }, Math.floor(Math.random() * 1000));
+    });
   })
   .then( (step7) => {
-    document.querySelector("#steak").innerHTML += `<li>${step7}</li>`;
-    document.querySelector("#steak").innerHTML += `<li>Stake is ready!</li>`;
+
+    document.querySelector("#steak").innerHTML += `<li>${step7}</li>`;  
+
+    setTimeout( () => {
+
+      document.querySelector("#steak").innerHTML += `<li>Stake is ready!</li>`;
+
+    }, Math.floor(Math.random() * 1000));
+    
   })
   .catch( err => console.log(err));
   
@@ -104,7 +181,12 @@ async function makeBroccoli() {
     document.querySelector("#broccoli").innerHTML += `<li> ${await obtainInstruction('broccoli', 4)} </li>`;
     document.querySelector("#broccoli").innerHTML += `<li> ${await obtainInstruction('broccoli', 5)} </li>`;
     document.querySelector("#broccoli").innerHTML += `<li> ${await obtainInstruction('broccoli', 6)} </li>`;
-    document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`;
+    setTimeout( () => {
+        
+      document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`;
+
+    }, Math.floor(Math.random() * 1000));
+    
     
   } catch (error) {
     
