84aa1f594e8059b236764190fa6370332832a470
AnaModesto21 <77633954+AnaModesto21@users.noreply.github.com>
2022-01-11 20:50:51 +0000

    iteration 1

--- javascript/index.js
+++ javascript/index.js
@@ -28,16 +28,23 @@
 
 
 // Iteration 1 - using callbacks
-getInstruction('mashedPotatoes', 0, (step0) => {
-  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step0}</li>`
-getInstruction('mashedPotatoes', 1, (step1) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`
-getInstruction('mashedPotatoes', 2, (step2) => {
-      document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`
-getInstruction('mashedPotatoes', 3, (step3) => {
-        document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`
-getInstruction('mashedPotatoes', 0, (step4) => {
-          document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`
+getInstruction('mashedPotatoes', 0, (step1) => {
+  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`
+getInstruction('mashedPotatoes', 1, (step2) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`
+getInstruction('mashedPotatoes', 2, (step3) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`
+getInstruction('mashedPotatoes', 3, (step4) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`
+getInstruction('mashedPotatoes', 0, (step5) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>mashed potatoes are ready!</li>`;
+    document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
+}, (error) => console.log(error));
+}, (error) => console.log(error));
+}, (error) => console.log(error));
+}, (error) => console.log(error));
+}, (error) => console.log(error));
 
 // Iteration 2 - using promises
 // ...
