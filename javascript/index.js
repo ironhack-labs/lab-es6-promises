09cac0d12b5fec2fe28c64038cbaa23c919fd262
Jucimeire Silva <jucimeiresilva@hotmail.com>
2022-01-12 20:53:11 -0300

    done

--- javascript/index.js
+++ javascript/index.js
@@ -44,20 +44,15 @@
           getInstruction("mashedPotatoes", 4, (step5) => {
             document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
 
-            getInstruction("mashedPotatoes", 5, (step6) => {
-              document.querySelector("#mashedPotatoes").innerHTML += `<li>${step6}</li>`;
+
+              document.querySelector("#mashedPotatoes").innerHTML += `<li>mashedPotatoesAreReady!</li>`;
                document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
-               
               
             }, (error) => console.log(error));
           }, (error) => console.log(error));
         }, (error) => console.log(error));
       }, (error) => console.log(error));
     }, (error) => console.log(error));
-  }, (error) => console.log(error));
-
-  //<li>mashedPotatoesAreReady!</li> descobrir aonde colocar 
-
 
 // Iteration 2 - using promises
 // ...
@@ -93,25 +88,55 @@ obtainInstruction('steak', 0)
                 .then( (step7) => {
                   document.querySelector("#steak").innerHTML += `<li>${step7}</li>`
 
-                  obtainInstruction('steak')
-                  .then( (step8) => {
-                    document.querySelector("#steak").innerHTML += `<li>${step8}</li>`
+              
+                    document.querySelector("#steak").innerHTML += `<li>Stake Igits Ready!</li>`
                     document.querySelector("#steakImg").removeAttribute("hidden");
 
-  //<li>StakeIgitsReady!</li> aonde colocar?
+                  }).catch(error => console.log(error));
+                }).catch(error => console.log(error));
+              }).catch(error => console.log(error));
+            }).catch(error => console.log(error));
+          }).catch(error => console.log(error));
+        }).catch(error => console.log(error));
+      }).catch(error => console.log(error));
+    }).catch(error => console.log(error));
+  
 
-                  }, (error) => console.log(error));
-                }, (error) => console.log(error));
-              }, (error) => console.log(error));
-            }, (error) => console.log(error));
-          }, (error) => console.log(error));
-        }, (error) => console.log(error));
-      }, (error) => console.log(error));
-    }, (error) => console.log(error));
+// // Iteration 3 using async/await
+// // ...
+async function makeBroccoli() {
+  try{
+    const step1 = await obtainInstruction("broccoli", 0);
+    document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`;
 
-// Iteration 3 using async/await
-// ...
+    const step2 = await obtainInstruction("broccoli", 1);
+    document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`;
 
+    const step3 = await obtainInstruction("broccoli", 2);
+    document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`;
+
+    const step4 = await obtainInstruction("broccoli", 3);
+    document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`;
+
+    const step5 = await obtainInstruction("broccoli", 4);
+    document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`;
+
+    const step6 = await obtainInstruction("broccoli", 5);
+    document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
+
+    const step7 = await obtainInstruction("broccoli", 6);
+    document.querySelector("#broccoli").innerHTML += `<li>${step7}</li>`;
+
+    document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`;
+    document.querySelector("#broccoliImg").removeAttribute("hidden");
+    
+  } catch(error) {
+  console.log(error);
+  }
+}
+makeBroccoli()
+
+// aonde colocar 
 
 // Bonus 2 - Promise all
 // ...
