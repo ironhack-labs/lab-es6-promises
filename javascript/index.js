02cdbe5b0253d2451bf012e445912ae38c9b4ed5
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-01-13 17:15:47 +0100

    finish iteration 3 and refactor iteration 2

--- javascript/index.js
+++ javascript/index.js
@@ -46,36 +46,64 @@ getInstruction('mashedPotatoes', 0, (step1) => {
   }, (error) => console.log(error))
 }, (error) => console.log(error));
 
+
 // Iteration 2 - using promises
-obtainInstruction("steak", 0).then((step1) => {
-  document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
-  obtainInstruction("steak", 1).then((step2) => {
+obtainInstruction('steak', 0)
+  .then( (step1) => {
+    document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
+    return obtainInstruction('steak', 1);
+  })
+  .then( (step2) => {
     document.querySelector("#steak").innerHTML += `<li>${step2}</li>`;
-    obtainInstruction("steak", 2).then((step3) => {
-      document.querySelector("#steak").innerHTML += `<li>${step3}</li>`;
-      obtainInstruction("steak", 3).then((step4) => {
-        document.querySelector("#steak").innerHTML += `<li>${step4}</li>`;
-        obtainInstruction("steak", 4).then((step5) => {
-          document.querySelector("#steak").innerHTML += `<li>${step5}</li>`;
-          obtainInstruction("steak", 5).then((step6) => {
-            document.querySelector("#steak").innerHTML += `<li>${step6}</li>`;
-            obtainInstruction("steak", 6).then((step7) => {
-              document.querySelector("#steak").innerHTML += `<li>${step7}</li>`;
-              obtainInstruction("steak", 7).then((step8) => {
-                document.querySelector("#steak").innerHTML += `<li>${step8}</li>`;
-                document.querySelector("#steak").innerHTML += `<li>Stake is ready!</li>`;
-                document.querySelector("#steakImg").removeAttribute("hidden");
-              });
-            });
-          });
-        });
-      });
-    });
-  });
-});
+    return obtainInstruction('steak', 2);
+  })
+  .then( (step3) => {
+    document.querySelector("#steak").innerHTML += `<li>${step3}</li>`;
+    return obtainInstruction('steak', 3);
+  })
+  .then( (step4) => {
+    document.querySelector("#steak").innerHTML += `<li>${step4}</li>`;
+    return obtainInstruction('steak', 4);
+  })
+  .then( (step5) => {
+    document.querySelector("#steak").innerHTML += `<li>${step5}</li>`;
+    return obtainInstruction('steak', 5);
+  })
+  .then( (step6) => {
+    document.querySelector("#steak").innerHTML += `<li>${step6}</li>`;
+    return obtainInstruction('steak', 6);
+  })
+  .then( (step7) => {
+    document.querySelector("#steak").innerHTML += `<li>${step7}</li>`;
+    return obtainInstruction('steak', 7);
+  })
+  .then( (step8) => {
+    document.querySelector("#steak").innerHTML += `<li>${step8}</li>`;
+    document.querySelector("#steak").innerHTML += `<li>Stake is ready!</li>`;
+    document.querySelector("#steakImg").removeAttribute("hidden");
+  })
+
 
 // Iteration 3 using async/await
-// ...
+(async function makeBroccoli() {
+    const step1 = await obtainInstruction('broccoli', 0);
+    document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`;
+    const step2 = await obtainInstruction('broccoli', 1);
+    document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`;
+    const step3 = await obtainInstruction('broccoli', 2);
+    document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`;
+    const step4= await obtainInstruction('broccoli', 3);
+    document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`;
+    const step5 = await obtainInstruction('broccoli', 4);
+    document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`;
+    const step6 = await obtainInstruction('broccoli', 5);
+    document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
+    const step7 = await obtainInstruction('broccoli', 6);
+    document.querySelector("#broccoli").innerHTML += `<li>${step7}</li>`;
+    document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`;
+    document.querySelector("#broccoliImg").removeAttribute("hidden");
+}());
+
 
 // Bonus 2 - Promise all
 // ...
