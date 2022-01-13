580c422b2a01792ffd6d9f10d14ffea138e80e4f
Oscar Sangrador <oscarsangrador@gmail.com>
2022-01-13 18:23:31 +0100

    iteration 2 done

--- javascript/index.js
+++ javascript/index.js
@@ -56,7 +56,47 @@
 
 
 // Iteration 2 - using promises
-// ...
+
+  obtainInstruction('steak', 0)
+  .then( (step0) => {
+    document.querySelector("#steak").innerHTML += `<li>${step0}</li>`;
+  });
+  obtainInstruction('steak', 1).then((step1) => {
+    document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
+    
+    obtainInstruction('steak', 2).then((step2) => {
+      document.querySelector("#steak").innerHTML += `<li>${step2}</li>`;
+
+      obtainInstruction('steak', 3).then((step3) => {
+        document.querySelector("#steak").innerHTML += `<li>${step3}</li>`;
+
+        obtainInstruction('steak', 4).then((step4) => {
+          document.querySelector("#steak").innerHTML += `<li>${step4}</li>`;
+
+          obtainInstruction('steak', 5).then((step5) => {
+            document.querySelector("#steak").innerHTML += `<li>${step5}</li>`;
+
+            obtainInstruction('steak', 6).then((step6) => {
+              document.querySelector("#steak").innerHTML += `<li>${step6}</li>`;
+
+              obtainInstruction('steak', 7).then((step7) => {
+                document.querySelector("#steak").innerHTML += `<li>${step7}</li>`;
+                document.querySelector("#steak").innerHTML += `<li> Stake is ready! </li>`;
+                document.querySelector("#steakImg").removeAttribute("hidden");
+
+
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+  
+  
+  
+
+                
 
 // Iteration 3 using async/await
 // ...
