d0ff3e0d32e0fa75fe311658e5386680028bc593
Oscar Sangrador <oscarsangrador@gmail.com>
2022-01-13 19:19:13 +0100

    iteration 3 done

--- javascript/index.js
+++ javascript/index.js
@@ -46,7 +46,6 @@
             document.querySelector("#mashedPotatoes").innerHTML += `<li> Mashed potatoes are ready! </li>`;
             document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
 
-
           }, (error) => console.log(error));
         }, (error) => console.log(error));
       }, (error) => console.log(error));
@@ -57,10 +56,9 @@
 
 // Iteration 2 - using promises
 
-  obtainInstruction('steak', 0)
-  .then( (step0) => {
+  obtainInstruction('steak', 0).then( (step0) => {
     document.querySelector("#steak").innerHTML += `<li>${step0}</li>`;
-  });
+  
   obtainInstruction('steak', 1).then((step1) => {
     document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
     
@@ -83,23 +81,53 @@
                 document.querySelector("#steak").innerHTML += `<li>${step7}</li>`;
                 document.querySelector("#steak").innerHTML += `<li> Stake is ready! </li>`;
                 document.querySelector("#steakImg").removeAttribute("hidden");
-
-
-              });
-            });
-          });
-        });
-      });
-    });
-  });
+                
+                }).catch (error => console.log(error));
+              }).catch (error => console.log(error));
+            }).catch (error => console.log(error));
+          }).catch (error => console.log(error));
+        }).catch (error => console.log(error));
+      }).catch (error => console.log(error));
+    }).catch (error => console.log(error));
+  }).catch (error => console.log(error));
   
-  
-  
-
                 
 
 // Iteration 3 using async/await
-// ...
+
+async function makeBroccoli() {
+  try{
+  const step0 = await obtainInstruction('broccoli', 0);
+  document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`;
+
+  const step1 = await obtainInstruction('broccoli', 1);
+  document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`;
+
+  const step2 = await obtainInstruction('broccoli', 2);
+  document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`;
+
+  const step3 = await obtainInstruction('broccoli', 3);
+  document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`;
+
+  const step4 = await obtainInstruction('broccoli', 4);
+  document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`;
+
+  const step5 = await obtainInstruction('broccoli', 5);
+  document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`;
+
+  const step6 = await obtainInstruction('broccoli', 6) ;
+  document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
+  document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready! </li>`;
+  document.querySelector("#broccoliImg").removeAttribute("hidden");
+  
+  } catch(err) {
+    console.error(err)
+  }};
+
+  makeBroccoli();
+
+
+
 
 // Bonus 2 - Promise all
 // ...
