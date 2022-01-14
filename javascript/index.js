3bc2d75f8190b72ce731b39394df2652be663356
Matheus Benamor <matheusqueirozbenamor@gmail.com>
2022-01-14 18:45:48 +0000

    masteriteration1-done

--- javascript/index.js
+++ javascript/index.js
@@ -28,8 +28,29 @@
 
 
 // Iteration 1 - using callbacks
-// ...
+  getInstruction("mashedPotatoes", 0, (step1) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
+    getInstruction("mashedPotatoes", 1, (step2) => {
+      document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
+      getInstruction("mashedPotatoes", 2, (step3) => {
+        document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
+        getInstruction("mashedPotatoes", 3, (step4) => {
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
+          getInstruction("mashedPotatoes", 4, (step5) => {
+            document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
+            getInstruction("mashedPotatoes", 5, (step6) => {
+              document.querySelector("#mashedPotatoes").innerHTML += `<li>${step6}</li>`;
+              getInstruction("mashedPotatoes", 6, (step7) => {
+                document.querySelector("#mashedPotatoes").innerHTML += `<li>${step7}</li>`;
+              }, (error) => console.log(error))
+            }, (error) => console.log(error))
+          }, (error) => console.log(error))
+        }, (error) => console.log(error))
+      }, (error) => console.log(error))
+    }, (error) => console.log(error))
+  }, (error) => console.log(error))
 
+  
 // Iteration 2 - using promises
 // ...
 
