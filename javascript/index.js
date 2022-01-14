f0331ca82de24dc503ca095c0208185629393966
Pablo Mateos <pmateos805@gmail.com>
2022-01-14 20:25:22 +0100

    iteration 2

--- javascript/index.js
+++ javascript/index.js
@@ -25,8 +25,6 @@
     document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
   }, (error) => console.log(error)); */
 
-
-
 // Iteration 1 - using callbacks
 getInstruction('mashedPotatoes', 0, (step0) => {
   document.querySelector("#mashedPotatoes").innerHTML += `<li>${step0}</li>`;
@@ -47,7 +45,18 @@ getInstruction('mashedPotatoes', 0, (step0) => {
 }, (error) => console.log(error));
 
 // Iteration 2 - using promises
-// ...
+obtainInstruction('steak', 0)
+  .then((step0) => document.querySelector("#steak").innerHTML += `<li>${step0}</li>`)
+  .then(()=> obtainInstruction('steak', 1).then((step1) => document.querySelector("#steak").innerHTML += `<li>${step1}</li>`))
+  .then(()=> obtainInstruction('steak', 2).then((step2)=> document.querySelector("#steak").innerHTML += `<li>${step2}</li>`))
+  .then(()=> obtainInstruction('steak', 3).then((step3)=> document.querySelector("#steak").innerHTML += `<li>${step3}</li>`))
+  .then(()=> obtainInstruction('steak', 4).then((step4)=> document.querySelector("#steak").innerHTML += `<li>${step4}</li>`))
+  .then(()=> obtainInstruction('steak', 5).then((step5)=> document.querySelector("#steak").innerHTML += `<li>${step5}</li>`))
+  .then(()=> obtainInstruction('steak', 6).then((step6)=> document.querySelector("#steak").innerHTML += `<li>${step6}</li>`))
+  .then(()=> obtainInstruction('steak', 7).then((step7)=> document.querySelector("#steak").innerHTML += `<li>${step7}</li>`))
+  .finally(()=> document.querySelector("#steak").innerHTML += `<li>${'Stake is ready!'}</li>`)
+
+  // ... Your code here
 
 // Iteration 3 using async/await
 // ...
