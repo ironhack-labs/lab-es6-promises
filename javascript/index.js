8ec3aee303ee338f38a3c4d6150f1a11df46bca8
David Vekony <dave.vekony@gmail.com>
2022-01-24 14:22:17 +0000

    iteration 1 done

--- javascript/index.js
+++ javascript/index.js
@@ -28,8 +28,32 @@
 // Iteration 1 - using callbacks
 getInstruction("mashedPotatoes", 0, (step0) => {
   document.querySelector("#mashedPotatoes").innerHTML += `<li>${step0}</li>`;
-  // ... Your code here
-  // ...
+  getInstruction("mashedPotatoes", 1, (step1) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
+    getInstruction("mashedPotatoes", 2, (step2) => {
+      document.querySelector(
+        "#mashedPotatoes"
+      ).innerHTML += `<li>${step2}</li>`;
+      getInstruction("mashedPotatoes", 3, (step3) => {
+        document.querySelector(
+          "#mashedPotatoes"
+        ).innerHTML += `<li>${step3}</li>`;
+        getInstruction("mashedPotatoes", 4, (step4) => {
+          document.querySelector(
+            "#mashedPotatoes"
+          ).innerHTML += `<li>${step4}</li>`;
+          getInstruction("mashedPotatoes", 5, (step5) => {
+            document.querySelector(
+              "#mashedPotatoes"
+            ).innerHTML += `<li>Mashed potatoes are ready!</li>`;
+            document
+              .querySelector("#mashedPotatoesImg")
+              .removeAttribute("hidden");
+          });
+        });
+      });
+    });
+  });
 });
 
 // Iteration 2 - using promises
