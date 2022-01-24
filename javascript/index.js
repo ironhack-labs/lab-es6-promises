9b3f67a5ccb02df296d860f26babcf45e800c178
Clara Villa <Clara.Villa@lego.com>
2022-01-24 16:12:42 +0000

    bonus 2

--- javascript/index.js
+++ javascript/index.js
@@ -153,3 +153,22 @@ makeBroccoli();
 
 // Bonus 2 - Promise all
 // ...
+
+
+const step0 = obtainInstruction("brusselsSprouts",0);
+const step1 = obtainInstruction("brusselsSprouts",1);
+const step2 = obtainInstruction("brusselsSprouts",2);
+const step3 = obtainInstruction("brusselsSprouts",3);
+const step4 = obtainInstruction("brusselsSprouts",4);
+const step5 = obtainInstruction("brusselsSprouts",5);
+const step6 = obtainInstruction("brusselsSprouts",6);
+const step7 = obtainInstruction("brusselsSprouts",7);
+
+Promise.all([step0,step1,step2,step3,step4,step5,step6,step7])
+.then ((values)=>{values.forEach((eachValue)=>{
+  document.querySelector("#brusselsSprouts").innerHTML+=`<li>${eachValue}</li>`
+})
+document.querySelector("#brusselsSprouts").innerHTML+=`<li>Brussels sprouts are ready!</li>`;
+document.getElementById("brusselsSproutsImg").removeAttribute("hidden");
+})
+
