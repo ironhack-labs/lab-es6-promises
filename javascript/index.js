cfc23e4ed9134c87d9d7f17373f7ceda16012c0d
cschnitzel10 <boese.caroline@gmail.com>
2022-01-25 21:50:02 +0100

    Caroline PT

--- javascript/index.js
+++ javascript/index.js
@@ -28,13 +28,63 @@
 
 
 // Iteration 1 - using callbacks
-// ...
+getInstruction('mashedPotatoes', 0, (step0) => {
+  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step0}</li>`;
+  getInstruction('mashedPotatoes', 1, (step1) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
+    getInstruction('mashedPotatoes', 2, (step2) => {
+      document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
+      getInstruction('mashedPotatoes', 3, (step3) => {
+        document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
+        getInstruction('mashedPotatoes', 4, (step4) => {
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
+      }, (error) => console.log (error))
+    }, (error) => console.log (error))
+  }, (error) => console.log (error))
+}, (error) => console.log (error))
+}, (error) => console.log (error))
+ 
 
 // Iteration 2 - using promises
-// ...
+
+optainInstruction ('steak', 0)
+.then (step0 => {
+  document.querySelector("#steak").innerHTML += `<li>${step0}</li>`;
+return obtainInstruction('steak', 1)
+})
+.then((step1 => {
+  document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
+return obtainInstruction('steak', 2)
+})
+.then((step2 => {
+  document.querySelector("#steak").innerHTML += `<li>${step2}</li>`;
+return obtainInstruction('steak', 3)
+})
+.then((step3 => {
+  document.querySelector("#steak").innerHTML += `<li>${step3}</li>`;
+return obtainInstruction('steak', 4)
+})
+.then((step4 => {
+  document.querySelector("#steak").innerHTML += `<li>${step4}</li>`;
+})
+.catch(error => console.log(error))
 
 // Iteration 3 using async/await
-// ...
+
+const makeBroccoli = async () => {
+try { 
+  const step0 = await obtainInstruction ('broccoli', 0);
+  const step1 = await obtainInstruction ('broccoli', 1);
+  const step2 = await obtainInstruction ('broccoli', 2);
+  const step3 = await obtainInstruction ('broccoli', 3);
+  const step4 = await obtainInstruction ('broccoli', 4);
+}
+
+catch (error) {
+console.log(error)
+}
+
+}
 
 // Bonus 2 - Promise all
 // ...
