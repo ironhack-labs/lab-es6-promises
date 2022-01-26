27d3c0a3c1cc77c8ca47aef34f581405d95d8471
Olena Kraemer <olenakraem@gmail.com>
2022-01-26 21:50:25 +0100

    done BER-PT-22 Krämer

--- javascript/index.js
+++ javascript/index.js
@@ -149,3 +149,27 @@ async function makeBroccoli() {
 }
 
 makeBroccoli();
+
+// Bonus 2 - Promise all
+
+const promises = [
+  obtainInstruction("brusselsSprouts", 0),
+  obtainInstruction("brusselsSprouts", 1),
+  obtainInstruction("brusselsSprouts", 2),
+  obtainInstruction("brusselsSprouts", 3),
+  obtainInstruction("brusselsSprouts", 4),
+  obtainInstruction("brusselsSprouts", 5),
+  obtainInstruction("brusselsSprouts", 6),
+  obtainInstruction("brusselsSprouts", 7),
+];
+
+async function makeSprouts() {
+  const steps = await Promise.all(promises).then();
+
+  steps.forEach((step) => {
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>${step}</li>`;
+  });
+  document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
+}
+
+makeSprouts();
