c8b959307892d0c705e2551df67d34332cd714c5
Lamer217 <males.public@gmail.com>
2022-02-05 19:41:28 +0100

    Do iteartion 1

--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,7 @@
 // 🚨🚨🚨 Comment out the below code before you start working on the code
 
 // Out of sync
-  getInstruction("mashedPotatoes", 0, (step1) => {
+/*   getInstruction("mashedPotatoes", 0, (step1) => {
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
   }, (error) => console.log(error));
   
@@ -22,13 +22,66 @@
   
   getInstruction("mashedPotatoes", 4, (step5) => {
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
-    document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
-  }, (error) => console.log(error));
-
-
+    document.querySelector("#mashedPotatoesImg").removeAttribute("hidden"); 
+  }, (error) => console.log(error));*/
 
 // Iteration 1 - using callbacks
-// ...
+getInstruction(
+  'mashedPotatoes',
+  0,
+  (step0) => {
+    document.querySelector('#mashedPotatoes').innerHTML += `<li>${step0}</li>`;
+    // ... Your code here
+    // ...
+    getInstruction(
+      'mashedPotatoes',
+      1,
+      (step1) => {
+        document.querySelector(
+          '#mashedPotatoes'
+        ).innerHTML += `<li>${step1}</li>`;
+        getInstruction(
+          'mashedPotatoes',
+          2,
+          (step2) => {
+            document.querySelector(
+              '#mashedPotatoes'
+            ).innerHTML += `<li>${step2}</li>`;
+            getInstruction(
+              'mashedPotatoes',
+              3,
+              (step3) => {
+                document.querySelector(
+                  '#mashedPotatoes'
+                ).innerHTML += `<li>${step3}</li>`;
+                getInstruction(
+                  'mashedPotatoes',
+                  4,
+                  (step4) => {
+                    document.querySelector(
+                      '#mashedPotatoes'
+                    ).innerHTML += `<li>${step4}</li>`;
+                    document
+                      .querySelector('#mashedPotatoesImg')
+                      .removeAttribute('hidden');
+                    document.querySelector(
+                      '#mashedPotatoes'
+                    ).innerHTML += `<li>Mashed potatoes are ready!</li>`;
+                  },
+                  (error) => console.log(error)
+                );
+              },
+              (error) => console.log(error)
+            );
+          },
+          (error) => console.log(error)
+        );
+      },
+      (error) => console.log(error)
+    );
+  },
+  (error) => console.log(error)
+);
 
 // Iteration 2 - using promises
 // ...
