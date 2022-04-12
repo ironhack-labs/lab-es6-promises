2e57e7feb7e38f46331f2bda403a692352ccfcbe
aBoswell <A.B0ZZW3LL@gmail.com>
2022-04-12 10:11:30 -0400

    made new iteration on makeBroccoli function following walk-through

--- javascript/index.js
+++ javascript/index.js
@@ -43,7 +43,7 @@ getInstruction("mashedPotatoes", 0, (step1) => {
           getInstruction("mashedPotatoes", 4, (step5) => {
             document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
             document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
-            document.querySelector("#mashedPotatoes").innerHTML += `<li>'Mashed Potatoes Are Ready!</li>`;
+            document.querySelector("#mashedPotatoes").innerHTML += `<li>'Mashed potatoes are ready!</li>`;
 
           }, (error) => console.log(error));
         }, (error) => console.log(error));
@@ -77,29 +77,40 @@ obtainInstruction('steak', 0)
   .then( (step6) => {document.querySelector("#steak").innerHTML += `<li>${step6}</li>`})
 
   .then(() => obtainInstruction('steak', 7) 
-  .then( (step7) => {document.querySelector("#steak").innerHTML += `<li>${step7}</li>`
-
-  .finally(document.querySelector("#steak").innerHTML += `<li>'Steak is ready!'</li>`)
-  document.querySelector("#mashedPotatoesImg").removeAttribute("hidden")
+  .then( (step7) => {
+    document.querySelector("#steak").innerHTML += `<li>${step7}</li>`
+    document.querySelector("#steak").innerHTML += `<li>'Steak is ready!'</li>`
+    document.querySelector("#steakImg").removeAttribute("hidden");
 
 }))))))));
 
 // Iteration 3 - Async & Await..
 
-// async function makeBroccoli() {
-//   await obtainInstruction('broccoli', 0, (step0) => {document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`});
-//   await obtainInstruction('broccoli', 1, (step1) => {document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`});
-//   await obtainInstruction('broccoli', 2, (step2) => {document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`});
-//   await obtainInstruction('broccoli', 3, (step3) => {document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`});
-//   await obtainInstruction('broccoli', 4, (step4) => {document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`});
-//   await obtainInstruction('broccoli', 5, (step5) => {document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`});
-// };
-
-// makeBroccoli() 
-
 async function makeBroccoli() {
-  await obtainInstruction('broccoli', 0); 
-  await((step0) => {document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`});
+  try{
+
+    let step0 = await obtainInstruction('broccoli', 0); 
+    document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`;
+    let step1 = await obtainInstruction('broccoli', 1); 
+    document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`;
+    let step2 = await obtainInstruction('broccoli', 2); 
+    document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`;
+    let step3 = await obtainInstruction('broccoli', 3); 
+    document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`;
+    let step4 = await obtainInstruction('broccoli', 4); 
+    document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`;
+    let step5 = await obtainInstruction('broccoli', 5); 
+    document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`;
+    let step6 = await obtainInstruction('broccoli', 6); 
+    document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
+    document.querySelector("#broccoli").innerHTML += `<li>'Broccoli is ready!</li>`;
+    document.querySelector("#broccoliImg").removeAttribute("hidden");  
+
+//The above could also have been a for loop - see walk-through demo.
+
+  } catch(error){
+    console.log(error);
+  }
 };
 
 makeBroccoli()  
