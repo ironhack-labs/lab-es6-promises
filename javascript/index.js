fcbc8ac2b88c31e7165961e3d317ae9d47172695
jidekickpush <jeandenisdrane@gmail.com>
2022-04-18 18:08:20 +0200

    All done by Pedro and Jide

--- javascript/index.js
+++ javascript/index.js
@@ -83,10 +83,58 @@ obtainInstruction('steak', 0)
   .catch((error) => {
         console.log('something went wrong', error);
   });
-  
+
 
 // Iteration 3 using async/await try {}catch{}
 // ...
+async function makeBroccoli() {
+  // ... Your code here
+  try {
+    const step0 = await obtainInstruction('broccoli', 0);
+    document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`;
+    const step1 = await obtainInstruction('broccoli', 1);
+    document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`;
+    const step2 = await obtainInstruction('broccoli', 2);
+    document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`;
+    const step3 = await obtainInstruction('broccoli', 3);
+    document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`;
+    const step4 = await obtainInstruction('broccoli', 4);
+    document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`;
+    const step5 = await obtainInstruction('broccoli', 5);
+    document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`;
+    const step6 = await obtainInstruction('broccoli', 6);
+    document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
+    document.querySelector("#broccoli").innerHTML += `<li>The broccoli is ready!</li>`;
+    document.querySelector("#broccoliImg").removeAttribute('hidden');
+
+  } catch {
+    (error) => {
+      console.log('something went wrong', error);
+    }
+  }
+}
+makeBroccoli();
 
 // Bonus 2 - Promise all
 // ...
+
+Promise.all([obtainInstruction('brusselsSprouts', 0),
+              obtainInstruction('brusselsSprouts', 1),
+              obtainInstruction('brusselsSprouts', 2),
+              obtainInstruction('brusselsSprouts', 3),
+              obtainInstruction('brusselsSprouts', 4),
+              obtainInstruction('brusselsSprouts', 5),
+              obtainInstruction('brusselsSprouts', 6),
+              obtainInstruction('brusselsSprouts', 7)])
+  .then((instructions) => {
+    instructions.forEach((step) => {
+      document.querySelector("#brusselsSprouts").innerHTML += `<li>${step}</li>`;
+      document.querySelector("#brusselsSproutsImg").removeAttribute('hidden');
+    });
+  })
+  .then(()=>{
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>The brusselsSprouts are ready!</li>`;
+  })
+  .catch ((error) => {
+    console.log('something went wrong', error);
+  });
