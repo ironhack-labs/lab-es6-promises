5db0ffe251177523fde6af2b59d00c57dd1c2e8d
emmanuelsolis <emma.ruso@outlook.com>
2022-05-25 15:03:18 -0500

    Iteration 2 solved

--- javascript/index.js
+++ javascript/index.js
@@ -49,10 +49,30 @@
   }, (error) => {console.log(error)}
   );
 
-
 // ...
 
 // Iteration 2 - using promises
+obtainInstruction('steak', 0)
+.then(step0 => {document.querySelector("#steak").innerHTML += `<li>${step0}</li>`;
+  return obtainInstruction('steak', 1)})
+.then(step1 => {document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
+  return obtainInstruction('steak', 2)})
+.then(step3 => {document.querySelector("#steak").innerHTML += `<li>${step3}</li>`;
+  return obtainInstruction('steak', 3)})
+.then(step4 => {document.querySelector("#steak").innerHTML += `<li>${step4}</li>`;
+  return obtainInstruction('steak', 4)})
+.then(step5 => {document.querySelector("#steak").innerHTML += `<li>${step5}</li>`;
+  return obtainInstruction('steak', 5)})
+.then(step6 => {document.querySelector("#steak").innerHTML += `<li>${step6}</li>`;
+  return obtainInstruction('steak', 6)})
+.then(step7 => {document.querySelector("#steak").innerHTML += `<li>${step7}</li>`;
+  return obtainInstruction('steak', 7)})
+.then(step8 => {document.querySelector("#steak").innerHTML += `<li>Stake is ready!</li>`;
+document.querySelector("#steakImg").removeAttribute("hidden");
+return obtainInstruction('steak', 8)})
+.catch(error => console.log(error));
+
+  
 // ...
 
 // Iteration 3 using async/await
