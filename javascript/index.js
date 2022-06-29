629daea943a3147fdff8eab0c854a187b487e3dc
SamPetherick1898 <fuentessamuelignacio@gmail.com>
2022-06-29 17:29:25 -0500

    promises

--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,7 @@
 
 // 🚨🚨🚨 Comment out the below code before you start working on the code
 
-// Out of sync
+/* Out of sync
   getInstruction("mashedPotatoes", 0, (step1) => {
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
   }, (error) => console.log(error));
@@ -25,16 +25,127 @@
     document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
   }, (error) => console.log(error));
 
-
+*/
 
 // Iteration 1 - using callbacks
-// ...
+
+getInstruction('mashedPotatoes', 0, (step0) => {
+  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step0}</li>`
+  
+  getInstruction("mashedPotatoes", 1, (step2) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
+
+    getInstruction("mashedPotatoes", 2, (step3) => {
+      document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
+
+      getInstruction("mashedPotatoes", 3, (step4) => {
+        document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
+
+        getInstruction("mashedPotatoes", 4, (step5) => {
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>${"Mashed potatoes are ready!"}</li>`;
+          document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
+        }, (error) => console.log(error));
+
+      }, (error) => console.log(error));
+
+    }, (error) => console.log(error));
+
+  }, (error) => console.log(error));
+
+});
 
 // Iteration 2 - using promises
-// ...
+
+
+obtainInstruction('steak', 0)
+  .then( (step0) => {
+    document.querySelector("#steak").innerHTML += `<li>${step0}</li>`
+    return obtainInstruction('steak', 1)
+  })
+
+  .then( (step1) => {
+    document.querySelector("#steak").innerHTML += `<li>${step1}</li>`
+    return obtainInstruction('steak', 2)
+  })
+
+  .then( (step2) => {
+    document.querySelector("#steak").innerHTML += `<li>${step2}</li>`
+    return obtainInstruction('steak', 3)
+  })
+
+  .then( (step3) => {
+    document.querySelector("#steak").innerHTML += `<li>${step3}</li>`
+    return obtainInstruction('steak', 4)
+  })
+
+  .then( (step4) => {
+    document.querySelector("#steak").innerHTML += `<li>${step4}</li>`
+    return obtainInstruction('steak', 5)
+  })
+
+  .then( (step5) => {
+    document.querySelector("#steak").innerHTML += `<li>${step5}</li>`
+    return obtainInstruction('steak', 6)
+  })
+
+  .then( (step6) => {
+    document.querySelector("#steak").innerHTML += `<li>${step6}</li>`
+    return obtainInstruction('steak', 7)
+  })
+
+  .then( (step7) => {
+    document.querySelector("#steak").innerHTML += `<li>${'Stake is ready!'}</li>`
+    document.querySelector("#steakImg").removeAttribute("hidden");
+  })
+
+
+obtainInstruction()
 
 // Iteration 3 using async/await
-// ...
+
+async function makeBroccoli() {
+ try{
+  let step0 = await obtainInstruction('broccoli', 0);  
+  document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`
+
+  let step1 = await obtainInstruction('broccoli', 1);  
+    document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`
+
+    let step2 = await obtainInstruction('broccoli', 2);  
+    document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`
+    
+    let step3 = await obtainInstruction('broccoli', 3);  
+    document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`
+
+    let step4 = await obtainInstruction('broccoli', 4);  
+    document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`
+    
+    let step5 = await obtainInstruction('broccoli', 5);  
+    document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`
+
+    let step6 = await obtainInstruction('broccoli', 6);
+    document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`
+    document.querySelector("#broccoli").innerHTML += `<li>${"broccoli is ready!"}</li>`
+    document.querySelector("#broccoliImg").removeAttribute("hidden");
+ }
+ catch(error){
+  console.log("Nom puedem serm")
+ }
+}
+
+makeBroccoli();
+
 
 // Bonus 2 - Promise all
-// ...
+
+
+
+
+/*
+
+Using Promise.all display the cooking steps to make Brussels Sprouts in the correct order.
+
+After the last step, you should display an additional <li> with the text: Brussels sprouts are ready!.
+
+The final result should look like this - with all the cooking steps displaying in the correct order:
+*/
