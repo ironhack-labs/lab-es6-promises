1818b188f8121b487607e3ffb82cdc95d8a048d7
Txell <meritxell@gold.barcelona>
2022-07-19 21:46:40 +0200

    bonus 2

--- javascript/index.js
+++ javascript/index.js
@@ -92,7 +92,48 @@ obtainInstruction('steak', 0)
   .catch((error) => console.log(error));
 
 // Iteration 3 using async/await
-// ...
+async function makeBroccoli() {
+  // ... Your code here
+  try {
+    const step1 = await obtainInstruction('broccoli', 0)
+    document.querySelector('#broccoli'.innerHTML += `<li>${step1}</li>`)
+    const step2 = await obtainInstruction('broccoli', 1)
+    document.querySelector('#broccoli'.innerHTML += `<li>${step2}</li>`)
+    const step3 = await obtainInstruction('broccoli', 2)
+    document.querySelector('#broccoli'.innerHTML += `<li>${step3}</li>`)
+    const step4 = await obtainInstruction('broccoli', 3)
+    document.querySelector('#broccoli'.innerHTML += `<li>${step4}</li>`)
+    const step5 = await obtainInstruction('broccoli', 4)
+    document.querySelector('#broccoli'.innerHTML += `<li>${step5}</li>`)
+    const step6 = await obtainInstruction('broccoli', 5)
+    document.querySelector('#broccoli'.innerHTML += `<li>${step6}</li>`)
+    const step7 = await obtainInstruction('broccoli', 6)
+    document.querySelector('#broccoli'.innerHTML += `<li>${step7}</li>`)
+    document.querySelector('#broccoli'.innerHTML += `<li>Broccoli is ready!</li>`)
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 // Bonus 2 - Promise all
-// ...
+function makeBrusselsSprouts(){
+  const promises = [
+    obtainInstruction('brusselsSprouts', 0),
+    obtainInstruction('brusselsSprouts', 1),
+    obtainInstruction('brusselsSprouts', 2),
+    obtainInstruction('brusselsSprouts', 3),
+    obtainInstruction('brusselsSprouts', 4),
+    obtainInstruction('brusselsSprouts', 5),
+    obtainInstruction('brusselsSprouts', 6),
+    obtainInstruction('brusselsSprouts', 7)
+  ];
+  
+  Promise.all(promises)
+  .then(result => {
+    for(value of result){
+      document.querySelector('#brusselsSprouts').innerHTML += `<li>${value}<li>`
+    }
+    document.querySelector('#brusselsSprouts').innerHTML += '<li>Brussels sprouts are ready!<li>'
+  })
+  .catch(err => console.error(err));
+}
