6d2451bbb83aa65dbdcada2d31e3e37939291921
Maria Cassandra <maria.cassandra@gmail.com>
2022-07-21 14:14:40 +0200

    done

--- javascript/index.js
+++ javascript/index.js
@@ -48,43 +48,43 @@ getInstruction('mashedPotatoes', 0, (step0) => {
 obtainInstruction("steak", 0)
   .then((step0) => {
     document.querySelector("#steak").innerHTML += `<li>${step0}</li>`;
+    obtainInstruction("steak", 1)
+      .then((step1) => {
+        document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
+        obtainInstruction("steak", 2)
+          .then((step2) => {
+            document.querySelector("#steak").innerHTML += `<li>${step2}</li>`;
+            obtainInstruction("steak", 3)
+              .then((step3) => {
+                document.querySelector("#steak").innerHTML += `<li>${step3}</li>`;
+                obtainInstruction("steak", 4)
+                  .then((step4) => {
+                    document.querySelector("#steak").innerHTML += `<li>${step4}</li>`;
+                    obtainInstruction("steak", 5)
+                      .then((step5) => {
+                        document.querySelector("#steak").innerHTML += `<li>${step5}</li>`;
+                        obtainInstruction("steak", 6)
+                          .then((step6) => {
+                            document.querySelector("#steak").innerHTML += `<li>${step6}</li>`;
+                            obtainInstruction("steak", 7)
+                              .then((step7) => {
+                                document.querySelector("#steak").innerHTML += `<li>${step7}</li>`;
+                              })
+                              .catch((error) => console.log(error));
+                          })
+                          .catch((error) => console.log(error));
+                      })
+                      .catch((error) => console.log(error));
+                  })
+                  .catch((error) => console.log(error));
+              })
+              .catch((error) => console.log(error));
+          })
+          .catch((error) => console.log(error));
+      })
+      .catch((error) => console.log(error));
   })
-  .then(() => {
-    return obtainInstruction("steak", 1).then((step) => {
-      document.querySelector("#steak").innerHTML += `<li>${step}</li>`;
-    });
-  })
-  .then(() => {
-    return obtainInstruction("steak", 2).then((step) => {
-      document.querySelector("#steak").innerHTML += `<li>${step}</li>`;
-    });
-  })
-  .then(() => {
-    return obtainInstruction("steak", 3).then((step) => {
-      document.querySelector("#steak").innerHTML += `<li>${step}</li>`;
-    });
-  })
-  .then(() => {
-    return obtainInstruction("steak", 4).then((step) => {
-      document.querySelector("#steak").innerHTML += `<li>${step}</li>`;
-    });
-  })
-  .then(() => {
-    return obtainInstruction("steak", 5).then((step) => {
-      document.querySelector("#steak").innerHTML += `<li>${step}</li>`;
-    });
-  })
-  .then(() => {
-    return obtainInstruction("steak", 6).then((step) => {
-      document.querySelector("#steak").innerHTML += `<li>${step}</li>`;
-    });
-  })
-  .then(() => {
-    return obtainInstruction("steak", 7).then((step) => {
-      document.querySelector("#steak").innerHTML += `<li>${step}</li>`;
-    });
-  })
- 
+
 // Iteration 3 using async/await
 makeBroccoli();
 async function makeBroccoli() {
@@ -94,7 +94,11 @@ async function makeBroccoli() {
     });
   }
   document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`;
+  document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
+  document.querySelector("#steakImg").removeAttribute("hidden");
+  document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
   document.querySelector("#broccoliImg").removeAttribute("hidden");
+
 }
 
 // Bonus 1 - Promise all
@@ -103,3 +107,13 @@ async function makeBroccoli() {
 
 // Bonus 2 - Promise all
 //Using Promise.all display the cooking steps to make Brussels Sprouts in the correct order. After the last step, you should display an additional <li> with the text: Brussels sprouts are ready!
+Promise.all(promises)
+  .then(result => {
+    for (value of result) {
+      console.log(value)
+    }
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>Brussels sprouts are ready!</li>`;
+  })
+  .catch(error => {
+    console.log(error)
+  })
