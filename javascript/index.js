e2db15c11b3998996e41e3677e1a706096690efb
João Serafim <serafim.joao@gmail.com>
2022-08-31 22:47:21 +0100

    Done iteration 1.

--- javascript/index.js
+++ javascript/index.js
@@ -26,7 +26,27 @@
 // }, (error) => console.log(error));
 
 // Iteration 1 - using callbacks
-
+getInstruction("mashedPotatoes", 0, (step0) => {
+  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step0}</li>`;
+  getInstruction("mashedPotatoes", 1, (step1) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
+    getInstruction("mashedPotatoes", 2, (step2) => {
+      document.querySelector(
+        "#mashedPotatoes"
+      ).innerHTML += `<li>${step2}</li>`;
+      getInstruction("mashedPotatoes", 3, (step3) => {
+        document.querySelector(
+          "#mashedPotatoes"
+        ).innerHTML += `<li>${step3}</li>`;
+        getInstruction("mashedPotatoes", 4, (step4) => {
+          document.querySelector(
+            "#mashedPotatoes"
+          ).innerHTML += `<li>${step4}</li>`;
+        });
+      });
+    });
+  });
+});
 // ...
 
 // Iteration 2 - using promises
