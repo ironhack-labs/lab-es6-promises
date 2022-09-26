014f7c3ee351a76661efff3c31479632a5df7bf9
Julia Spriggs <ironhack@julia-spriggs.fr>
2022-09-26 17:15:04 +0200

    finished iteration 2

--- javascript/index.js
+++ javascript/index.js
@@ -45,10 +45,44 @@ getInstruction('mashedPotatoes', 0, (step0) => {
     });
 });
 
-          //dom manipulation for step 5
-
 // Iteration 2 - using promises (obtainInstructions) => pass food as string
-// ...
+
+obtainInstruction('steak', 0)
+  .then( (step0) => {
+    document.querySelector("#steak").innerHTML += `<li>${step0}</li>`
+    return obtainInstruction('steak', 0)
+  })
+  .then( (step1) => {
+    document.querySelector("#steak").innerHTML += `<li>${step1}</li>`
+    return obtainInstruction('steak', 1)
+  })
+  .then( (step2) => {
+    document.querySelector("#steak").innerHTML += `<li>${step2}</li>`
+    return obtainInstruction('steak', 2)
+  })
+  .then( (step3) => {
+    document.querySelector("#steak").innerHTML += `<li>${step3}</li>`
+    return obtainInstruction('steak', 3)
+  })
+  .then( (step4) => {
+    document.querySelector("#steak").innerHTML += `<li>${step4}</li>`
+    return obtainInstruction('steak', 4)
+  })
+  .then( (step5) => {
+    document.querySelector("#steak").innerHTML += `<li>${step5}</li>`
+    return obtainInstruction('steak', 5)
+  })
+  .then( (step6) => {
+    document.querySelector("#steak").innerHTML += `<li>${step6}</li>`
+    return obtainInstruction('steak', 6)
+  })
+  .then( (step7) => {
+    document.querySelector("#steak").innerHTML += `<li>${step7}</li>`
+    return obtainInstruction('steak', 0)
+  })
+  .then ( (step8) => {
+  document.querySelector("#steak").innerHTML += `<li>Steak is ready!</li>`
+  })
 
 // Iteration 3 using async/await
 // ...
