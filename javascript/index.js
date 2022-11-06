7b3e00bdf0f8320c47009e320eeed794d3dd55a8
Miguel Quiaios <miguelquiaios12@gmail.com>
2022-11-06 16:36:33 +0000

    lab update

--- javascript/index.js
+++ javascript/index.js
@@ -52,22 +52,64 @@ getInstruction('mashedPotatoes', 0, (step0) =>{
 // Itmas9eration 2 - using promises
 
 obtainInstruction('steak', 0)
-  .then((step0) => {document.querySelector('#mashedPotatoes').innerHTML += `<li>${step0}</li>`});
-  obtainInstruction('steak', 1)
-  .then((step1) => {document.querySelector('#mashedPotatoes').innerHTML += `<li>${step1}</li>`})
-  obtainInstruction('steak', 2)
-  .then((step2) => {document.querySelector('#mashedPotatoes').innerHTML += `<li>${step2}</li>`})
-  obtainInstruction('steak', 3)
-  .then((step3) => {document.querySelector('#mashedPotatoes').innerHTML += `<li>${step3}</li>`})
-  obtainInstruction('steak', 4)
-  .then((step4) => {document.querySelector('#mashedPotatoes').innerHTML += `<li>${step4}</li>`})
-  obtainInstruction('steak', 5)
-  .then((step5) => {document.querySelector('#mashedPotatoes').innerHTML += `<li>${step5}</li>`})
-  obtainInstruction('steak', 6)
-  .then((step6) => {document.querySelector('#mashedPotatoes').innerHTML += `<li>${step6}</li>`})
+.then((step0) =>{
+  document.getElementById('steak').innnerHTML += `<li>${step0}</li>`
+  return obtainInstruction('steak', 1)
+})
+.then((step1) =>{
+  document.getElementById('steak').innnerHTML += `<li>${step1}</li>`
+  return obtainInstruction('steak', 2)
+})
+.then((step2) =>{
+  document.getElementById('steak').innnerHTML += `<li>${step2}</li>`
+  return obtainInstruction('steak', 3)
+})
+.then((step3) =>{
+  document.getElementById('steak').innnerHTML += `<li>${step3}</li>`
+  return obtainInstruction('steak', 4)
+})
+.then((step4) =>{
+  document.getElementById('steak').innnerHTML += `<li>${step4}</li>`
+  return obtainInstruction('steak', 5)
+})
+.then((step5) =>{
+  document.getElementById('steak').innnerHTML += `<li>${step5}</li>`
+  return obtainInstruction('steak', 6)
+})
+.then((step6) =>{
+  document.getElementById('steak').innnerHTML += `<li>${step6}</li>`
+  return obtainInstruction('steak', 7)
+})
+.then((step7) =>{
+  document.getElementById('steak').innnerHTML += `<li>${step7}</li>`
+})
+.catch((error) => console.log(error))
+.finally(() => {
+let final = document.createElement("li");
+document.getElementById('steak').appendChild(final).innerHTML += 'Stake is ready!!'
+document.getElementById('steakImg').removeAttribute('hidden')
+})
 
 // Iteration 3 using async/await
-// ...
+let makeBrocoli = async () => {
+  try{
+    await obtainInstruction('broccoli', 0).then((step0) => document.getElementById('broccoli').innerHTML += `<li>${step0}</li>`)
+    await obtainInstruction('broccoli', 0).then((step0) => document.getElementById('broccoli').innerHTML += `<li>${step0}</li>`)
+    await obtainInstruction('broccoli', 0).then((step0) => document.getElementById('broccoli').innerHTML += `<li>${step0}</li>`)
+    await obtainInstruction('broccoli', 0).then((step0) => document.getElementById('broccoli').innerHTML += `<li>${step0}</li>`)
+    await obtainInstruction('broccoli', 0).then((step0) => document.getElementById('broccoli').innerHTML += `<li>${step0}</li>`)
+    await obtainInstruction('broccoli', 0).then((step0) => document.getElementById('broccoli').innerHTML += `<li>${step0}</li>`)
+    await obtainInstruction('broccoli', 0).then((step0) => document.getElementById('broccoli').innerHTML += `<li>${step0}</li>`)
+    await obtainInstruction('broccoli', 0).then((step0) => document.getElementById('broccoli').innerHTML += `<li>${step0}</li>`)
+  } catch(error){
+    console.log(error);
+  
+  } finally{
+    let final = document.createElement("li");
+    document.getElementById('broccoli').appendChild(final).innerHTML += 'Broccoli is ready!!'
+    document.getElementById('broccoliImg').removeAttribute('hidden')
+}
+  }
+  makeBrocoli()
 
-// Bonus 2 - Promise all
-// ...
+
