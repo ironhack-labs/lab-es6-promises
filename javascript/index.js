d5adc188f035068fae12033c5021c2eda6d77c35
David Hatch <hatchdavidv@gmail.com>
2022-11-16 15:40:01 -0600

    iteration1 callback hell

--- javascript/index.js
+++ javascript/index.js
@@ -4,6 +4,7 @@
 // 🚨🚨🚨 Comment out the below code before you start working on the code
 
 // Out of sync
+
   getInstruction("mashedPotatoes", 0, (step1) => {
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
   }, (error) => console.log(error));
@@ -37,11 +38,19 @@
         })
     })
 }, (error)=>{console.error(error)})  */
-getInstruction('mashedPotatoes', 0, ()=>{
-  getDirections(1, ()=>{
-      getDirections(2, ()=>{
-          getDirections(3, ()=>{
-              getDirections(4, ()=>{}, (error)=>{console.error(error)})
+getInstruction('mashedPotatoes', 0, (step1)=>{
+  console.log('Probando step1');
+  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`
+  getInstruction('mashedPotatoes',1, (step2)=>{
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`  
+    getInstruction('mashedPotatoes',2, (step3)=>{
+      document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`    
+      getInstruction('mashedPotatoes',3, (step4)=>{
+        document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`    
+        getInstruction('mashedPotatoes' , 4 ,(step5)=>{
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`
+          document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");  
+            }, (error) => console.log(error))
           })
       })
   })
