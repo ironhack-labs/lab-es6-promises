c85e1dfadb20b93633810c9f7bfab7ab9a2c9a38
asm-dev <salamanca.m@outlook.com>
2022-11-17 15:43:39 +0100

    iteration 2 --- steak using promises

--- javascript/index.js
+++ javascript/index.js
@@ -47,10 +47,64 @@ getInstruction('mashedPotatoes', 0, (step0) => {
  }, (error) => console.log(error));
 
 // Iteration 2 - using promises
-// ...
+
+// could this be refractored by using Promise.all?
+obtainInstruction('steak', 0)
+  .then( (step0) => {
+    document.querySelector("#steak").innerHTML += `<li>${step0}</li>`
+     obtainInstruction('steak', 1)
+     .then( (step1) => {
+        document.querySelector("#steak").innerHTML += `<li>${step1}</li>`
+          obtainInstruction('steak', 2)
+          .then( (step2) => {
+            document.querySelector("#steak").innerHTML += `<li>${step2}</li>`
+              obtainInstruction('steak', 3)
+              .then( (step3) => {
+                document.querySelector("#steak").innerHTML += `<li>${step3}</li>`
+                  obtainInstruction('steak', 4)
+                  .then( (step4) => {
+                    document.querySelector("#steak").innerHTML += `<li>${step4}</li>`
+                      obtainInstruction('steak', 5)
+                      .then( (step5) => {
+                        document.querySelector("#steak").innerHTML += `<li>${step5}</li>`
+                          obtainInstruction('steak', 6)
+                          .then( (step6) => {
+                            document.querySelector("#steak").innerHTML += `<li>${step6}</li>`
+                              obtainInstruction('steak', 7)
+                              .then( (step7) => {
+                                document.querySelector("#steak").innerHTML += `<li>${step7}</li>`
+                                document.querySelector("#steak").innerHTML += `<li>Stake is ready!</li>`
+                                document.querySelector("#steakImg").removeAttribute('hidden');
+                  })
+                })
+              })
+            })
+          })
+        })
+      })
+    })
 
 // Iteration 3 using async/await
-// ...
+async function makeBroccoli() {
+  const step0 = await obtainInstruction("broccoli", 0);
+  document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`
+  const step1 = await obtainInstruction("broccoli", 1);
+  document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`
+  const step2 = await obtainInstruction("broccoli", 2);
+  document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`
+  const step3 = await obtainInstruction("broccoli", 3);
+  document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`
+  const step4 = await obtainInstruction("broccoli", 4);
+  document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`
+  const step5 = await obtainInstruction("broccoli", 5);
+  document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`
+  const step6 = await obtainInstruction("broccoli", 6);
+  document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`
+  document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`
+  document.querySelector("#broccoliImg").removeAttribute('hidden');
+
+}
+makeBroccoli();
 
 // Bonus 2 - Promise all
 // ...
