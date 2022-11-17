bad86f550338526f708f201b6beb6f3a8ca529cb
Blanca <blancabcbt@gmail.com>
2022-11-17 17:07:47 +0100

    add iteration 3 and bonus 1

--- javascript/index.js
+++ javascript/index.js
@@ -28,25 +28,42 @@
 
 
 // Iteration 1 - using callbacks
-getInstruction('mashedPotatoes', 0, (step0) => {
-  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step0}</li>`;
-  getInstruction('mashedPotatoes', 1, (step1)=>{
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
-    getInstruction('mashedPotatoes',2,(step2)=>{
-      document.querySelector("#mashedPotatoes").
-      innerHTML += `<li>${step2}</li>`;
-      getInstruction('mashedPotatoes',3,(step3)=>{
-        document.querySelector("#mashedPotatoes").
-        innerHTML += `<li>${step3}</li>`;
-        getInstruction('mashedPotatoes',4,(step4)=>{
-          document.querySelector("#mashedPotatoes").
-          innerHTML += `<li>${step4}</li>`;
-          getInstruction('mashedPotatoes',5,(step5)=>{
-            document.querySelector("#mashedPotatoes").
-            innerHTML += `<li>${step5}</li>`;})
 
-            document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`
-  })})})})});
+function getImg(){
+  const img = document.getElementById('mashedPotatoesImg');
+  img.removeAttribute('hidden')
+
+}
+getInstruction('mashedPotatoes', 0, (step1) => {
+  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
+  getInstruction('mashedPotatoes', 1, (step2)=>{
+   document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
+    getInstruction('mashedPotatoes',2,(step3)=>{
+    document.querySelector("#mashedPotatoes").
+      innerHTML += `<li>${step3}</li>`;
+      getInstruction('mashedPotatoes',3,(step4)=>{
+        document.querySelector("#mashedPotatoes").
+        innerHTML += `<li>${step4}</li>`;
+        getInstruction('mashedPotatoes',4,(step5)=>{
+          document.querySelector("#mashedPotatoes").
+          innerHTML += `<li>${step5}</li>`;
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`
+          getImg()
+          
+
+            
+   }, (error) => console.log(error));
+  }, (error) => console.log(error));
+}, (error) => console.log(error));
+},(error) => console.log(error));
+
+},(error) => console.log(error)
+
+
+);
+
+
+
 
 // Iteration 2 - using promises
 
@@ -85,23 +102,53 @@ obtainInstruction('steak', 0)
     document.querySelector("#steak").innerHTML += `<li>${step7}</li>
      <li>Steak is ready!</li>`
     })
+    .then(()=>{
+      const img= document.querySelector('#steakImg');
+      img.removeAttribute('hidden')
+    })
   .catch((err)=>{
     document.querySelector("#steak").innerHTML += `<li>${err}</li>`
   })
-  
 
-
-  
-  
-
-  
-  
-  
+ 
   
 
 
 // Iteration 3 using async/await
-// ...
+
+
+async function displayImg(){
+   await makeBroccoli();
+   const broccoliImg = document.querySelector('#broccoliImg');
+   broccoliImg.removeAttribute('hidden');
+
+}
+async function makeBroccoli() {
+  
+  try{
+    const step0 =  await obtainInstruction('broccoli', 0);
+    document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`
+    const step1 =  await obtainInstruction("broccoli", 1);
+    document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`
+    const step2 =  await obtainInstruction("broccoli", 2);
+    document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`
+    const step3 =  await obtainInstruction("broccoli", 3);
+    document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`
+    const step4 =  await obtainInstruction("broccoli", 4);
+    document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`
+    const step5 =  await obtainInstruction("broccoli", 5);
+    document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`
+    const step6 =  await obtainInstruction("broccoli", 6);
+    document.querySelector("#broccoli").innerHTML += `<li>${step6}</li><li>Broccoli is ready</li>!`
+    
+  }
+
+  catch(error){
+      console.log('error');
+  }}
+
+displayImg();
+
 
 // Bonus 2 - Promise all
 // ...
