5fbc6e2ecc475ad0c2247abb8daf7c22be6c80d3
Laura Bährs <laura.baehrs@googlemail.com>
2022-12-12 17:22:56 +0100

    finish Bonus 1"
    :wq

--- javascript/index.js
+++ javascript/index.js
@@ -39,8 +39,8 @@ getInstruction("mashedPotatoes", 0, (step1) => {
         document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
         getInstruction("mashedPotatoes", 4, (step5) => {
           document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
-          document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
           document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready</li>`;
+          document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
         }, (error) => console.log(error));
       }, (error) => console.log(error));
     }, (error) => console.log(error));
@@ -80,6 +80,7 @@ obtainInstruction("steak", 0)
 })
 .then(() => {
   document.querySelector("#steak").innerHTML += `<li>Steak is ready</li>`;
+  document.querySelector("#steakImg").removeAttribute("hidden");
 })
 .catch((error) => {
   console.log(error);
@@ -103,6 +104,7 @@ async function makeBroccoli() {
     const step6 = await obtainInstruction("broccoli", 6);
     document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`
     document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready</li>`
+    document.querySelector("#broccoliImg").removeAttribute("hidden");
   }
   catch (error) {
     console.log(error);
