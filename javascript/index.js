9b81d0503e3a1676fd00a90f386673adfe880a5b
Carlos Sosa <devcaco@simpletg.com>
2023-01-08 13:23:37 -0500

    iteration 3 - done

--- javascript/index.js
+++ javascript/index.js
@@ -41,7 +41,7 @@ getInstruction("mashedPotatoes", 0, (step1) => {
         document.querySelector('#mashedPotatoes').innerHTML += `<li>${step4}</li>`;
         getInstruction('mashedPotatoes', 4, (step5) => {
           document.querySelector('#mashedPotatoes').innerHTML += `<li>${step5}</li>`;
-          document.querySelector('#mashedPotatoes').innerHTML += `<li>MASHED POTATOES ARE READY</li>`;
+          document.querySelector('#mashedPotatoes').innerHTML += `<li>Mashed potatoes are ready!</li>`;
           getInstruction('mashedPotatos', 5, (step6) => {
             document.querySelector('#mashedPotatoes').innerHTML += `<li>${step6}</li>`;
           }, (err) => { console.log(err); })
@@ -85,12 +85,36 @@ obtainInstruction('steak', 0)
   })
   .then((step7) => {
     document.querySelector('#steak').innerHTML += `<li>${step7}</li>`;
-    document.querySelector('#steak').innerHTML += `<li>STEAK IS READY</li>`;
+    document.querySelector('#steak').innerHTML += `<li>Steak is ready!</li>`;
   })
   .catch((err) => console.log(err));
 
 // Iteration 3 using async/await
 // ...
+
+async function makeBroccoli() {
+  try {
+    step1 = await obtainInstruction('broccoli', 0);
+    document.querySelector('#broccoli').innerHTML += `<li>${step1}</li>`
+    step2 = await obtainInstruction('broccoli', 1);
+    document.querySelector('#broccoli').innerHTML += `<li>${step2}</li>`;
+    step3 = await obtainInstruction('broccoli', 2);
+    document.querySelector('#broccoli').innerHTML += `<li>${step3}</li>`;
+    step4 = await obtainInstruction('broccoli', 3);
+    document.querySelector('#broccoli').innerHTML += `<li>${step4}</li>`;
+    step5 = await obtainInstruction('broccoli', 4);
+    document.querySelector('#broccoli').innerHTML += `<li>${step5}</li>`;
+    step6 = await obtainInstruction('broccoli', 5);
+    document.querySelector('#broccoli').innerHTML += `<li>${step1}</li>`;
+    step7 = await obtainInstruction('broccoli', 6);
+    document.querySelector('#broccoli').innerHTML += `<li>${step1}</li>`;
+    document.querySelector('#broccoli').innerHTML += `<li>Broccoli is ready!</li>`;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+makeBroccoli();
 
 // Bonus 2 - Promise all
 // ...
