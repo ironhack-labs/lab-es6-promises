64d55394940c722fb89c9f8aa2171d2b4ba2ead2
Maria Fernanda Echeverria Colorado <fernanda.ecmf@gmail.com>
2023-01-30 17:59:04 -0600

    Tercer Y ULTIMO edit

--- javascript/index.js
+++ javascript/index.js
@@ -92,6 +92,65 @@ obtainInstruction('steak', 0)
 
 // Iteration 3 using async/await
 
+//definición de una función asíncrona
+//try: El bloque "try" contiene código que se ejecuta y puede generar una excepción.
+async function makeBroccoli() {
+  try {
+ ///espera a que se complete antes de continuar con la ejecución. La función "obtainInstruction" probablemente devuelve una promesa, y "await" permite la espera de la resolución de la promesa antes de continuar.
+   const step0 = await obtainInstruction("broccoli", 0)
+   document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`;
+ 
+   const step1 = await obtainInstruction("broccoli", 1)
+   document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`;
+ 
+   const step2 = await obtainInstruction("broccoli", 2)
+   document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`;
+ 
+   const step3 = await obtainInstruction("broccoli", 3)
+   document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`;
+ 
+   const step4 = await obtainInstruction("broccoli", 4)
+   document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`;
+ 
+   const step5 = await obtainInstruction("broccoli", 5)
+   document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`;
+ 
+   const step6 = await obtainInstruction("broccoli", 6)
+   //esta expresión selecciona un elemento con un ID de "broccoli" en el DOM y agrega un elemento de lista (li)
+   document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
+   document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready</li>`;
+   document.querySelector("#broccoliImg").removeAttribute(`hidden`);
+ 
+  }
+ ////El bloque "catch" contiene código que se ejecuta si se produce una excepción dentro del bloque "try". En este caso, se pasa un objeto "err" a la función de retorno de llamada y se imprime en la consola con "console.log(err)".
+  catch {(err) => console.log(err) }
+ }
+ 
+ //// Esta es la invocación de la función asíncrona "makeBroccoli". Esto ejecutará el código dentro de la función y mostrará los resultados en la página web.
+ makeBroccoli()
+
+// Bonus 2 - Promise all
+
+
+
+///La función obtainInstruction se llama 7 veces con diferentes parámetros, cada vez retornando una promesa. Luego, el método Promise.all es llamado con el objetivo de esperar a que todas las promesas se resuelvan antes de continuar.
+const step0 = obtainInstruction ("brusselsSprouts", 0)
+const step1 = obtainInstruction ("brusselsSprouts", 1)
+const step2 = obtainInstruction ("brusselsSprouts", 2)
+const step3 = obtainInstruction ("brusselsSprouts", 3)
+const step4 = obtainInstruction ("brusselsSprouts", 4)
+const step5 = obtainInstruction ("brusselsSprouts", 5)
+const step6 = obtainInstruction ("brusselsSprouts", 6)
+
+////se llama a un método .then que recibe como parámetro el resultado de la promesa (un array de valores), y usa .forEach para agregar cada uno de estos valores como un nuevo elemento de una lista en el HTML 
+
+Promise.all([step0, step1, step2, step3, step4, step5, step6])
+  .then((element) => {
+    element.forEach((ele) => {
+      document.querySelector("#brusselsSprouts").innerHTML += `<li>${ele}</li>`})})
+
+      ///.then es llamado para remover el atributo hidden del elemento con id "brusselsSproutsImg". Esto hace que la imagen sea visible en la página.
+  .then ( document.querySelector("#brusselsSproutsImg").removeAttribute(`hidden`));
 
 
 
