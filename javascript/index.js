14aca578c71bc045bd809456931f65a023881176
Tobias Krauss <tobias.tk.krauss@gmail.com>
2023-01-30 14:50:06 +0100

    Iteration 2

--- javascript/index.js
+++ javascript/index.js
@@ -48,7 +48,44 @@ getInstruction("mashedPotatoes", 0, (step1) => {
 }, (err) => console.log(err))
 
 // Iteration 2 - using promises
-// ...
+obtainInstruction("steak", 0)
+  .then( (step1) => {
+    document.querySelector("#steak").innerHTML += `<li>${step1}</li>`
+    return obtainInstruction("steak", 1)
+    .then( (step2) => {
+      document.querySelector("#steak").innerHTML += `<li>${step2}</li>`
+      return obtainInstruction("steak", 2)
+      .then( (step3) => {
+        document.querySelector("#steak").innerHTML += `<li>${step3}</li>`
+        return obtainInstruction("steak", 3)
+        .then( (step4) => {
+          document.querySelector("#steak").innerHTML += `<li>${step4}</li>`
+          return obtainInstruction("steak", 4)
+          .then( (step5) => {
+            document.querySelector("#steak").innerHTML += `<li>${step5}</li>`
+            return obtainInstruction("steak", 5)
+            .then( (step6) => {
+              document.querySelector("#steak").innerHTML += `<li>${step6}</li>`
+              return obtainInstruction("steak", 6)
+              .then( (step7) => {
+                document.querySelector("#steak").innerHTML += `<li>${step7}</li>`
+                return obtainInstruction("steak", 7)
+                .then( (step8) => {
+                  document.querySelector("#steak").innerHTML += `<li>${step8}</li>`
+                  return obtainInstruction("steak", 8)
+            
+
+.catch((err) => console.log(err))
+  })
+})
+})
+})
+})
+})
+})
+})
+
+
 
 // Iteration 3 using async/await
 // ...
