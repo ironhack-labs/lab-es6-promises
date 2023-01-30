49aafd451c58c43833424de311f7c4690277125e
Maria Fernanda Echeverria Colorado <fernanda.ecmf@gmail.com>
2023-01-30 17:35:20 -0600

    primer cambio

--- javascript/index.js
+++ javascript/index.js
@@ -28,7 +28,24 @@
 
 
 // Iteration 1 - using callbacks
-// ...
+getInstruction('mashedPotatoes', 0, (step0) => {
+  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step0}</li>`
+    getInstruction("mashedPotatoes", 1, (step1) => {
+      document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`
+        getInstruction("mashedPotatoes", 2, (step2) => {
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`
+            getInstruction("mashedPotatoes", 3, (step3) => {
+              document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`
+                getInstruction("mashedPotatoes", 4, (step4) => {
+                  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`
+                      document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed Potatoes are ready</li>`
+                      document.querySelector("#mashedPotatoesImg").removeAttribute(`hidden`)
+                    }, (err) => console.log(err)) 
+                },(err) => console.log(err))
+            }, (err) => console.log(err))
+        }, (err) => console.log(err))
+    },(err) => console.log(err) )
+
 
 // Iteration 2 - using promises
 // ...
