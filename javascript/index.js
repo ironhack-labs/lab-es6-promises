9c11497113e705faee199aa21721c79f3fa89dc1
Wilkins Morales <wilkinscom@gmail.com>
2023-01-31 22:29:24 +0200

    iteration 1

--- javascript/index.js
+++ javascript/index.js
@@ -30,6 +30,27 @@
 // Iteration 1 - using callbacks
 // ...
 
+getInstruction('mashedPotatoes',0,(step0) =>{
+  document.querySelector('#mashedPotatoes').innerHTML += `<li>${step0}</li>`
+    getInstruction('mashedPotatoes',1,(step1) =>{
+      document.querySelector('#mashedPotatoes').innerHTML += `<li>${step1}</li>`
+        getInstruction('mashedPotatoes',2,(step2)=>{
+          document.querySelector('#mashedPotatoes').innerHTML += `<li>${step2}</li>`
+            getInstruction('mashedPotatoes',3,(step3)=>{
+              document.querySelector('#mashedPotatoes').innerHTML += `<li>${step3}</li>`
+                getInstruction('mashedPotatoes',4,(step4) =>{
+                  document.querySelector('#mashedPotatoes').innerHTML += `<li>${step4}</li>`
+                    getInstruction('mashedPotatoes',5,(step5) => {
+                      document.querySelector('#mashedPotatoes').innerHTML += `<li>${step5}</li>`
+                      
+                    }); document.querySelector('#mashedPotatoes').innerHTML += `<li>Mashed potatoes are ready!</li>` 
+                })
+            })
+        })
+    })
+}) 
+
+
 // Iteration 2 - using promises
 // ...
 
