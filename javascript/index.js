aec432bfe11a66da14d747d13ff78526ca9538d8
Yona <118460222+yosoyyona@users.noreply.github.com>
2023-02-01 22:40:33 +0100

    Bonus 2 - done

--- javascript/index.js
+++ javascript/index.js
@@ -137,4 +137,27 @@ async function makeBroccoli() {
 makeBroccoli()
 
 // Bonus 2 - Promise all
-// ...
+const p1 = obtainInstruction('brusselsSprouts', 0)
+const p2 = obtainInstruction('brusselsSprouts', 1)
+const p3 = obtainInstruction('brusselsSprouts', 2)
+const p4 = obtainInstruction('brusselsSprouts', 3)
+const p5 = obtainInstruction('brusselsSprouts', 4)
+const p6 = obtainInstruction('brusselsSprouts', 5)
+const p7 = obtainInstruction('brusselsSprouts', 6)
+const p8 = obtainInstruction('brusselsSprouts', 7)
+
+
+Promise.all( [p1, p2, p3, p4, p5, p6, p7, p8] )
+  .then((element) => {
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>${element[0]}</li>`
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>${element[1]}</li>`
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>${element[2]}</li>`
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>${element[3]}</li>`
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>${element[4]}</li>`
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>${element[5]}</li>`
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>${element[6]}</li>`
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>${element[7]}</li>`
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>Brussels sprouts are ready!</li>`
+    document.querySelector("#brusselsSproutsImg").removeAttribute("hidden")
+  })
+  .catch((error) => console.log(error))
