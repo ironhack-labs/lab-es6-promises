90c553cd34a710cb89b4fd9fbea84d6084496885
michael07200 <mickmhtong@gmail.com>
2023-02-04 10:52:25 +0100

    build: bonus 1

--- javascript/index.js
+++ javascript/index.js
@@ -99,6 +99,7 @@ obtainInstruction("steak", 0)
   .then((step) => {
     document.querySelector("#steak").innerHTML += `<li>${step}</li>`;
     document.querySelector("#steak").innerHTML += `<li>Stake is ready!</li>`;
+    document.querySelector("#steakImg").removeAttribute("hidden");
   })
   .catch((error) => console.log(error));
 
@@ -120,6 +121,7 @@ async function makeBroccoli() {
     const step6 = await obtainInstruction("broccoli", 6);
     document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
     document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`;
+    document.querySelector("#broccoliImg").removeAttribute("hidden");
   } catch (error) {
     console.log(error);
   }
