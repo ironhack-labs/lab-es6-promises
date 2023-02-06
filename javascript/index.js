dc8e6bb05c0dfb2f7fd14047b36b6fa14b88d442
Araceli Galiano Salas <araceligalianosalas@gmail.com>
2023-02-06 19:17:01 +0100

    Bonus done!

--- javascript/index.js
+++ javascript/index.js
@@ -142,3 +142,25 @@ async function makeBroccoli() {
 makeBroccoli();
 // Bonus 2 - Promise all
 // ...
+
+Promise.all([
+  obtainInstruction("brusselsSprouts", 0),
+  obtainInstruction("brusselsSprouts", 1),
+  obtainInstruction("brusselsSprouts", 2),
+  obtainInstruction("brusselsSprouts", 3),
+  obtainInstruction("brusselsSprouts", 4),
+  obtainInstruction("brusselsSprouts", 5),
+  obtainInstruction("brusselsSprouts", 6),
+  obtainInstruction("brusselsSprouts", 7),
+  
+])
+.then((response) => {
+  response.forEach((step) => {
+    document.querySelector("#brusselsSprouts").innerHTML += `<li> ${step}</li>`;
+  })
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>Brussels Sprouts are ready!</li>`;
+  console.log(response)
+})
+.catch((error) => {
+  console.log(error)
+})
