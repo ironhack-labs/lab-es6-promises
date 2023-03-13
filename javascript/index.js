275c199437cd208ab49edb61b49fd6440f0d3db8
Sotiris Ntokas <ntokas1995@live.com>
2023-03-13 17:42:49 +0100

    completed iteration 2 and 3

--- javascript/index.js
+++ javascript/index.js
@@ -57,9 +57,69 @@ getInstruction("mashedPotatoes", 0, (step1) => {
 
 // Iteration 2 - using promises
 // ...
+obtainInstruction('steak', 0)
+  .then( (step0) => {
+    document.querySelector("#steak").innerHTML += `<li>${step0}</li>`
+    return obtainInstruction('steak', 1)
+  })
+  .then( (step1) => {
+    document.querySelector("#steak").innerHTML += `<li>${step1}</li>`
+    return obtainInstruction('steak', 2)
+  })
+  .then( (step2) => {
+    document.querySelector("#steak").innerHTML += `<li>${step2}</li>`
+    return obtainInstruction('steak', 3)
+  })
+  .then( (step3) => {
+    document.querySelector("#steak").innerHTML += `<li>${step3}</li>`
+    return obtainInstruction('steak', 4)
+  })
+  .then( (step4) => {
+    document.querySelector("#steak").innerHTML += `<li>${step4}</li>`
+    return obtainInstruction('steak', 5)
+  })
+  .then( (step5) => {
+    document.querySelector("#steak").innerHTML += `<li>${step5}</li>`
+    return obtainInstruction('steak', 6)
+  })
+  .then( (step6) => {
+    document.querySelector("#steak").innerHTML += `<li>${step6}</li>`
+    document.querySelector("#steak").innerHTML += `<li>Steak is ready!</li>`
+    document.querySelector("#steakImg").removeAttribute("hidden");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 
 // Iteration 3 using async/await
 // ...
+async function makeBroccoli() {
+  console.log("asdfa");
+  try {
+    const instruction1 = await obtainInstruction("broccoli", 0);
+    const instruction2 = await obtainInstruction("broccoli", 1);
+    const instruction3 = await obtainInstruction("broccoli", 2);
+    const instruction4 = await obtainInstruction("broccoli", 3);
+    const instruction5 = await obtainInstruction("broccoli", 4);
+    const instruction6 = await obtainInstruction("broccoli", 5);
+    const instruction7 = await obtainInstruction("broccoli", 6);
 
+    document.querySelector("#broccoli").innerHTML += `<li>${instruction1}</li>`
+    document.querySelector("#broccoli").innerHTML += `<li>${instruction2}</li>`
+    document.querySelector("#broccoli").innerHTML += `<li>${instruction3}</li>`
+    document.querySelector("#broccoli").innerHTML += `<li>${instruction4}</li>`
+    document.querySelector("#broccoli").innerHTML += `<li>${instruction5}</li>`
+    document.querySelector("#broccoli").innerHTML += `<li>${instruction6}</li>`
+    document.querySelector("#broccoli").innerHTML += `<li>${instruction7}</li>`
+    document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`
+    document.querySelector("#broccoliImg").removeAttribute("hidden");
+    
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+makeBroccoli();
 // Bonus 2 - Promise all
 // ...
