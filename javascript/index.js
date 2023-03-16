24c1212aab2366493dd8ac2e9591f1b39820607b
Natalia Nossack <nvcordeiro22@gmail.com>
2023-03-16 17:27:30 +0100

    solved iteration 3 + 1 bonus

--- javascript/index.js
+++ javascript/index.js
@@ -74,10 +74,10 @@ getInstruction(
 // ...
 
  obtainInstruction("steak", 0)
-   .then((step0) => {
+   //.then((step0) => {
     
-    document.querySelector("#steak").innerHTML += `<li>${step0}</li>`
-    return obtainInstruction("steak", 0)})
+   // document.querySelector("#steak").innerHTML += `<li>${step0}</li>`
+   // return obtainInstruction("steak", 0)})
    .then((step1) => {
     document.querySelector("#steak").innerHTML += `<li>${step1}</li>`
     return obtainInstruction("steak", 1)})
@@ -106,6 +106,42 @@ getInstruction(
 
 // Iteration 3 using async/await
 // ...
+
+async function makeBroccoli() {
+  
+  try {
+    const step0 = await obtainInstruction('broccoli', 0)
+    document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`
+ 
+    const step1 = await obtainInstruction("broccoli", 1)
+    document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`
+
+    const step2 = await obtainInstruction("broccoli", 2)
+    document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`
+
+    const step3 = await obtainInstruction("broccoli", 3)
+    document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`
+
+    const step4 = await obtainInstruction("broccoli", 4)
+    document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`
+
+    const step5 = await obtainInstruction("broccoli", 5)
+    document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`
+
+    const step6 = await obtainInstruction("broccoli", 6)
+    document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`
+    document.querySelector("#broccoli").innerHTML += `<li> Broccoli is ready! </li>`
+    document.querySelector("#broccoliImg").removeAttribute("hidden");
+    
+  } catch (error) {
+    console.log('ERROR >>>', error);
+  }
+}
+makeBroccoli();
+
+
+
+
 
 // Bonus 2 - Promise all
 // ...
