9b5d5f9887abdf55d5e56dafb6711394a8887ad8
“Zuzana <zuzana@getyourguide.com>
2023-03-22 09:25:41 +0100

    first iteration

--- javascript/index.js
+++ javascript/index.js
@@ -4,6 +4,7 @@
 // 🚨🚨🚨 Comment out the below code before you start working on the code
 
 // Out of sync
+/*
   getInstruction("mashedPotatoes", 0, (step1) => {
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
   }, (error) => console.log(error));
@@ -25,10 +26,35 @@
     document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
   }, (error) => console.log(error));
 
-
+*/
 
 // Iteration 1 - using callbacks
-// ...
+
+mashedPotatoes.push("Mashed potatoes are ready!");
+
+getInstruction('mashedPotatoes', 0, (step0) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step0}</li>`;
+      getInstruction('mashedPotatoes', 1, (step1) => {
+        document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
+          getInstruction('mashedPotatoes', 2, (step2) => {
+            document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
+              getInstruction('mashedPotatoes', 3, (step3) => {
+                document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
+                  getInstruction('mashedPotatoes', 4, (step4) => {
+                    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
+                      getInstruction('mashedPotatoes', 5, (step5) => {
+                        document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
+                          getInstruction('mashedPotatoes', 6, (step6) => {
+                            document.querySelector("#mashedPotatoes").innerHTML += `<li>${step6}</li>`;
+                
+                          }), (error) => console.log(error);
+                      }), (error) => console.log(error);
+                  }), (error) => console.log(error);
+              }), (error) => console.log(error);
+          }), (error) => console.log(error);
+      }), (error) => console.log(error);
+  }), (error) => console.log(error);
+
 
 // Iteration 2 - using promises
 // ...
