3a0982a830fc145a687913d6dff8483e32b174ef
Nicolas_M <nicolas.meot@live.fr>
2023-03-29 20:13:23 +0200

    2nd iteration

--- javascript/index.js
+++ javascript/index.js
@@ -103,8 +103,34 @@ obtainInstruction("steak", 0)
   })
   .then(() => (document.querySelector("#steak").innerHTML += `<li>Steak is Ready !</li>`))
   .catch((error) => console.log(error));
+
 // Iteration 3 using async/await
-// ...
+
+async function makeBroccoli() {
+
+  const step1 = await obtainInstruction("broccoli",0)
+  document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`
+
+  const step2 = await obtainInstruction("broccoli",1)
+  document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`
+
+  const step3 = await obtainInstruction("broccoli",2) 
+  document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`
+
+  const step4 = await obtainInstruction("broccoli",3)
+  document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`
+
+  const step5 = await obtainInstruction("broccoli",4)
+  document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`
+
+  const step6 = await obtainInstruction("broccoli",5)
+  document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`
+
+  const step7 = await obtainInstruction("broccoli",6)
+  document.querySelector("#broccoli").innerHTML += `<li>${step7}</li>`
+}
+
+makeBroccoli().catch(function(err) {console.log(err)}) 
 
 // Bonus 2 - Promise all
 // ...
