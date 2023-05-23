6020ceb0be031a6e0b4706cb11cd84bb45fa2f18
Walter Bernal <wltrbm@gmail.com>
2023-05-23 15:48:39 -0700

    Bonus 2

--- javascript/index.js
+++ javascript/index.js
@@ -81,7 +81,9 @@ obtainInstruction("steak", 0)
   })
   .then((step6) => {
     addLiElement('steak', step6)
-    addLiElement('steak', 'Steak is ready!')
+  })
+  .finally(() => { 
+    addLiElement("steak", "Steak is ready!")
     unhide("steakImg")
   })
 
@@ -106,4 +108,20 @@ function unhide(id) {
 }
 
 // Bonus 2 - Promise all
-// ...
+const p1 = obtainInstruction("brusselsSprouts", 0)
+const p2 = obtainInstruction("brusselsSprouts", 1)
+const p3 = obtainInstruction("brusselsSprouts", 2)
+const p4 = obtainInstruction("brusselsSprouts", 3)
+const p5 = obtainInstruction("brusselsSprouts", 4)
+const p6 = obtainInstruction("brusselsSprouts", 5)
+const p7 = obtainInstruction("brusselsSprouts", 6)
+const p8 = obtainInstruction("brusselsSprouts", 7)
+
+Promise.all([p1, p2, p3, p4, p5, p6, p7, p8])
+  .then(promises => {
+    promises.forEach(promise => addLiElement("brusselsSprouts", promise))
+  })
+  .finally(() => {
+    addLiElement("brusselsSprouts", "Brussels sprouts are ready!")
+    unhide("brusselsSproutsImg")
+  })
