42f0a70139f2f6ccd020311d6b31b2d9dcb64793
Luis <luisguile495@gmail.com>
2023-05-27 21:56:55 -0400

    3 iteration + bonus 1

--- javascript/index.js
+++ javascript/index.js
@@ -61,6 +61,7 @@ getInstruction('mashedPotatoes', 0, (step0) => {
         getInstruction('mashedPotatoes', 4, (step5) => {
           document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
           document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready</li>`;
+          document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
         });
       });
     });
@@ -108,12 +109,37 @@ obtainInstruction('steak', 0)
   .then((step7) => {
     document.querySelector('#steak').innerHTML += `<li>${step7}</li>`;
     document.querySelector("#steak").innerHTML += `<li>Steak is ready</li>`;
+    document.querySelector("#steakImg").removeAttribute("hidden");
   })
 
 
 
 // Iteration 3 using async/await
-// ...
+async function makeBroccoli(){
+  try{
+    const step0 = await obtainInstruction('broccoli', 0);
+    document.querySelector('#broccoli').innerHTML += `<li>${step0}</li>`;
+    const step1 = await obtainInstruction('broccoli',1);
+    document.querySelector('#broccoli').innerHTML += `<li>${step1}</li>`;
+    const step2 = await obtainInstruction('broccoli', 2);
+    document.querySelector('#broccoli').innerHTML += `<li>${step2}</li>`;
+    const step3 = await obtainInstruction('broccoli',3);
+    document.querySelector('#broccoli').innerHTML += `<li>${step3}</li>`; 
+    const step4 = await obtainInstruction('broccoli',4);
+    document.querySelector('#broccoli').innerHTML += `<li>${step4}</li>`;
+    const step5 = await obtainInstruction('broccoli',5);
+    document.querySelector('#broccoli').innerHTML += `<li>${step5}</li>`;
+    const step6 = await obtainInstruction('broccoli',6);
+    document.querySelector('#broccoli').innerHTML += `<li>${step6}</li>`;
+    document.querySelector('#broccoli').innerHTML += `<li>Broccoli is ready!</li>`;
+    document.querySelector("#broccoliImg").removeAttribute("hidden");
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+makeBroccoli();
 
 // Bonus 2 - Promise all
 // ...
