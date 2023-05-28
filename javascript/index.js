a76cd258f34209c2055e3723733b0dffd181c822
Luis <luisguile495@gmail.com>
2023-05-27 22:10:43 -0400

    Bonus 2 and lab solved

--- javascript/index.js
+++ javascript/index.js
@@ -142,4 +142,24 @@ async function makeBroccoli(){
 makeBroccoli();
 
 // Bonus 2 - Promise all
-// ...
+const list = [
+  obtainInstruction('brusselsSprouts', 0),
+  obtainInstruction('brusselsSprouts', 1),
+  obtainInstruction('brusselsSprouts', 2),
+  obtainInstruction('brusselsSprouts', 3),
+  obtainInstruction('brusselsSprouts', 4),
+  obtainInstruction('brusselsSprouts', 5),
+  obtainInstruction('brusselsSprouts', 6),
+  obtainInstruction('brusselsSprouts', 7),
+];
+
+Promise.all(list)
+  .then((steps) => {
+    steps.forEach((step) => {
+      document.querySelector("#brusselsSprouts").innerHTML += `<li>${step}</li>`;
+    });
+
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>Brussels sprouts are ready!</li>`;
+    document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
+  })
+  .catch((err) => console.log(err));
