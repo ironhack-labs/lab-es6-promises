3425ae4a78d2dbd09ff009cd40bd3434b6460125
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-06-24 00:33:51 +0200

    Iteration 3

--- javascript/data.js
+++ javascript/data.js
@@ -35,5 +35,5 @@ const broccoli = [
   'once boiling, carefully lower the broccoli into the water',
   'cook for 3 to 4 minutes, or until tender',
   'drain, then leave to steam dry for a minute',
-  'enjoy'
+  'Broccoli is ready!'
 ];
