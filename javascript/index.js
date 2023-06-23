d87abe99ac430948fe7ddc2d97eaecb689bf525f
Gabriela García <ggarciaborregon@gmail.com>
2023-06-23 13:50:36 +0200

    iteration 3

--- javascript/index.js
+++ javascript/index.js
@@ -28,6 +28,7 @@
 
 
 // Iteration 1 - using callbacks
+
     getInstruction("mashedPotatoes", 0, (step1) => {
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
 
@@ -43,6 +44,7 @@
                   getInstruction("mashedPotatoes", 4, (step5) => {
                   document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
                   document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
+                  document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`;
 
 
                   }, (error) => console.log(error));
@@ -98,16 +100,47 @@
 
       .then( (step8) => {
         document.querySelector("#steak").innerHTML += `<li>${step8}</li>`;
+        document.querySelector("#steak").innerHTML += `<li>Steak is ready!</li>`
         document.querySelector("#steakImg").removeAttribute("hidden");
       })
 
       .catch( (err) => console.log(err) );
 
-      
-// ...
 
 // Iteration 3 using async/await
-// ...
+    
+async function makeFood(step) {
+  try {
+    const step1 = await obtainInstruction("broccoli", 0) 
+    document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`;
+
+    const step2 = await obtainInstruction("broccoli", 1) 
+    document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`;
+
+    const step3 = await obtainInstruction("broccoli", 2) 
+    document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`;
+
+    const step4 = await obtainInstruction("broccoli", 3) 
+    document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`;
+
+    const step5 = await obtainInstruction("broccoli", 4) 
+    document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`;
+
+    const step6 = await obtainInstruction("broccoli", 5) 
+    document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
+
+    const step7 = await obtainInstruction("broccoli", 6) 
+    document.querySelector("#broccoli").innerHTML += `<li>${step7}</li>`;
+    document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`
+    document.querySelector("#broccoliImg").removeAttribute("hidden");
+  }
+
+  catch (err) {
+    console.log(err);
+  }
+}
+
+makeFood();
 
 // Bonus 2 - Promise all
 // ...
