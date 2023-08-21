6fb67f189392033cda42024f017d30768aea97d3
Alejandro Ahijado Ibarra <alejandro.ahijado@gmail.com>
2023-08-22 00:34:27 +0200

    Done

--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,7 @@
 // 🚨🚨🚨 Comment out the below code before you start working on the code
 
 // Out of sync
-  getInstruction("mashedPotatoes", 0, (step1) => {
+ /* getInstruction("mashedPotatoes", 0, (step1) => {
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
   }, (error) => console.log(error));
   
@@ -24,17 +24,101 @@
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
     document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
   }, (error) => console.log(error));
-
+*/
 
 
 // Iteration 1 - using callbacks
-// ...
+getInstruction('mashedPotatoes', 0, (step0) => {
+  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step0}</li>`
+  getInstruction('mashedPotatoes', 1, (step1) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`
+    getInstruction('mashedPotatoes', 2, (step2) => {
+      document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`
+      getInstruction('mashedPotatoes', 3, (step3) => {
+        document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`
+        getInstruction('mashedPotatoes', 4, (step4) => {
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`
+          getInstruction('mashedPotatoes', 5, (step5) => {
+            document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`
+            document.querySelector("#images/mashed-potatoes.jpg");
+          }, (error) => console.log (error))
+        }, (error) => console.log (error))
+      }, (error) => console.log (error))
+    }, (error) => console.log (error))
+  }, (error) => console.log (error))
+}, (error) => console.log (error));
+
+
 
 // Iteration 2 - using promises
-// ...
+obtainInstruction('steak', 0) 
+  .then((step0) => {
+   document.querySelector('#steak').innerHTML += `<li>${step0}</li>`
+})
+.then(() => {
+  return obtainInstruction('steak', 1)
+  .then((step1)=> {
+    document.querySelector('#steak').innerHTML += `<li>${step1}</li>`
+  })
+})
+.then(() =>{
+  return obtainInstruction('steak', 2)
+  .then((step2) => {
+    document.querySelector('#steak').innerHTML += `<li>${step2}</li>`
+  })
+})
+.then(() => {
+  return obtainInstruction('steak', 3)
+  .then ((step3) => {
+    document.querySelector('#steak').innerHTML += `<li>${step3}</li>`
+  })
+})
+.then (() => {
+  return obtainInstruction('steak', 4)
+  .then ((step4) => {
+    document.querySelector('#steak').innerHTML += `<li>${step4}</li>`
+  })
+})
+.then (() => {
+  return obtainInstruction('steak', 5)
+  .then ((step5) => {
+    document.querySelector('#steak').innerHTML += `<li>${step5}</li>`
+  })
+})
+.then (() => {
+  return obtainInstruction('steak', 6)
+  .then ((step6) => {
+    document.querySelector('#steak').innerHTML += `<li>${step6}</li>`
+  })
+})
+.then (() => {
+  return obtainInstruction('steak', 7)
+  .then ((step7) => {
+    document.querySelector('#steak').innerHTML += `<li>${step7}</li>`
+  })
+})
+.catch(error => {
+  console.log(error)
+})
 
 // Iteration 3 using async/await
-// ...
+async function makeBroccoli() {
+  try {
+    const step0 = document.querySelector('#broccoli').innerHTML += `<li>${step0}</li>`
+    const step1 = document.querySelector('#broccoli').innerHTML += `<li>${step1}</li>`
+    const step2 = document.querySelector('#broccoli').innerHTML += `<li>${step2}</li>`
+    const step3 = document.querySelector('#broccoli').innerHTML += `<li>${step3}</li>`
+    const step4 = document.querySelector('#broccoli').innerHTML += `<li>${step4}</li>`
+    const step5 = document.querySelector('#broccoli').innerHTML += `<li>${step5}</li>`
+    const step6 = document.querySelector('#broccoli').innerHTML += `<li>${step6}</li>`
+
+    console.log(step0, step1, step2, step3, step4, step5, step6)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+makeBroccoli();
 
 // Bonus 2 - Promise all
 // ...
