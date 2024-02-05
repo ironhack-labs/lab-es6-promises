1cfc98e0355f7b415bc5ff6a787dd7819172ca09
Juan Yuste Del Valle <kurobaa@gmail.com>
2024-02-05 09:40:00 +0100

    fix error

--- javascript/index.js
+++ javascript/index.js
@@ -123,24 +123,20 @@ obtainInstruction("steak", 0)
 // Iteration 3 using async/await
 // ...
 async function makeBroccoli() {
-  await obtainInstruction("broccoli", 0);
-  document.querySelector("#broccoli").innerHTML += `<li>${broccoli[0]}</li>`;
-
-  await obtainInstruction("broccoli", 1);
-  document.querySelector("#broccoli").innerHTML += `<li>${broccoli[1]}</li>`;
-
-  await obtainInstruction("broccoli", 2);
-  document.querySelector("#broccoli").innerHTML += `<li>${broccoli[2]}</li>`;
-
-  await obtainInstruction("broccoli", 3);
-  document.querySelector("#broccoli").innerHTML += `<li>${broccoli[3]}</li>`;
-
-  await obtainInstruction("broccoli", 4);
-  document.querySelector("#broccoli").innerHTML += `<li>${broccoli[4]}</li>`;
-  await obtainInstruction("broccoli", 5);
-  document.querySelector("#broccoli").innerHTML += `<li>${broccoli[5]}</li>`;
-  await obtainInstruction("broccoli", 6);
-  document.querySelector("#broccoli").innerHTML += `<li>${broccoli[6]}</li>`;
+  const step0 = await obtainInstruction("broccoli", 0);
+  document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`;
+  const step1 = await obtainInstruction("broccoli", 1);
+  document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`;
+  const step2 = await obtainInstruction("broccoli", 2);
+  document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`;
+  const step3 = await obtainInstruction("broccoli", 3);
+  document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`;
+  const step4 = await obtainInstruction("broccoli", 4);
+  document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`;
+  const step5 = await obtainInstruction("broccoli", 5);
+  document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`;
+  const step6 = await obtainInstruction("broccoli", 6);
+  document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
 }
 
 makeBroccoli();
