0c8cf7205eedc29ee74e37bdf3bd39bb800f2c48
Karen Schneider <karen-schneider@outlook.com>
2024-02-08 20:44:45 +0100

    final

--- javascript/index.js
+++ javascript/index.js
@@ -4,28 +4,6 @@
 // 🚨🚨🚨 Comment out the below code before you start working on the code
 
 // Out of sync
- /* getInstruction("mashedPotatoes", 0, (step1) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
-  }, (error) => console.log(error));
-  
-  getInstruction("mashedPotatoes", 1, (step2) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
-  }, (error) => console.log(error));
-  
-  getInstruction("mashedPotatoes", 2, (step3) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
-  }, (error) => console.log(error));
-  
-  getInstruction("mashedPotatoes", 3, (step4) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
-  }, (error) => console.log(error));
-  
-  getInstruction("mashedPotatoes", 4, (step5) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
-    document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
-  }, (error) => console.log(error));*/
-
-
 
 // Iteration 1 - using callbacks -----------------------------------------------------
 
@@ -88,10 +66,10 @@ obtainInstruction('steak', 0)
     document.querySelector("#steakImg").removeAttribute("hidden");
   })
 
-// Iteration 3 using async/await
+/* Iteration 3 using async/await
 async function makeBroccoli() {
   // ... Your code here
-}
+}*/
 
 // Bonus 2 - Promise all
 // ...
