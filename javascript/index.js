a088b77f067bf6960dd53102be414cbc7083fda1
Adrià Chalaux <adriachalaux@gmail.com>
2024-02-08 20:29:29 +0100

    Lab done

--- javascript/index.js
+++ javascript/index.js
@@ -41,11 +41,11 @@ getInstruction("mashedPotatoes", 0, (step1) => {
           document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
           document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`;
           document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
-        }, (error) => console.log(error));
-      }, (error) => console.log(error));
-    }, (error) => console.log(error));
-  }, (error) => console.log(error));
-}, (error) => console.log(error));
+        }, (error) => console.log('Error:', error));
+      }, (error) => console.log('Error:', error));
+    }, (error) => console.log('Error:', error));
+  }, (error) => console.log('Error:', error));
+}, (error) => console.log('Error:', error));
 
 
 
@@ -93,7 +93,45 @@ obtainInstruction('steak', 0)
 
 
 // Iteration 3 using async/await
-// ...
+async function makeBroccoli() {
+  try {
+    for (let i = 0; i < broccoli.length; i++) {
+      console.log(broccoli[i])
+      await obtainInstruction("broccoli", i);
+      document.querySelector("#broccoli").innerHTML += `<li>${broccoli[i]}</li>`;
+    }
+  } catch (error) {
+    console.error('Error', error)
+  }
+
+  document.querySelector("#broccoliImg").removeAttribute("hidden");
+}
+
+makeBroccoli()
 
 // Bonus 2 - Promise all
-// ...
+let promises = []
+
+for (let step = 0; step < broccoli.length; step++) {
+  promises.push(obtainInstruction('brusselsSprouts', step)
+)}
+
+// let promises = [0, 1, 2, 3, 4, 5, 6, 7].map((step) => {
+//   return getInstruction("brusselsSprouts", step);
+// });
+
+// console.log(promises)
+
+Promise.all(promises)
+  .then((steps) => {
+    steps.forEach((step) => {
+      document.querySelector("#brusselsSprouts").innerHTML += `<li>${step}</li>`;
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(() => {
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>brusselsSprouts is ready!</li>`;
+    document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
+  })
